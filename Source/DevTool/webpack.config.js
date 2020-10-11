b594b33ed382e278102d637464162231f8a6fe6a
Einar Ingebrigtsen <einar@dolittle.com>
2020-10-11 10:04:09 +0200

    Setting title

--- Source/DevTool/webpack.config.js
+++ Source/DevTool/webpack.config.js
@@ -5,5 +5,5 @@ const webpack = require('@shared/webpack');
 module.exports = (env, argv) => {
     return webpack(env, argv, '/', config => {
         config.devServer.port = 9100;
-    });
+    }, 'Dolittle Studio DevTool');
 };
