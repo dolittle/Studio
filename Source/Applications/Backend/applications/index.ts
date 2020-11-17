312be6d8354c93a05fb97e1c751f4ac8a760c485
Einar Ingebrigtsen <einar@dolittle.com>
2020-11-17 13:32:22 +0100

    Hooking up Events + Projection

---
+++ Source/Applications/Backend/applications/index.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export * from './Application';
+export * from './ApplicationQueries';
+
+
+import { ClientBuilder } from '@dolittle/sdk';
+import { Application, ApplicationModel } from './Application';
+import { ApplicationCreated } from './events';
+
+export function registerDolittle(clientBuilder: ClientBuilder) {
+    clientBuilder.withEventTypes(_ => {
+        _.register(ApplicationCreated);
+    });
+
+    clientBuilder.withProjectionFor(Application, _ => {
+        _.withId('c25d1a88-fd4c-4b78-ac81-f90c3545fab9')
+            .useModel(ApplicationModel)
+            .fromScope('da92a933-a4c5-478d-a0c4-49aeef72f6d5');
+        _.set(a => a.name, p => p.from(ApplicationCreated, e => e.name));
+    });
+}
--- Source/Applications/Backend/index.ts
+++ Source/Applications/Backend/index.ts
@@ -3,8 +3,13 @@
 
 import path from 'path';
 import { startBackend } from '@shared/backend';
-
 import { getSchema } from './schema';
+import { PartitionId } from '@dolittle/sdk.events';
+
+import tenant from '@shared/backend/Tenant';
+import Microservices from '@shared/backend/Microservices';
+
+import { registerDolittle as applicationsRegisterDolittle } from './applications';
 
 (async () => {
     const schema = await getSchema();
@@ -20,6 +25,20 @@ import { getSchema } from './schema';
         expressCallback: _ => {
         },
         dolittleCallback: _ => {
+            applicationsRegisterDolittle(_);
+
+            _.withEventHorizons(eh => {
+                eh.forTenant(tenant, ts => {
+                    ts.fromProducerMicroservice(Microservices.k8s)
+                        .fromProducerTenant(tenant)
+                        .fromProducerStream('b628cd98-9d0d-4dbc-b4dd-56e89d7aa272')
+                        .fromProducerPartition(PartitionId.unspecified.value)
+                        .toScope('da92a933-a4c5-478d-a0c4-49aeef72f6d5')
+                        .onSuccess((t, s, sr) => console.log('Subscription: Success'))
+                        .onFailure((t, s, sr) => console.log(`Subscription: Failed - ${sr.failure?.reason}`))
+                        .onCompleted((t, s, sr) => console.log(`Subscription: Completed`));
+                });
+            });
         }
     });
 })();
