713942fad005e0f979b0f573b3153c521329538a
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-02 08:13:36 +0100

    Copyright header

--- Source/Events/Backend/webpack.config.js
+++ Source/Events/Backend/webpack.config.js
@@ -1,3 +1,6 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 const webpack = require('@shared/webpack/backend');
 
 module.exports = (env, argv) => {
