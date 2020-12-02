6b5409ca5d85c76ceeecdbc6493b7484763ea5cc
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-02 07:26:56 +0100

    Moving frontend WebPack into frontend folder

--- Source/Shared/WebPack/frontend/resolve.js
+++ Source/Shared/WebPack/frontend/resolve.js
@@ -2,9 +2,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 module.exports = {
-    extensions: ['.js', '.json', '.ts', '.tsx'],
-    modules: [
-        '.',
-        'node_modules'
-    ]
+    extensions: ['.js', '.json', '.ts', '.tsx']
 };
