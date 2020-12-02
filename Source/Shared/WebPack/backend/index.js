4dd56266ecee7ab41a9e8af0f1cbe32a5c6065f7
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-02 07:27:08 +0100

    Introducing backend WebPack

---
+++ Source/Shared/WebPack/backend/index.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
+// https://stackoverflow.com/questions/53801293/webpack-4-cant-find-package-json-in-production-mode
+// https://blog.webbylab.com/minimal_size_docker_image_for_your_nodejs_app/
+
+
+module.exports = (env, argv, callback) => {
+    const production = argv.mode === 'production';
+
+    const config = {
+        devtool: false,
+        mode: argv.mode || 'production',
+        target: 'node',
+        entry: './index.ts',
+        optimization: {
+            minimize: false
+            /*
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        cache: true,
+                        parallel: true,
+                        sourceMap: false,
+
+                        // We want the class names and function names to be there for the IoC to work its magic
+                        keep_classnames: true,
+                        keep_fnames: true
+                    }
+                })
+            ],*/
+        },
+        output: {
+            path: path.resolve(process.cwd(), 'dist'),
+            filename: 'index.js',
+            libraryTarget: 'commonjs'
+        },
+        resolve: {
+            enforceModuleExtension: false,
+            extensions: ['.mjs', '.js', '.json', '.ts'],
+            plugins: [
+                new TsconfigPathsPlugin()
+            ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.[tj]s$/i,
+                    exclude: /(node_modules)/,
+                    loader: 'ts-loader',
+                    options: {
+                        projectReferences: true,
+                        allowTsInNodeModules: true
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new webpack.ProgressPlugin()
+        ]
+    };
+
+    if (callback) {
+        callback(config);
+    }
+
+    return config;
+};
