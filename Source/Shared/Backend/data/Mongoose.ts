4d3f188d73cabea87b65e54d007c130ed3284dcc
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-09 18:48:31 +0100

    Turning off unified topology connection approach

--- Source/Shared/Backend/data/Mongoose.ts
+++ Source/Shared/Backend/data/Mongoose.ts
@@ -11,7 +11,6 @@ export class Mongoose {
 
         await mongoose.connect(connectionString, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
             dbName: databaseName,
             useFindAndModify: false
         });
