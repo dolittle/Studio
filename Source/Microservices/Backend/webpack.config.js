cd6ad7010f684ca38f775501b3613620b3a1712b
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-10 15:41:58 +0100

    Removing Microservices microservice

--- Source/Microservices/Backend/webpack.config.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-const webpack = require('@shared/webpack/backend');
-
-module.exports = (env, argv) => {
-    return webpack(env, argv, config => { });
-};
