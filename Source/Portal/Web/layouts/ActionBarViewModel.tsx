cec8d5f0f655cb2b273be983cca4e3d3e4fe3639
Pavneet Singh Saund <pavneet@dolittle.com>
2020-12-10 15:56:31 +0100

    Merge ActionBarState into the ViewModel and introduced 'filter'

--- Source/Portal/Web/layouts/ActionBarViewModel.tsx
+++ Source/Portal/Web/layouts/ActionBarViewModel.tsx
@@ -1,15 +1,40 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import { injectable } from 'tsyringe';
-import { ActionBarState } from './ActionBarState';
-import { ActionBar } from '@shared/portal';
+import { filter } from 'rxjs/operators';
+import {
+    ActionBar,
+    ActionBarAction,
+    ActionBarActionForMessage,
+    ActionBarActionWasTriggered,
+    ActionBarStructureChanged,
+} from '@shared/portal';
+import { IMessenger } from '@shared/mvvm';
 
 @injectable()
 export class ActionBarViewModel {
     actionBar?: ActionBar;
 
-    constructor(private readonly _state: ActionBarState) {}
+    constructor(private readonly _messenger: IMessenger) {}
     activate() {
-        this._state.current.subscribe((_) => (this.actionBar = _));
+        this._messenger
+            .observe(ActionBarStructureChanged)
+            .pipe(filter((_) => _.actionBar.button !== ActionBarActionForMessage.EMPTY))
+            .subscribe((_) => {
+                const actionBar = {
+                    placement: _.actionBar.placement,
+                    button: new ActionBarAction(
+                        _.actionBar.button.text,
+                        _.actionBar.button.icon,
+                        this._publishActionTriggered.bind(this),
+                        _.actionBar.button.id
+                    ),
+                } as ActionBar;
+                this.actionBar = actionBar;
+            });
+    }
+
+    private _publishActionTriggered() {
+        this._messenger.publish(new ActionBarActionWasTriggered());
     }
 }
