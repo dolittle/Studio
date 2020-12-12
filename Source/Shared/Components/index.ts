fe825db53ca8fbb98eaa936b0730203b64a01f9c
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-12 09:12:57 +0100

    Step #1 in changing casing

--- Source/Shared/Components/index.ts
+++ Source/Shared/Components/index.ts
@@ -2,5 +2,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 export * from './ContentFrame';
-export * from './Toolbar';
-export * from './ActionBar';
+export * from './Toolbar_';
+export * from './ActionBar_';
