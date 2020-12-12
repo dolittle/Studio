3d26a2096c3c25eea97c87a10e77cd9e4b306447
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-12 09:02:22 +0100

    Adding ActionBar component wrapper

---
+++ Source/Shared/Components/ActionBar/ActionBarViewModel.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { ActionBarProps } from './ActionBarProps';
+import { IViewContext } from '@shared/mvvm';
+import { ActionBarAction, ActionBarBroker } from '@shared/portal';
+import { injectable } from 'tsyringe';
+
+@injectable()
+export class ActionBarViewModel {
+
+    constructor(private readonly _actionBarItems: ActionBarBroker) {
+
+    }
+
+    activate(viewContext: IViewContext<ActionBarViewModel, ActionBarProps>) {
+        this._actionBarItems.set({
+            button: new ActionBarAction(
+                viewContext.props.title,
+                viewContext.props.icon,
+                viewContext.props.onClick),
+            placement: 'bottom'
+        })
+    }
+}
