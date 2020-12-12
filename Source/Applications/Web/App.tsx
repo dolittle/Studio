fb8827ff108ec69e9cead3e75149debb27656685
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-12 09:07:42 +0100

    Separating out App

---
+++ Source/Applications/Web/App.tsx
@@ -0,0 +1,48 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React, { useState } from 'react';
+
+import '@shared/styles/theme';
+import './index.scss';
+import { CreateApplication } from './CreateApplication';
+import { AssignMicroservice } from './AssignMicroservice';
+import { NavBar } from './NavBar';
+import { withViewModel } from '@shared/mvvm';
+import { AppViewModel } from './AppViewModel';
+
+import { ActionBar, Toolbar, ToolbarItem } from '@shared/components';
+
+export const App = withViewModel(AppViewModel, ({ viewModel }) => {
+    const [showCreateApplicationDialog, setShowCreateApplicationDialog] = useState(false);
+    const [showAssignMicroserviceDialog, setShowAssignMicroserviceDialog] = useState(false);
+
+    return (
+        <>
+            <NavBar/>
+            <Toolbar>
+                <ToolbarItem
+                    title="Assign microservice"
+                    icon="AppIconDefaultAdd"
+                    onClick={() => setShowAssignMicroserviceDialog(true)} />
+            </Toolbar>
+
+            <ActionBar
+                title="New Application"
+                icon="Add"
+                onClick={() => setShowCreateApplicationDialog(true)}/>
+
+            <CreateApplication
+                visible={showCreateApplicationDialog}
+                onCreated={() => setShowCreateApplicationDialog(false)}
+                onCancelled={() => setShowCreateApplicationDialog(false)}
+            />
+            <AssignMicroservice
+                forApplication={viewModel.selectedApplication}
+                visible={showAssignMicroserviceDialog}
+                onAssigned={() => setShowAssignMicroserviceDialog(false)}
+                onCancelled={() => setShowAssignMicroserviceDialog(false)}
+            />
+        </>
+    );
+});
