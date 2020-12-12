eac2ca1f19f9c6fd43600814b9d8c3bb4db0db81
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-12 08:26:44 +0100

    Introducing a component wrapper for Toolbar
    This enables us to declaratively work with the toolbar. Feels more natural from a view.

--- Source/Shared/Components/Toolbar/ToolbarItemProps.ts
+++ Source/Shared/Components/Toolbar/ToolbarItemProps.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export * from './ContentFrame';
-
-export * from './Toolbar';
+export interface ToolbarItemProps {
+    title: string;
+    icon?: string;
+    onClick: () => void;
+}
