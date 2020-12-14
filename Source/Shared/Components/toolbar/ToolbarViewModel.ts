ba09d95e9dc9a66941badedb8ff4d64ae62a0ee7
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-14 19:42:30 +0100

    Removing import statement

--- Source/Shared/Components/toolbar/ToolbarViewModel.ts
+++ Source/Shared/Components/toolbar/ToolbarViewModel.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { ToolbarItem, ToolbarItems } from '@shared/portal';
-import { ToolbarItemProps } from './ToolbarItemProps';
 import { ToolbarProps } from './ToolbarProps';
 import { IViewContext } from '@shared/mvvm';
 import { injectable } from 'tsyringe';
