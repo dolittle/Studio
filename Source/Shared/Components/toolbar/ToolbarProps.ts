7142430df131297954f9a78c014503c154e7dc17
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-14 19:40:35 +0100

    Forcing elements that have ToolbarItemProps

--- Source/Shared/Components/toolbar/ToolbarProps.ts
+++ Source/Shared/Components/toolbar/ToolbarProps.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import { ToolbarItemProps } from './ToolbarItemProps';
+
 export interface ToolbarProps {
-    children?: JSX.Element[] | JSX.Element;
-}
+    children?: React.ReactElement<ToolbarItemProps>[] |React.ReactElement<ToolbarItemProps>;
+}  
