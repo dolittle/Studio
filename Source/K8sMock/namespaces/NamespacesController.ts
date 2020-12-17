3a964b7a4ffd805dab3e86847a87c055fb142d1b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-12-17 07:56:27 +0100

    Add a few more namespaces, and add some more metadata

--- Source/K8sMock/namespaces/NamespacesController.ts
+++ Source/K8sMock/namespaces/NamespacesController.ts
@@ -4,64 +4,91 @@
 import { Controller, Get, Route, Tags, Query } from 'tsoa';
 import { V1NamespaceList, V1ListMeta, V1Namespace, V1ObjectMeta, V1NamespaceSpec, V1NamespaceStatus } from '@kubernetes/client-node';
 
+import { Guid } from '@dolittle/rudiments';
+
 @Route('api/v1/namespaces')
 @Tags('core_v1')
 export class NamespacesController extends Controller {
+    private readonly _namespaces: V1NamespaceList;
+
+    constructor() {
+        super();
+        this._namespaces = this.createNamespaceList();
+    }
 
     @Get()
     async getNamespaces(@Query() watch?: boolean): Promise<V1NamespaceList> {
         if (watch) {
             await new Promise(_ => {});
         }
+        return this._namespaces;
+    }
+
+    private createNamespaceList(): V1NamespaceList {
         const list = new V1NamespaceList();
         list.apiVersion = 'v1';
         list.kind = 'NamespaceList';
-        list.metadata = new V1ListMeta();
 
-        const objectMeta = new V1ObjectMeta();
-        objectMeta.name = 'application-fe7736bb-57fc-4166-bb91-6954f4dd4eb7';
-        objectMeta.labels = {
-            'tenant': 'Dolittle',
-            'application': 'Studio',
-            'tenant-id': '508c1745-5f2a-4b4c-b7a5-2fbb1484346d',
-        };
-        const studioNamespace = this.createNamespace(
-            'application-fe7736bb-57fc-4166-bb91-6954f4dd4eb7',
+        const metadata = list.metadata = new V1ListMeta();
+        metadata.selfLink = '/api/v1/namespaces';
+        metadata.resourceVersion = `${Math.ceil(Math.random() * 1e6)}`;
+
+        list.items = [];
+
+        list.items.push(this.createNamespace(
+            'application-853ebea1-1e6b-4fee-9855-10d1df5cad1e',
             {
                 'tenant': 'Dolittle',
                 'application': 'Studio',
                 'tenant-id': '508c1745-5f2a-4b4c-b7a5-2fbb1484346d',
-            });
-        const truckNamespace = this.createNamespace(
+            }));
+
+        list.items.push(this.createNamespace(
+            'application-a7e5065c-11a7-4e14-b281-c86bcf2dbd5c',
+            {
+                'tenant': 'Dolittle',
+                'application': 'Lunch App',
+                'tenant-id': '508c1745-5f2a-4b4c-b7a5-2fbb1484346d',
+            }));
+
+        list.items.push(this.createNamespace(
             'application-17e4f11e-4cf9-49a6-af0d-b69be8af1e63',
             {
                 'tenant': 'Dolittle-Truck-Service',
                 'application': 'trucker',
                 'tenant-id': 'afffe616-9797-489e-af2d-eccb09a96020'
-            });
+            }));
 
-        const labellessNamespace = this.createNamespace(
-            'labelless',
-            {});
-
-        const ingressNamespace = this.createNamespace(
-            'ingress',
+        list.items.push(this.createNamespace(
+            'kube-system',
             {
-                'certmanager.k8s.io/disable-validation': 'true'
-            });
-        list.items = [studioNamespace, truckNamespace, labellessNamespace, ingressNamespace];
+                'control-plane': 'true',
+                'kubernetes.io/cluster-service': 'true'
+            }));
+
+        list.items.push(this.createNamespace(
+            'system-auth',
+            {}));
 
         return list;
     }
 
     private createNamespace(name: string, labels: { [key: string]: string; }): V1Namespace {
-        const objectMeta = new V1ObjectMeta();
-        objectMeta.name = name;
-        objectMeta.labels = labels;
-
         const namespace = new V1Namespace();
-        namespace.metadata = objectMeta;
-        namespace.spec = new V1NamespaceSpec();
+
+        const metadata = namespace.metadata = new V1ObjectMeta();
+        metadata.name = name;
+        metadata.labels = labels;
+        metadata.uid = Guid.create().toString();
+        metadata.selfLink = `/api/v1/namespaces/${name}`;
+        metadata.resourceVersion = `${Math.ceil(Math.random() * 1e6)}`;
+        metadata.creationTimestamp = new Date(Date.now() - Math.random()*100*24*3600*1000);
+
+        const spec = namespace.spec = new V1NamespaceSpec();
+        spec.finalizers = [ 'kubernetes' ];
+
+        const status = namespace.status = new V1NamespaceStatus();
+        status.phase = 'Active';
 
         return namespace;
     }
