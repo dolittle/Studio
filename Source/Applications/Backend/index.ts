4628aaa779f8eff9b330915fe779c1743ead0cfd
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-20 09:36:47 +0100

    Changing applications to work with new config

--- Source/Applications/Backend/index.ts
+++ Source/Applications/Backend/index.ts
@@ -1,31 +1,20 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import path from 'path';
 import { startBackend } from '@shared/backend';
-import { getSchema } from './schema';
+import queries from './queries';
 import { PartitionId } from '@dolittle/sdk.events';
 
 import tenant from '@shared/backend/Tenant';
 import Microservices from '@shared/backend/Microservices';
 
 import { projectFromEventsToReadModels } from './applications';
+import { logger } from '@shared/backend/logging';
 
 (async () => {
-    const schema = await getSchema();
-
     await startBackend({
-        microserviceId: '05a823dd-0f0d-4157-bc46-14834cca3cd2',
-        prefix: '/_/applications',
-        publicPath: './public',
-        port: 3002,
-        dolittleRuntimePort: 50057,
-        graphQLSchema: schema,
-        defaultDatabaseName: 'applications',
-        defaultEventStoreDatabaseName: 'event_store_applications',
-        expressCallback: _ => {
-        },
-            dolittleCallback: _ => {
+        graphQLResolvers: queries,
+        dolittleCallback: _ => {
             projectFromEventsToReadModels(_);
 
             _.withEventHorizons(eh => {
@@ -35,9 +24,9 @@ import { projectFromEventsToReadModels } from './applications';
                         .fromProducerStream('b628cd98-9d0d-4dbc-b4dd-56e89d7aa272')
                         .fromProducerPartition(PartitionId.unspecified.value)
                         .toScope('da92a933-a4c5-478d-a0c4-49aeef72f6d5')
-                        .onSuccess((t, s, sr) => console.log('Subscription: Success'))
-                        .onFailure((t, s, sr) => console.log(`Subscription: Failed - ${sr.failure?.reason}`))
-                        .onCompleted((t, s, sr) => console.log(`Subscription: Completed`));
+                        .onSuccess((t, s, sr) => logger.info('EventHorizon subscription: Success'))
+                        .onFailure((t, s, sr) => logger.error(`EventHorizon subscription: Failed - ${sr.failure?.reason}`))
+                        .onCompleted((t, s, sr) => logger.info(`EventHorizon subscription: Completed`));
                 });
             });
         }
---
+++ Source/Applications/Backend/queries.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { ApplicationQueries, MicroserviceQueries } from './applications';
+import { ApplicationForListingQueries } from './listing/ApplicationForListingQueries';
+
+export default [
+    ApplicationQueries, MicroserviceQueries, ApplicationForListingQueries
+];
