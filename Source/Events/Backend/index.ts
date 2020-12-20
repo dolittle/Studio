59abed25bf736e00cbdc5fb1db3747db0df79e75
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-20 09:49:03 +0100

    FIxing for new config

--- Source/Events/Backend/index.ts
+++ Source/Events/Backend/index.ts
@@ -1,26 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import path from 'path';
 import { startBackend } from '@shared/backend';
 
-import { getSchema } from './schema';
-
 (async () => {
-    const schema = await getSchema();
-
-    await startBackend({
-        microserviceId: '81cad113-a001-45dc-86cb-7b1e725ae25e',
-        prefix: '/_/events',
-        publicPath: './public',
-        port: 3003,
-        dolittleRuntimePort: 50059,
-        graphQLSchema: schema,
-        defaultDatabaseName: 'events',
-        defaultEventStoreDatabaseName: 'event_store_applications',
-        expressCallback: _ => {
-        },
-        dolittleCallback: _ => {
-        }
-    });
+    await startBackend({});
 })();
