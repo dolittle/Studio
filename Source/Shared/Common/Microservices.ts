cecb3648657142e1426d35c7de909189b2b81ed5
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-24 10:00:39 +0100

    Adding common -> shared project

---
+++ Source/Shared/Common/Microservices.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export default {
+    portal: '590696bc-1c4e-4a14-b621-c2125713a974',
+    k8s: 'c2e34414-5054-4050-8ee4-eb6f3265cce3',
+    applications: '05a823dd-0f0d-4157-bc46-14834cca3cd2',
+    microservices: 'A9CF063D-415F-4F83-A528-2F0D6057F05D',
+    events: '81cad113-a001-45dc-86cb-7b1e725ae25e'
+};
---
+++ Source/Shared/Common/Tenant.ts
@@ -0,0 +1,4 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export default '508c1745-5f2a-4b4c-b7a5-2fbb1484346d';
