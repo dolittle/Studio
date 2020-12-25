210e1c343e2934bf57e062587a2a1dc96d78c81e
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-25 10:14:44 +0100

    Fixing ref to bindings - deep dist import

--- Source/Shared/Portal/Bindings.ts
+++ Source/Shared/Portal/Bindings.ts
@@ -1,9 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Bindings as NavigationBindings } from '@dolittle/vanir-web/navigation/Bindings';
+import { Bindings as NavigationBindings } from '@dolittle/vanir-web/dist/navigation/Bindings';
 import { Bindings as ActionBarBindings } from './actionbar/Bindings';
-
 export class Bindings {
     static initialize() {
         NavigationBindings.initialize();
