546b9aa96745c0a596b4261e00124c1b7854afa1
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-26 08:15:45 +0100

    Switching to Host for startup

--- Source/K8sMock/index.ts
+++ Source/K8sMock/index.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { startBackend } from '@dolittle/vanir-backend';
+import { Host } from '@dolittle/vanir-backend';
 import { RegisterRoutes } from './routes';
 const swaggerDoc = require('./swagger.json');
 
 (async () => {
-    await startBackend({
+    await Host.start({
         swaggerDoc,
         expressCallback: _ => {
             RegisterRoutes(_);
