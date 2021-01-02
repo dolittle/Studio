6c57b3a55560716590426cbb865c30907fea6f63
Einar Ingebrigtsen <einar@dolittle.com>
2021-01-02 07:39:41 +0100

    Changing title

--- Source/DevTool/web/webpack.config.js
+++ Source/DevTool/web/webpack.config.js
@@ -5,5 +5,5 @@ const webpack = require('@dolittle/vanir-webpack/frontend');
 module.exports = (env, argv) => {
     return webpack(env, argv, '/', config => {
         config.devServer.port = 9100;
-    }, 'Dolittle Studio DevTool');
+    }, 'Dolittle DevCentral');
 };
