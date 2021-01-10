96ca8763c757365537788edbb468ddcba493ba75
Einar Ingebrigtsen <einar@dolittle.com>
2021-01-10 09:28:47 +0100

    Setting correct basePath for both dev and build

--- Source/DevTool/web/webpack.config.js
+++ Source/DevTool/web/webpack.config.js
@@ -5,7 +5,9 @@ const path = require('path');
 
 const webpack = require('@dolittle/vanir-webpack/frontend');
 module.exports = (env, argv) => {
-    return webpack(env, argv, './', config => {
+    const isWebDevServer = (process.env.WEBPACK_DEV_SERVER || false) === 'true' ? true : false;
+    const basePath = isWebDevServer ? '/' : './';
+    return webpack(env, argv, basePath, config => {
         config.output.path = path.resolve(process.cwd(), '..', 'build');
         config.devServer.port = 9100;
     }, 'Dolittle DevCentral');
