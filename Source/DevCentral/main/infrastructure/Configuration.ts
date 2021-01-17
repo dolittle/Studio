c2bded2ce580a6883b86d6cbe9d30de3dc6d56a5
Einar Ingebrigtsen <einar@dolittle.com>
2021-01-17 18:44:54 +0100

    Adding missing import

--- Source/DevCentral/main/infrastructure/Configuration.ts
+++ Source/DevCentral/main/infrastructure/Configuration.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import Handlebars from 'handlebars';
+
 import { container } from 'tsyringe';
 import { IFileSystem } from './IFileSystem';
 import { FileSystem } from './FileSystem';
