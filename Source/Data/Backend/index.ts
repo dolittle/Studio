b754a96d4b93b36aed754d9178cf31cb22ffe63e
Chris <chris@dolittle.com>
2021-03-26 13:50:05 +0100

    Ugly hack so I can remember to add a label to the build step

--- Source/Data/Backend/index.ts
+++ Source/Data/Backend/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// Trigger rebuild :(
 import 'reflect-metadata';
 import { Host } from '@dolittle/vanir-backend';
 import { RegisterRoutes } from './routes';
