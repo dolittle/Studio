6a8194d008a9d7a9207bcf6e42b49cc5c8e4785c
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-19 16:00:08 +0200

    Putting in property for embed code

--- Source/BusinessMomentsAdapters/Backend/read/connectors/Connector.ts
+++ Source/BusinessMomentsAdapters/Backend/read/connectors/Connector.ts
@@ -1,9 +1,15 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 import { ObjectType, Field } from 'type-graphql';
 import { Guid } from '@dolittle/rudiments';
-
 
 @ObjectType()
 export class Connector {
     @Field()
-    _id!: Guid;
+    _id!: string;
+
+    @Field()
+    embedCode!: string;
 }
+
