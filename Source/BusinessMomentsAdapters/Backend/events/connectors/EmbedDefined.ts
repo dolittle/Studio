9cebe35f4ac5a3c1481344eef66b9e5fdfa22094
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-19 15:58:03 +0200

    Introducing EmbedDefined event

---
+++ Source/BusinessMomentsAdapters/Backend/events/connectors/EmbedDefined.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { eventType } from '@dolittle/sdk.events';
+
+@eventType('944a702b-302c-4856-89bc-f6830a8464ae')
+export class EmbedDefined {
+    constructor(readonly code: string) { }
+}
