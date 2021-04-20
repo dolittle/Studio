20452371494a030ccd8b59e8d0095a02f7f9b1f3
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-20 14:00:31 +0200

    Restructuring specs

--- Source/BusinessMomentsAdapters/Backend/domain/adapters/for_Adapter/when_adding_connector/and_there_are_no_other_connectors.ts
+++ Source/BusinessMomentsAdapters/Backend/domain/adapters/for_Adapter/when_adding_connector/and_there_are_no_other_connectors.ts
@@ -1,13 +1,13 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Adapter } from '../Adapter';
+import { Adapter } from '../../Adapter';
 import { EventSourceId } from '@dolittle/sdk.events';
 import { Guid } from '@dolittle/rudiments';
-import { ConnectorTypes } from '../../../concepts';
-import { ConnectorAddedToAdapter } from '../../../events/adapters';
+import { ConnectorTypes } from '../../../../concepts';
+import { ConnectorAddedToAdapter } from '../../../../events/adapters';
 
-describe('when adding first connector', () => {
+describe('when adding connector and there are no other connectors', () => {
     const name = 'Some Connector';
     const connectorId = Guid.parse('a60bd940-7a3f-417d-bf2d-8126513fe3fc');
     const adapter = new Adapter(EventSourceId.from('46e44f22-1e76-4110-8b1d-d088cd3a1481'));
