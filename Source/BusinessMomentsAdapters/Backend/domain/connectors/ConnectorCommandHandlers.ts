5cc3835afd7e42ea0f2aa5843c3ff41d3a73e2e4
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-21 14:34:58 +0200

    Hooking up add miner to connector

--- Source/BusinessMomentsAdapters/Backend/domain/connectors/ConnectorCommandHandlers.ts
+++ Source/BusinessMomentsAdapters/Backend/domain/connectors/ConnectorCommandHandlers.ts
@@ -1,13 +1,22 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Resolver } from 'type-graphql';
+import { Arg, Resolver, Mutation } from 'type-graphql';
 import { IAggregate } from '@dolittle/vanir-backend';
 import { injectable } from 'tsyringe';
+import { AddMinerToConnector } from './AddMinerToConnector';
+import { Connector } from './Connector';
 
 @Resolver()
 @injectable()
 export class ConnectorCommandHandlers {
     constructor(private readonly _aggregate: IAggregate) {
+    }
+
+    @Mutation(() => Boolean)
+    async addMinerToConnector(@Arg('command') command: AddMinerToConnector) {
+        const connector = await this._aggregate.of(Connector, command.connectorId);
+        await connector.perform(_ => _.addMiner(command.minerId, command.name));
+        return true;
     }
 }
