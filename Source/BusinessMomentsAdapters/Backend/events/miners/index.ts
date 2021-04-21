2b2b5970736fdd2e2e8e2a9f9fcc21bf80b01bd5
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-21 14:36:00 +0200

    Export the new event type; EntityAssociatedWithMiner

--- Source/BusinessMomentsAdapters/Backend/events/miners/index.ts
+++ Source/BusinessMomentsAdapters/Backend/events/miners/index.ts
@@ -2,11 +2,14 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { ImperativeEmbedDefined } from './ImperativeEmbedDefined';
+import { EntityAssociatedWithMiner } from './EntityAssociatedWithMiner';
 
 export const Events = [
     ImperativeEmbedDefined,
+    EntityAssociatedWithMiner
 ];
 
 export {
     ImperativeEmbedDefined,
+    EntityAssociatedWithMiner
 };
