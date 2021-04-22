c613bb0ec4b6ce022f2a9ed1a37bf85bef4e3097
freshteapot <chris@dolittle.com>
2021-04-22 16:21:52 +0200

    more ugly

--- Source/BusinessMomentsAdapters/Web/moments/EntityEditor.tsx
+++ Source/BusinessMomentsAdapters/Web/moments/EntityEditor.tsx
@@ -1,12 +1,100 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-import React from 'react';
 
+import React, { useEffect } from 'react';
+
+import { TextField, ITextFieldStyles } from '@fluentui/react/lib/TextField';
+import { Dropdown } from '@fluentui/react/lib/Dropdown';
+import { Stack } from '@fluentui/react/lib/Stack';
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+import { Pivot, PivotItem, IDropdownOption } from '@fluentui/react';
+
+
+import CodeEditor, { useMonaco } from '@monaco-editor/react';
+
+
+const textFieldStyles: Partial<ITextFieldStyles> = { fieldGroup: { width: 300 } };
+const stackTokens = { childrenGap: 15 };
 
 export const Editor: React.FunctionComponent = () => {
+
+    const monaco = useMonaco();
+
+    useEffect(() => {
+        if (monaco) {
+            console.log('here is the monaco isntance:', monaco);
+        }
+    }, [monaco]);
+
+    // Not sure how to hook up to IPivotItemProps
+    const [outputScreen, setOutputScreen] = React.useState('Raw Data');
+    const onOutputScreenChanged = (item?: PivotItem, ev?: React.MouseEvent<HTMLElement>) => {
+        setOutputScreen(item!.props.headerText as string);
+    };
+
     return (
         <>
-            <h1>Entity Editor</h1>
+            <Stack tokens={stackTokens}>
+                <Stack horizontal tokens={stackTokens}>
+                    <Dropdown placeholder="Select"
+                        label="Connector:"
+                        options={[]}
+                    />
+
+                    <TextField
+                        label="Entity Name:"
+                        styles={textFieldStyles}
+                    />
+
+                    <TextField
+                        label="Entity ID:"
+                        styles={textFieldStyles}
+                        value="id"
+                    />
+
+                </Stack>
+
+                <Stack grow={1} tokens={stackTokens}>
+                    <Stack horizontal grow={1} tokens={stackTokens}>
+
+                        <Stack tokens={{ childrenGap: 15, maxWidth: '50vw' }}>
+                            <h1>Filter Payload</h1>
+                            <CodeEditor
+                                height="20vh"
+                                width="50vw"
+                                defaultLanguage="javascript"
+                                defaultValue="// some comment"
+                            />
+
+                            <h1>Transform Payload</h1>
+                            <CodeEditor
+                                height="20vh"
+                                width="50vw"
+                                defaultLanguage="javascript"
+                                defaultValue="// some comment"
+                            />
+                        </Stack>
+
+                        <Stack tokens={{ childrenGap: 15, maxWidth: '50vw' }}>
+                            <Pivot onLinkClick={onOutputScreenChanged}>
+                                <PivotItem
+                                    headerText="Raw Data"
+                                >
+                                </PivotItem>
+                                <PivotItem
+                                    headerText="With Filter">
+                                </PivotItem>
+                                <PivotItem
+                                    headerText="With Transform">
+                                </PivotItem>
+                            </Pivot>
+                            <textarea value={outputScreen} />
+                        </Stack>
+
+                    </Stack>
+                </Stack>
+
+            </Stack>
         </>
     );
 };
