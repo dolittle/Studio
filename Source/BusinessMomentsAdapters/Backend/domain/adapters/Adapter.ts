7168480fc682855c29903d0ba6ffa6e8541b0aaf
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-24 08:57:21 +0200

    Making sure we don't get duplicate connectors on id

--- Source/BusinessMomentsAdapters/Backend/domain/adapters/Adapter.ts
+++ Source/BusinessMomentsAdapters/Backend/domain/adapters/Adapter.ts
@@ -10,9 +10,11 @@ import { BrokenRule } from '@dolittle/vanir-backend';
 export const ConnectorWithNameAlreadyExists =
     BrokenRule.create('ConnectorWithNameAlreadyExists', 'Connector with name "{name}" already exists on adapter "{adapter}"');
 
+export const ConnectorWithIdAlreadyExists =
+    BrokenRule.create('ConnectorWithIdAlreadyExists', 'Connector with identifier "{id}" already exists on adapter "{adapter}"');
 @aggregateRoot('08dfe483-0b6a-40e1-95b7-1b3c7ccaf187')
 export class Adapter extends AggregateRoot {
-    private _connectors: string[] = [];
+    private _connectors: { id: Guid, name: string }[] = [];
 
     constructor(eventSourceId: EventSourceId) {
         super(eventSourceId);
@@ -20,18 +22,31 @@ export class Adapter extends AggregateRoot {
 
     addConnector(name: string, connectorId: Guid, connectorTypeId: Guid): void {
         this.failIfConnectorWithNameAlreadyExists(name);
+        this.failIfConnectorWithIdAlreadyExists(connectorId);
         this.apply(new ConnectorAddedToAdapter(name, connectorId.toString(), connectorTypeId.toString()));
     }
 
     @on(ConnectorAddedToAdapter)
     onConnectorAddedToAdapter(event: ConnectorAddedToAdapter) {
-        this._connectors.push(event.name);
+        this._connectors.push({
+            id: Guid.parse(event.connectorId),
+            name: event.name
+        });
     }
 
     private failIfConnectorWithNameAlreadyExists(name: string) {
-        if (this._connectors.some(_ => _ === name)) {
+        if (this._connectors.some(_ => _.name === name)) {
             this.fail(ConnectorWithNameAlreadyExists, {
                 name,
+                adapter: this.eventSourceId.toString()
+            });
+        }
+    }
+
+    private failIfConnectorWithIdAlreadyExists(id: Guid) {
+        if (this._connectors.some(_ => _.id.equals(id))) {
+            this.fail(ConnectorWithIdAlreadyExists, {
+                id,
                 adapter: this.eventSourceId.toString()
             });
         }
--- Source/BusinessMomentsAdapters/Backend/domain/adapters/for_Adapter/when_adding_connector/and_there_is_an_existing_with_same_id.ts
+++ Source/BusinessMomentsAdapters/Backend/domain/adapters/for_Adapter/when_adding_connector/and_there_is_an_existing_with_same_id.ts
@@ -1,27 +1,28 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Adapter, ConnectorWithIdAlreadyExists } from '../../Adapter';
+import { Adapter, ConnectorWithNameAlreadyExists } from '../../Adapter';
 import { EventSourceId } from '@dolittle/sdk.events';
 import { Guid } from '@dolittle/rudiments';
 import { ConnectorTypes } from '../../../../concepts';
 import { BrokenRuleError } from '@dolittle/vanir-backend';
 
-describe('when adding connector and there is an existing with same id', () => {
+describe('when adding connector and there is an existing with same name', () => {
+    const name = 'Some Connector';
     const adapter = new Adapter(EventSourceId.from('46e44f22-1e76-4110-8b1d-d088cd3a1481'));
     adapter.onConnectorAddedToAdapter({
-        name: 'Some Other Connector',
-        connectorId: 'f4815694-5698-4fa6-8938-ad9104a7b63a',
+        name,
+        connectorId: 'a60bd940-7a3f-417d-bf2d-8126513fe3fc',
         connectorTypeId: ConnectorTypes.WebHook.toString()
     });
 
     let brokenRuleError: BrokenRuleError;
 
     try {
-        adapter.addConnector('Some Connector', Guid.parse('f4815694-5698-4fa6-8938-ad9104a7b63a'), ConnectorTypes.WebHook);
+        adapter.addConnector(name, Guid.parse('f4815694-5698-4fa6-8938-ad9104a7b63a'), ConnectorTypes.WebHook);
     } catch (ex) {
         brokenRuleError = ex;
     }
 
-    it('should fail with a connector with id already exists broken rule', () => brokenRuleError.rule.should.be.equal(ConnectorWithIdAlreadyExists));
+    it('should fail with a connector with name already exists broken rule', () => brokenRuleError.rule.should.be.equal(ConnectorWithNameAlreadyExists));
 });
