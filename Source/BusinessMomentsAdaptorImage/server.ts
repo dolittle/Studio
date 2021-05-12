1e7044db2c14d357bd3b9201fe102a2d77c4dd06
Parham Barazesh <parham.barazesh@gmail.com>
2021-05-12 11:02:49 +0200

    removing unneccessary comments

--- Source/BusinessMomentsAdaptorImage/server.ts
+++ Source/BusinessMomentsAdaptorImage/server.ts
@@ -1,20 +1,15 @@
-// import express from 'express';
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import process from 'process';
-// import { default } from '../Shared/Common/Microservices';
 
 createServer();
 export default function createServer() {
-    // Initialize express and define a port
     const app: Application = express();
     const PORT = 3008;
 
     // Tell express to use body-parser's JSON parsing
-
     app.use(bodyParser.json());
 
-    // Start express on the defined port
     if (process.env.WH_AUTHORIZATION) {
         if (!process.env.NODE_ENV) {
             app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
@@ -27,8 +22,8 @@ export default function createServer() {
 
     app.post('/api/webhooks-ingestor', (req: Request, res: Response) => {
         if (req.headers.authorization === process.env.WH_AUTHORIZATION) {
-            console.log(req.body); // Call your action on the request here
-            res.status(200).end(); // Responding is important
+            console.log(req.body);
+            res.status(200).end();
         } else {
             res.status(401).end();
         }
