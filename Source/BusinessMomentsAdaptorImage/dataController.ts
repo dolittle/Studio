a772a8d351a69fc922f1976de279bbc45a2be50b
Parham Barazesh <parham.barazesh@gmail.com>
2021-05-25 18:20:38 +0200

    remove unneccessary endpoint controllers

--- Source/BusinessMomentsAdaptorImage/dataController.ts
+++ Source/BusinessMomentsAdaptorImage/dataController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
 import rawData from './dataschema';
 
-// every we have from our database
 export let allData = (req: any, res: any) => {
-    let data = rawData.find((err: any, allData: any) => {
+    let data = rawData.find((err: any, data: any) => {
         if (err) {
             res.send('Error!');
         } else {
-            res.send(allData);
+            res.send(data);
         }
     });
 };
@@ -29,26 +28,6 @@ export let addData = (req: Request, res: Response) => {
             res.send(err);
         } else {
             res.send(data);
-        }
-    });
-};
-
-export let deleteData = (req: Request, res: Response) => {
-    rawData.deleteOne({ _id: req.params.id }, (err: any) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send('Successfully Deleted the data');
-        }
-    });
-};
-
-export let updateData = (req: Request, res: Response) => {
-    rawData.findByIdAndUpdate(req.params.id, req.body, (err: any, data: any) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send('Successfully updated the data');
         }
     });
 };
