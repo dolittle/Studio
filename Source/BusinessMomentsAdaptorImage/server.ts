a58651da66d42fe04da88384818513ec670540e0
goeran <mail@goeran.no>
2021-05-28 12:46:55 +0200

    fixed bug after merge

--- Source/BusinessMomentsAdaptorImage/server.ts
+++ Source/BusinessMomentsAdaptorImage/server.ts
@@ -20,65 +20,65 @@ export function createServer(rawDataStorage: IRawDataStorage) {
     const transformers = fsloader.loadSync(transformersDir);
     for (const transformer of transformers) {
         rawDataProcessor.AddEntityTransformer(transformer);
-
-        // Tell express to use body-parser's JSON parsing.
-        app.use(bodyParser.json());
-
-        app.post('/api/webhooks-ingestor', async (req: Request, res: Response) => {
-            // TODO: figure out how we do this with middleware (less duplication)
-            if (!isAuthorized(req)) {
-                res.status(401).end();
-                return;
-            }
-
-            try {
-                const result = rawDataProcessor.Process(req.body);
-                if (result.WasProcessed) {
-                    await repo.Append(req.body);
-                    //console.log(req.body);
-                    console.log(result.Result);
-                } else {
-                    console.log('was not able to process payload');
-                }
-
-            } catch (err) {
-                console.log(err);
-                res.status(500).end();
-            }
-            res.status(200).end();
-        });
-
-        app.get('/api/webhooks-ingestor/data', async (req: Request, res: Response) => {
-            if (!isAuthorized(req)) {
-                res.status(401).end();
-                return;
-            }
-
-            try {
-                const result = await repo.GetAll();
-                res.send(result).end();
-            } catch (err) {
-                console.log(err);
-                res.status(500).end();
-            }
-        });
-
-        app.get('/api/webhooks-ingestor/data/:id', async (req: Request, res: Response) => {
-            if (!isAuthorized(req)) {
-                res.status(401).end();
-                return;
-            }
-
-            try {
-                const result = await repo.GetById(req.params.id);
-                res.send(result).end();
-            } catch (_) {
-                res.status(500).end();
-            }
-        });
-
-        return app;
     }
+
+    // Tell express to use body-parser's JSON parsing.
+    app.use(bodyParser.json());
+
+    app.post('/api/webhooks-ingestor', async (req: Request, res: Response) => {
+        // TODO: figure out how we do this with middleware (less duplication)
+        if (!isAuthorized(req)) {
+            res.status(401).end();
+            return;
+        }
+
+        try {
+            const result = rawDataProcessor.Process(req.body);
+            if (result.WasProcessed) {
+                await repo.Append(req.body);
+                //console.log(req.body);
+                console.log(result.Result);
+            } else {
+                console.log('was not able to process payload');
+            }
+
+        } catch (err) {
+            console.log(err);
+            res.status(500).end();
+        }
+        res.status(200).end();
+    });
+
+    app.get('/api/webhooks-ingestor/data', async (req: Request, res: Response) => {
+        if (!isAuthorized(req)) {
+            res.status(401).end();
+            return;
+        }
+
+        try {
+            const result = await repo.GetAll();
+            res.send(result).end();
+        } catch (err) {
+            console.log(err);
+            res.status(500).end();
+        }
+    });
+
+    app.get('/api/webhooks-ingestor/data/:id', async (req: Request, res: Response) => {
+        if (!isAuthorized(req)) {
+            res.status(401).end();
+            return;
+        }
+
+        try {
+            const result = await repo.GetById(req.params.id);
+            res.send(result).end();
+        } catch (_) {
+            res.status(500).end();
+        }
+    });
+
+    return app;
 }
 
 function isAuthorized(req: Request) {
