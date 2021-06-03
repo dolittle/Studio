d1b30db0d08290c0dfceb5e93d2913188b534029
Parham Barazesh <parham.barazesh@gmail.com>
2021-06-03 12:56:52 +0200

    Added webhookReceived event handler to print the status of webhook received

---
+++ Source/BusinessMomentsAdaptorImage/wehbookReceivedHandler.ts
@@ -0,0 +1,11 @@
+import { EventContext } from '@dolittle/sdk.events';
+import { eventHandler, handles } from '@dolittle/sdk.events.handling';
+import { WebhookReceived } from './WebhookReceived';
+
+@eventHandler('ed6d9f23-64d0-4009-a19b-a07301fa0c98')
+export class WebhookReceivedHandler {
+    @handles(WebhookReceived)
+    webhookReceived(event: WebhookReceived, eventContext: EventContext) {
+        console.log(`webhook received status: ${event.status}`);
+    }
+}
