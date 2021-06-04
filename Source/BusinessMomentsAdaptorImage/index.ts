36c6e2afcb6c15696d379b2d6e130d5252a2352d
Parham Barazesh <parham.barazesh@gmail.com>
2021-06-04 11:23:36 +0200

    moved client to index.ts

--- Source/BusinessMomentsAdaptorImage/index.ts
+++ Source/BusinessMomentsAdaptorImage/index.ts
@@ -3,8 +3,16 @@
 //test comment
 import { createServer, startServer } from './server';
 import { MongodbRawDataStorage } from './RawDataStorage';
+import { Client } from '@dolittle/sdk';
+import { WebhookReceived } from './WebhookReceived';
+import { WebhookReceivedHandler } from './wehbookReceivedHandler';
 
 const rawDataStorage = new MongodbRawDataStorage();
+export const client = Client.forMicroservice('a87b3411-7f23-4ea3-9b74-f6fc7f7c5481')
+    .withEventTypes((eventTypes) => eventTypes.register(WebhookReceived))
+    .withEventHandlers((builder) => builder.register(WebhookReceivedHandler))
+    .build();
 // TODO: inject dependency with IoC in the future.
 const app = createServer(rawDataStorage);
+
 startServer(app);
