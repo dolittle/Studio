5bdd21f5ef26205044acb2bc690d486593d31832
Parham Barazesh <parham.barazesh@gmail.com>
2021-06-10 11:39:57 +0200

    Add another event handler for checking the code

--- Source/Purchaseorderapi/Backend/eventHandlers.ts
+++ Source/Purchaseorderapi/Backend/eventHandlers.ts
@@ -1,13 +1,21 @@
 import { EventContext } from '@dolittle/sdk.events';
 import { eventHandler, handles } from '@dolittle/sdk.events.handling';
-import { PurchaseOrderLineUpdated } from './events';
+import { PurchaseOrderLineUpdated, FacilityCreated } from './events';
 
-@eventHandler('808fd9f2-235e-462b-beba-fbfae6518802')
+@eventHandler('a6cca04c-2a19-4ce0-8928-6d6377cfa1e5')
 export class PurchaseOrderLineUpdatedHandler {
     @handles(PurchaseOrderLineUpdated)
     webhookReceived(event: PurchaseOrderLineUpdated, eventContext: EventContext) {
         console.log(
             `Purchase Order Line Updated: ${event.username} \n description: ${event.facilityNumber}`
         );
+    }
+}
+
+@eventHandler('47352355-269c-4b41-94c5-64ea4ad83103')
+export class FacilityCreatedHandler {
+    @handles(FacilityCreated)
+    webhookReceived(event: FacilityCreated, eventContext: EventContext) {
+        console.log(`Facility Created: ${event.FacilityNumber}`);
     }
 }
