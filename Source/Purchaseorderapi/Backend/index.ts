a54eaa2d8c544db57423368c893b63ca1e202489
Parham Barazesh <parham.barazesh@gmail.com>
2021-06-10 11:40:25 +0200

    import more events

--- Source/Purchaseorderapi/Backend/index.ts
+++ Source/Purchaseorderapi/Backend/index.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 import { Host } from '@dolittle/vanir-backend';
 import { RegisterRoutes } from './routes';
 import { Client } from '@dolittle/sdk';
-import { PurchaseOrderLineUpdated } from './events';
-import { PurchaseOrderLineUpdatedHandler } from './eventHandlers';
+import { PurchaseOrderLineUpdated, FacilityCreated } from './events';
+import { PurchaseOrderLineUpdatedHandler, FacilityCreatedHandler } from './eventHandlers';
 import { TenantId } from '@dolittle/sdk.execution';
 // import { client } from './index';
 
@@ -21,17 +21,26 @@ const swaggerDoc = require('./swagger.json');
         console.log('CATCHED ERROR', err);
     }
 
-    // console.log('TEST2');
-    // const client = Client.forMicroservice('f33d02df-088a-6946-a450-37c87ed6dc9e')
-    //     .withEventTypes((eventTypes) => eventTypes.register(PurchaseOrderLineUpdated))
-    //     .withEventHandlers((builder) => builder.register(PurchaseOrderLineUpdatedHandler))
-    //     .build();
+    console.log('TEST2');
+    const client = Client.forMicroservice('1ff432ae-de10-403d-9a13-e66bba7d47f8')
+        .withEventTypes((eventTypes) => eventTypes.register(PurchaseOrderLineUpdated))
+        .withEventHandlers((builder) => builder.register(PurchaseOrderLineUpdatedHandler))
+        .build();
 
-    // const purchaseOrderLineUpdated = new PurchaseOrderLineUpdated(
-    //     'parham.barazesh',
-    //     '12345'
-    // );
-    // client.eventStore
-    //     .forTenant(TenantId.development)
-    //     .commit(purchaseOrderLineUpdated, '59d578b2-3c58-483f-8865-961b29bd8b36');
+    const purchaseOrderLineUpdated = new PurchaseOrderLineUpdated(
+        'parham.barazesh',
+        '12345'
+    );
+    client.eventStore
+        .forTenant(TenantId.development)
+        .commit(purchaseOrderLineUpdated, '59d578b2-3c58-483f-8865-961b29bd8b36');
+
+    const client2 = Client.forMicroservice('1ff432ae-de10-403d-9a13-e66bba7d47f8')
+        .withEventTypes((eventTypes) => eventTypes.register(FacilityCreated))
+        .withEventHandlers((builder) => builder.register(FacilityCreatedHandler))
+        .build();
+    const facilityCreated = new FacilityCreated('12345');
+    client2.eventStore
+        .forTenant(TenantId.development)
+        .commit(facilityCreated, '7495c578-3cbd-40ff-9434-d40044ccc7ff');
 })();
