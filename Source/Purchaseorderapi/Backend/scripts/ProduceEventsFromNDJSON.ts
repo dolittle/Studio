aa46ef08bd8cc66a5abc0e114bf18cbfdbf81426
Parham Barazesh <parham.barazesh@gmail.com>
2021-06-15 18:25:16 +0200

    In PurchaseOrderRequestedDateChanged event, SupplierReference changed to RequestedDate.

--- Source/Purchaseorderapi/Backend/scripts/ProduceEventsFromNDJSON.ts
+++ Source/Purchaseorderapi/Backend/scripts/ProduceEventsFromNDJSON.ts
@@ -7,14 +7,18 @@ interface ScriptArgs {
 }
 
 const argv = <ScriptArgs>yargs(process.argv.slice(2)).options({
-    inputFile: { type: 'string', demandOption: true, description: 'NDJSON file to produce events from' },
+    inputFile: {
+        type: 'string',
+        demandOption: true,
+        description: 'NDJSON file to produce events from',
+    },
 }).argv;
 
 const inputFileReadStream = fs.createReadStream(argv.inputFile);
 const rl = readline.createInterface(inputFileReadStream);
 const eventProducer = new EventProducer();
 
-rl.on('line', line => {
+rl.on('line', (line) => {
     const data = JSON.parse(line);
     const event = eventProducer.produce(data.payload);
     console.log(`${data.payload.document}: ${data.payload.operation}`, event);
--- Source/Purchaseorderapi/Backend/scripts/ReadNDJSON.ts
+++ Source/Purchaseorderapi/Backend/scripts/ReadNDJSON.ts
@@ -4,7 +4,7 @@ import readline from 'readline';
 const x = fs.createReadStream('po-example.ndjson');
 const rl = readline.createInterface(x);
 
-rl.on('line', line => {
+rl.on('line', (line) => {
     const data = JSON.parse(line);
     console.log(`${data.payload.document}: ${data.payload.operation}`);
 });
