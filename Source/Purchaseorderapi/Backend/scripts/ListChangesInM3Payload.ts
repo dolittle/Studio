297a7d03574e7ba2c3e6912a38c2cf1be24d0eb3
goeran <mail@goeran.no>
2021-06-15 23:56:05 +0200

    Script for listing changes in payload

---
+++ Source/Purchaseorderapi/Backend/scripts/ListChangesInM3Payload.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import readline from 'readline';
+import yargs from 'yargs/yargs';
+interface ScriptArgs {
+    inputFile: string;
+    position: number;
+}
+
+const argv = <ScriptArgs>yargs(process.argv.slice(2)).options({
+    inputFile: {
+        type: 'string',
+        demandOption: true,
+        description: 'NDJSON file to read',
+    },
+    position: {
+        type: 'number',
+        demandOption: true,
+        description: 'Position in NDJSON file'
+    }
+}).argv;
+
+const inputFileReadStream = fs.createReadStream(argv.inputFile);
+const rl = readline.createInterface(inputFileReadStream);
+
+let position = 0;
+rl.on('line', (line) => {
+    const data = JSON.parse(line);
+    if (position === argv.position) {
+        var changeList: any = [];
+        data.payload.elements.forEach((element) => {
+            if (element.oldvalue !== element.newvalue) {
+                changeList.push(`${element.name} (old: ${element.oldvalue}, new: ${element.newvalue})`);
+            }
+        });
+
+        console.log(changeList);
+        return;
+    }
+    position += 1;
+});
