4bb178bd7202c3ca294cc653506751def5b16a17
goeran <mail@goeran.no>
2021-06-18 13:56:28 +0200

    Producing events from webhooks payload

--- Source/Purchaseorderapi/Backend/scripts/ProduceEventsFromNDJSON.ts
+++ Source/Purchaseorderapi/Backend/scripts/ProduceEventsFromNDJSON.ts
@@ -5,8 +5,35 @@ import fs from 'fs';
 import readline from 'readline';
 import yargs from 'yargs/yargs';
 import { EventProducer } from '../eventsproducer';
+import { Client } from '@dolittle/sdk';
+import { TenantId } from '@dolittle/sdk.execution';
+import {
+    PurchaseOrderChangedNumberChanged,
+    PurchaseOrderCreated,
+    PurchaseOrderDateChanged,
+    PurchaseOrderDeleted,
+    PurchaseOrderDeliveryMethodChanged,
+    PurchaseOrderDeliveryTerms,
+    PurchaseOrderFacilityNumberChanged,
+    PurchaseOrderHighestStatusChanged,
+    PurchaseOrderInternalReferenceChanged,
+    PurchaseOrderLowestStatusChanged,
+    PurchaseOrderNetOrderValueChanged,
+    PurchaseOrderNumberOfPurchaseOrderLinesChanged,
+    PurchaseOrderOurInvoicingAddressChanged,
+    PurchaseOrderPaymentTermsChanged,
+    PurchaseOrderReferenceChanged,
+    PurchaseOrderRequestedDateChanged,
+    PurchaseOrderSupplierIdChanged,
+    PurchaseOrderSupplierReferenceChanged,
+    PurchaseOrderTotalOrderCostChanged,
+    PurchaseOrderTotalQuantityChanged
+} from '../events';
+import { isReturnStatement } from 'typescript';
+
 interface ScriptArgs {
     inputFile: string;
+    commitToRuntime: boolean;
 }
 
 const argv = <ScriptArgs>yargs(process.argv.slice(2)).options({
@@ -15,16 +42,67 @@ const argv = <ScriptArgs>yargs(process.argv.slice(2)).options({
         demandOption: true,
         description: 'NDJSON file to produce events from',
     },
+    commitToRuntime: { type: 'boolean', description: 'true if you want to produce events in Runtime' }
 }).argv;
 
 const inputFileReadStream = fs.createReadStream(argv.inputFile);
 const rl = readline.createInterface(inputFileReadStream);
 const eventProducer = new EventProducer();
 
+const client = Client
+    .forMicroservice('f2586326-1057-495b-a12b-7f45193b9402')
+    .withEventTypes(eventTypes => {
+        eventTypes.register(PurchaseOrderCreated);
+        eventTypes.register(PurchaseOrderDeleted);
+        eventTypes.register(PurchaseOrderFacilityNumberChanged);
+        eventTypes.register(PurchaseOrderLowestStatusChanged);
+        eventTypes.register(PurchaseOrderHighestStatusChanged);
+        eventTypes.register(PurchaseOrderSupplierIdChanged);
+        eventTypes.register(PurchaseOrderDeliveryMethodChanged);
+        eventTypes.register(PurchaseOrderReferenceChanged);
+        eventTypes.register(PurchaseOrderSupplierReferenceChanged);
+        eventTypes.register(PurchaseOrderRequestedDateChanged);
+        eventTypes.register(PurchaseOrderInternalReferenceChanged);
+        eventTypes.register(PurchaseOrderNetOrderValueChanged);
+        eventTypes.register(PurchaseOrderNumberOfPurchaseOrderLinesChanged);
+        eventTypes.register(PurchaseOrderTotalOrderCostChanged);
+        eventTypes.register(PurchaseOrderTotalQuantityChanged);
+        eventTypes.register(PurchaseOrderOurInvoicingAddressChanged);
+        eventTypes.register(PurchaseOrderDeliveryTerms);
+        eventTypes.register(PurchaseOrderPaymentTermsChanged);
+        eventTypes.register(PurchaseOrderDateChanged);
+        eventTypes.register(PurchaseOrderChangedNumberChanged);
+    })
+    .build();
+
 let counter = 0;
+const eventsToProduce: any[] = [];
 rl.on('line', (line) => {
     const data = JSON.parse(line);
     const events = eventProducer.produce(data.payload);
-    console.log(`${data.payload.document} (${counter}): ${data.payload.operation}`, events);
+
+    if (argv.commitToRuntime) {
+        // only support MPHEAD so far
+        if (data.part !== 'MPHEAD') {
+            return;
+        }
+
+        for (const e of events) {
+            eventsToProduce.push(e);
+        }
+    } else {
+        console.log(`${data.payload.document} (${counter}): ${data.payload.operation}`, events);
+    }
+
     counter += 1;
+});
+
+rl.on('close', async () => {
+    for (const event of eventsToProduce) {
+        console.log(event);
+        await client.eventStore
+            .forTenant(TenantId.development)
+            .commit(event, '3b9dfbaf-8592-46ab-95f0-7845926b2c23');
+    }
+    //console.log(eventsToProduce);
 });
