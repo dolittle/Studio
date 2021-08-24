89a1aa01c429ba1135facb15b47055bc0d1b7b20
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-08-24 08:38:23 +0200

    Make it work, seemingly

--- Source/SelfService/Web/index.tsx
+++ Source/SelfService/Web/index.tsx
@@ -3,16 +3,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { VersionInfo } from '@shared/web';
 
 const version = require('../microservice.json') as VersionInfo;
 
-import '@shared/styles/theme';
 import './index.scss';
 import { App } from './App';
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
     document.getElementById('root')
 );
