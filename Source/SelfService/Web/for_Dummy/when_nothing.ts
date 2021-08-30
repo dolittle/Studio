62c75145d2e11ebfef6e2a5972d27dd8b82cb38a
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-08-30 10:04:51 +0200

    Setup wallaby

--- Source/SelfService/Web/for_Dummy/when_nothing.ts
+++ Source/SelfService/Web/for_Dummy/when_nothing.ts
@@ -2,5 +2,4 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 describe('when nothing', () => {
     it('should not fail', () => true.should.be.true);
-
 });
