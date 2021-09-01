b0ff36cc607224ce90de45ff56b30096b9c56464
Parham Barazesh <parham.barazesh@gmail.com>
2021-09-01 23:00:04 +0200

    add different routes for Applications Controller

--- Source/SelfService/BackendMock/controllers/applications/ApplicationsController.ts
+++ Source/SelfService/BackendMock/controllers/applications/ApplicationsController.ts
@@ -4,24 +4,24 @@
 import { Request, Response, Router } from 'express';
 import { Logger } from 'winston';
 import { IController } from '../IController';
+import { allApplications } from './given';
 
 /**
  * A controller for handling requests related to applications.
  */
 export class ApplicationsController implements IController {
     readonly baseRoute = '/applications';
-    constructor(
-        protected readonly _logger: Logger
-    ) { }
+    constructor(protected readonly _logger: Logger) {}
 
     /** @inheritdoc */
     registerRoutes(router: Router) {
-        router.get(this.baseRoute, this.test.bind(this));
+        router.get(this.baseRoute, this.getAllApplications.bind(this));
     }
 
-    private test(req: Request, res: Response) {
-        return res.status(200).json({ message: 'Hello World' });
+    private getAllApplications(req: Request, res: Response) {
+        return res.status(200).json(allApplications);
     }
 }
 
-export const createApplicationsController = (logger: Logger) => new ApplicationsController(logger);
+export const createApplicationsController = (logger: Logger) =>
+    new ApplicationsController(logger);
