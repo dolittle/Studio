65aa6fe99f5bb75a61e2184804c88746e12b2908
Parham Barazesh <parham.barazesh@gmail.com>
2021-09-01 22:59:47 +0200

    add different routes for Application Controller

--- Source/SelfService/BackendMock/controllers/applications/ApplicationController.ts
+++ Source/SelfService/BackendMock/controllers/applications/ApplicationController.ts
@@ -4,32 +4,48 @@
 import { Request, Response, Router } from 'express';
 import { Logger } from 'winston';
 import { IController } from '../IController';
+import {
+    allMicroservicesInApplication,
+    environmentsAndMicroservicesInApplication,
+} from './given';
 
 /**
  * A controller for handling requests related to applications.
  */
 export class ApplicationController implements IController {
     readonly baseRoute = '/application';
-    constructor(
-        protected readonly _logger: Logger
-    ) { }
+    constructor(protected readonly _logger: Logger) {}
 
     /** @inheritdoc */
     registerRoutes(router: Router) {
         router.get(this.baseRoute, this.test.bind(this));
-        router.get(`${this.baseRoute}/:applicationID`, this.get.bind(this));
-        router.get(`${this.baseRoute}/:applicationID/microservices`, this.getAllMicroservices.bind(this));
+        router.get(
+            `${this.baseRoute}/:applicationID`,
+            this.getEnvironmentsAndMicroservices.bind(this)
+        );
+        router.get(
+            `${this.baseRoute}/:applicationID/microservices`,
+            this.getAllMicroservices.bind(this)
+        );
+        router.delete(
+            `${this.baseRoute}/:applicationID/environment/:environment/microservice/:microserviceid`,
+            this.deleteMicroservice.bind(this)
+        );
     }
 
     private test(req: Request, res: Response) {
         return res.status(200).json({ message: 'Hello World' });
     }
     private getAllMicroservices(req: Request, res: Response) {
-        return res.status(200).json({ microservice: 'your microservices' });
+        return res.status(200).send(allMicroservicesInApplication);
     }
-    private get(req: Request, res: Response) {
-        return res.status(200).json({ application: 'your appliction' });
+    private getEnvironmentsAndMicroservices(req: Request, res: Response) {
+        return res.status(200).json(environmentsAndMicroservicesInApplication);
+    }
+    private deleteMicroservice(req: Request, res: Response) {
+        return res.status(200).json({ message: 'Microservice Deleted' });
     }
 }
 
-export const createApplicationController = (logger: Logger) => new ApplicationController(logger);
+export const createApplicationController = (logger: Logger) =>
+    new ApplicationController(logger);
