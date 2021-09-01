ab760d100f4eaf3b6fc21b5d3db12c28ebed8500
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-09-01 11:27:13 +0200

    Add base for routes

---
+++ Source/SelfService/BackendMock/routes/Routes.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { Router } from 'express';
+import { Controller } from '../controllers/Controller';
+
+/**
+ * The API endpoint routes to register on an HTTP server.
+ */
+export class Routes {
+    constructor(private readonly _controller: Controller) { }
+
+    /**
+     * Registers the API routes on the provided router.
+     * @param router The {@link Router} to register routes on.
+     */
+    registerRoutes(router: Router) {
+        // router.get('/purchaseorders', this._controller.getAllBySupplierId.bind(this._controller));
+        // router.get(
+        //     '/purchaseorders/:orderNumber',
+        //     this._controller.getPurchaseOrderByOrderNumberAndSupplierId.bind(this._controller)
+        // );
+    }
+}
