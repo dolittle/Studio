1dc82283fa8d8a96f15172780c5cdbc75349bcc9
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-09-01 13:37:28 +0200

    Remove reflect-metadata import in mocharc
    Co-authored-by: Parham Barazesh <parham.barazesh@gmail.com>
    Co-authored-by: Jakob Høgenes <jakhog@users.noreply.github.com>

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-require('reflect-metadata');
-
 const chai = require('chai');
 global.expect = chai.expect;
 const should = chai.should();
