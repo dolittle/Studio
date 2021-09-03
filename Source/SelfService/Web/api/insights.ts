53565552354caf812085e06930ec3a4e9a963bc4
Parham Barazesh <parham.barazesh@gmail.com>
2021-09-03 13:02:34 +0200

    Continuing the todos. ongoing process.
    Co-authored-by: Sindre Alstad Wilting <woksin@users.noreply.github.com>
    Co-authored-by: Chris Williams <chris@dolittle.com>
    Co-authored-by: Petri <petriw@users.noreply.github.com>

--- Source/SelfService/Web/api/insights.ts
+++ Source/SelfService/Web/api/insights.ts
@@ -1,20 +1,20 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
 import { getServerUrlPrefix } from './api';
 // HACK for getting testdata
 //const testData = require('./testdata/insights-runtime-v1.json');
 
-export async function getRuntimeV1(applicationId: string, environment: string): Promise<any> {
+export async function getRuntimeV1(
+    applicationId: string,
+    environment: string
+): Promise<any> {
     //return testData as any;
     const url = `${getServerUrlPrefix()}/live/application/${applicationId}/environment/${environment.toLowerCase()}/insights/runtime-v1`;
-    const response = await fetch(
-        url,
-        {
-            method: 'GET',
-            mode: 'cors'
-        });
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+    });
 
     if (!response.ok) {
         console.error(response);
