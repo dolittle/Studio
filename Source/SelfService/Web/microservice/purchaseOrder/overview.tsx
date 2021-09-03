710ab57731ea25319ad9586fb81fef0d67eedf47
Parham Barazesh <parham.barazesh@gmail.com>
2021-09-03 09:12:16 +0200

    add some material UI components to PO API create page

--- Source/SelfService/Web/microservice/purchaseOrder/overview.tsx
+++ Source/SelfService/Web/microservice/purchaseOrder/overview.tsx
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import React from 'react';
 
+import TextField from '@material-ui/core/TextField';
+
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
@@ -13,6 +15,7 @@ import Typography from '@material-ui/core/Typography';
 
 // This doesnt load :(
 import logoInfor from '../../images/infor.png'; // with import
+import { Color } from '@material-ui/lab';
 import { Grid } from '@material-ui/core';
 
 type Props = {};
@@ -32,7 +35,7 @@ const useStyles = makeStyles((theme: Theme) =>
         resetContainer: {
             padding: theme.spacing(3),
         },
-    }),
+    })
 );
 
 export const Overview: React.FunctionComponent<Props> = (props) => {
@@ -43,36 +46,69 @@ export const Overview: React.FunctionComponent<Props> = (props) => {
     const steps = ['Select ERP system', 'Provide a name', 'Connect with ERP system'];
 
     const stepsContent = [
-        (
-            <>
-                <Typography>
-                    <p>Select the ERP system you have. Make sure you have access to the system for the next two steps.</p>
-                </Typography>
-                <Grid
-                    container
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                >
-                    <img src={logoInfor} />
-                </Grid>
-            </>
-        ),
-        (
-            <>
-                <Typography>
-                    <p>Establish a descriptive name for this microservice. A good example might be, “supplier purchase orders”. This can always be changed later. </p>
-                </Typography>
-            </>
-        ),
-
-        (
-            <>
-                <Typography>
-                    <p>The webhook endpoints are provided below. Each one will be established separately in program CMS045 in M3, however, the same username and password can be used for both endpoints.</p>
-                </Typography>
-            </>
-        ),
+        <>
+            <Typography>
+                <p>
+                    Select the ERP system you have. Make sure you have access to the
+                    system for the next two steps.
+                </p>
+            </Typography>
+            <Grid
+                container
+                direction='row'
+                justifyContent='flex-start'
+                alignItems='center'
+            >
+                <img src={logoInfor} />
+            </Grid>
+        </>,
+        <>
+            <Typography>
+                <p>
+                    Establish a descriptive name for this microservice. A good example
+                    might be, “supplier purchase orders”. This can always be changed
+                    later.{' '}
+                </p>
+                <form className={classes.root} noValidate autoComplete='off'>
+                    <TextField id='microserviceName' label='Name' variant='outlined' />
+                </form>
+            </Typography>
+        </>,
+        <>
+            <Typography>
+                <p>
+                    The webhook endpoints are provided below. Each one will be established
+                    separately in program CMS045 in M3, however, the same username and
+                    password can be used for both endpoints.
+                </p>
+                <p>Webhook for purchase order head (POHEAD)</p>
+                <TextField
+                    disabled
+                    id='standard-disabled'
+                    defaultValue='https://dazzling-fayman.dolittle.io/api/webhooks / m3/pohead'
+                    style={{ width: '30%' }}
+                />
+                <Button color='primary'>COPY TO CLIPBOARD</Button>
+                <p>Webhook for purchase order line (POLINE)</p>
+                <TextField
+                    disabled
+                    id='standard-disabled'
+                    defaultValue='https://dazzling-fayman.dolittle.io/api/webhooks / m3/poline'
+                    style={{ width: '30%' }}
+                />
+                <Button color='primary'>COPY TO CLIPBOARD</Button>
+                <p>Create username</p>
+                <TextField required id='outlined-required' variant='outlined' />
+                <p>Create Password</p>
+                <TextField
+                    id='outlined-password-input'
+                    type='password'
+                    autoComplete='current-password'
+                    variant='outlined'
+                />
+                <Button color='primary'>GENERATE AND COPY TO CLIPBOARD</Button>
+            </Typography>
+        </>,
     ];
 
     const handleNext = () => {
@@ -88,9 +124,8 @@ export const Overview: React.FunctionComponent<Props> = (props) => {
     };
 
     return (
-
         <div className={classes.root}>
-            <Stepper activeStep={activeStep} orientation="vertical">
+            <Stepper activeStep={activeStep} orientation='vertical'>
                 {steps.map((label, index) => (
                     <Step key={label}>
                         <StepLabel>{label}</StepLabel>
@@ -107,12 +142,14 @@ export const Overview: React.FunctionComponent<Props> = (props) => {
                                         Back
                                     </Button>
                                     <Button
-                                        variant="contained"
-                                        color="primary"
+                                        variant='contained'
+                                        color='primary'
                                         onClick={handleNext}
                                         className={classes.button}
                                     >
-                                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                        {activeStep === steps.length - 1
+                                            ? 'Finish'
+                                            : 'Next'}
                                     </Button>
                                 </div>
                             </div>
