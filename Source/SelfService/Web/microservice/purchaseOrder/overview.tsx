6cdc32632be54d9d1bcbfd0b55734cf50c1fa85e
Parham Barazesh <parham.barazesh@gmail.com>
2021-09-08 21:21:06 +0200

    add frontend components for creating purchase order api microservice

--- Source/SelfService/Web/microservice/purchaseOrder/overview.tsx
+++ Source/SelfService/Web/microservice/purchaseOrder/overview.tsx
@@ -13,9 +13,18 @@ import Typography from '@material-ui/core/Typography';
 
 // This doesnt load :(
 import logoInfor from '../../images/infor.png'; // with import
-import { Grid } from '@material-ui/core';
+import logoIFS from '../../images/ifs.png';
+import logoSAP from '../../images/sap.png';
 
-type Props = {};
+import { Grid } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import { classes } from '../../theme/theme';
+import LoopIcon from '@material-ui/icons/Loop';
+import '../purchaseOrder/purchaseorder.scss';
+
+type Props = {
+    // onNameChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+};
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -32,47 +41,112 @@ const useStyles = makeStyles((theme: Theme) =>
         resetContainer: {
             padding: theme.spacing(3),
         },
-    }),
+        inactiveText: {
+            color: 'grey',
+        },
+        progressBar: {
+            color: '#ff9366',
+        },
+    })
 );
 
 export const Overview: React.FunctionComponent<Props> = (props) => {
     const _props = props!;
-
+    // const onNameChange = _props.onNameChange;
     const classes = useStyles();
     const [activeStep, setActiveStep] = React.useState(0);
-    const steps = ['Select ERP system', 'Provide a name', 'Connect with ERP system'];
+    const steps = [
+        'Select ERP system',
+        'Provide a name',
+        'Configure ERP system',
+        'Wait for data',
+    ];
 
     const stepsContent = [
-        (
-            <>
-                <Typography>
-                    <p>Select the ERP system you have. Make sure you have access to the system for the next two steps.</p>
-                </Typography>
-                <Grid
-                    container
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                >
-                    <img src={logoInfor} />
-                </Grid>
-            </>
-        ),
-        (
-            <>
-                <Typography>
-                    <p>Establish a descriptive name for this microservice. A good example might be, “supplier purchase orders”. This can always be changed later. </p>
-                </Typography>
-            </>
-        ),
+        <>
+            <Typography>
+                <p>
+                    Select the ERP system you have. Make sure you have access to the
+                    system for the next two steps.
+                </p>
+            </Typography>
+            <Grid
+                container
+                direction='row'
+                justifyContent='flex-start'
+                alignItems='center'
+            >
+                <img src={logoInfor} />
+                <img src={logoIFS} />
+                <img src={logoSAP} />
+            </Grid>
+        </>,
+        <>
+            <Typography>
+                <p>
+                    Establish a descriptive name for this microservice. A good example
+                    might be, “supplier purchase orders”. This can always be changed
+                    later.
+                </p>
+                <form className={classes.root} noValidate autoComplete='off'>
+                    <TextField
+                        id='microserviceName'
+                        label='Name'
+                        variant='outlined'
+                        // className={classes.textField}
+                        // onChange={onNameChange}
+                    />
+                </form>
+            </Typography>
+        </>,
+        <>
+            <Typography>
+                <p>
+                    The webhook endpoints are provided below. Each one will be established
+                    separately in program CMS045 in M3, however, the same username and
+                    password can be used for both endpoints.
+                </p>
+                <p>Webhook for purchase order head (POHEAD)</p>
+                <span className={classes.inactiveText}>
+                    https://dazzling-fayman.dolittle.io/api/webhooks / m3/pohead
+                </span>
+                <Button color='primary'>COPY TO CLIPBOARD</Button>
 
-        (
-            <>
-                <Typography>
-                    <p>The webhook endpoints are provided below. Each one will be established separately in program CMS045 in M3, however, the same username and password can be used for both endpoints.</p>
-                </Typography>
-            </>
-        ),
+                <p>Webhook for purchase order line (POLINE)</p>
+                <span className={classes.inactiveText}>
+                    https://dazzling-fayman.dolittle.io/api/webhooks / m3/poline
+                </span>
+                <Button color='primary'>COPY TO CLIPBOARD</Button>
+
+                <p>Create username</p>
+                <TextField
+                    required
+                    id='outlined-required'
+                    label='Username'
+                    variant='outlined'
+                />
+                <p>Create Password</p>
+                <TextField
+                    id='outlined-password-input'
+                    type='password'
+                    label='Password'
+                    autoComplete='current-password'
+                    variant='outlined'
+                />
+                <Button color='primary'>GENERATE AND COPY TO CLIPBOARD</Button>
+            </Typography>
+        </>,
+        <>
+            <Typography>
+                <LoopIcon className={classes.progressBar} />
+                <span className='waitForData'>WAITING FOR DATA</span>
+                <span>
+                    It may take a few moments for data to start flowing. If studio cannot
+                    connect to your ERP system, try trouble-shooting.
+                </span>
+                <Button color='primary'>TROUBLESHOOT</Button>
+            </Typography>
+        </>,
     ];
 
     const handleNext = () => {
@@ -88,9 +162,8 @@ export const Overview: React.FunctionComponent<Props> = (props) => {
     };
 
     return (
-
         <div className={classes.root}>
-            <Stepper activeStep={activeStep} orientation="vertical">
+            <Stepper activeStep={activeStep} orientation='vertical'>
                 {steps.map((label, index) => (
                     <Step key={label}>
                         <StepLabel>{label}</StepLabel>
@@ -107,12 +180,14 @@ export const Overview: React.FunctionComponent<Props> = (props) => {
                                         Back
                                     </Button>
                                     <Button
-                                        variant="contained"
-                                        color="primary"
+                                        variant='contained'
+                                        color='primary'
                                         onClick={handleNext}
                                         className={classes.button}
                                     >
-                                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                        {activeStep === steps.length - 1
+                                            ? 'Finish'
+                                            : 'Next'}
                                     </Button>
                                 </div>
                             </div>
