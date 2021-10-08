eb09e42d729e25f4eb7388676aac7155d25e967b
freshteapot <chris@dolittle.com>
2021-10-08 09:58:47 +0200

    fixup: removing commented out rules

--- Source/Shared/Web/webpack/rules.ts
+++ Source/Shared/Web/webpack/rules.ts
@@ -12,34 +12,6 @@ export default [
             allowTsInNodeModules: true
         }
     },
-    //{
-    //    test: /\.s[ac]ss$/i,
-    //    issuer: /\.[tj]s[x]*$/i,
-    //    use: [
-    //        // Creates `style` nodes from JS strings
-    //        'style-loader',
-    //        // Translates CSS into CommonJS
-    //        'css-loader',
-    //        // Compiles Sass to CSS
-    //        'sass-loader',
-    //    ],
-    //    exclude: /\.module\.s[ac]ss$/i
-    //},
-    //{
-    //    test: /\.s[ac]ss$/i,
-    //    issuer: /\.[tj]s[x]*$/i,
-    //    use: [
-    //        // Creates `style` nodes from JS strings
-    //        'style-loader',
-    //        // Adds typescript declarations to the css modules and allows importing and using strongly typed scss modules in react components.
-    //        'css-modules-typescript-loader',
-    //        // Translates CSS into CommonJS
-    //        { loader: 'css-loader', options: { modules: true, importLoaders: 1 } },
-    //        // Compiles Sass to CSS
-    //        'sass-loader',
-    //    ],
-    //    include: /\.module\.s[ac]ss$/i
-    //},
     {
         test: /\.(png|gif|jpg|cur)$/i,
         type: 'asset/resource',
