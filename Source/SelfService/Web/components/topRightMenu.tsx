f16b494b8840c813ec60dad8bbb16795987052bb
freshteapot <chris@dolittle.com>
2021-11-26 10:25:32 +0100

    move to snackbar

--- Source/SelfService/Web/components/topRightMenu.tsx
+++ Source/SelfService/Web/components/topRightMenu.tsx
@@ -6,17 +6,15 @@ import React from 'react';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { useSnackbar } from 'notistack';
+
 import { ShortInfoWithEnvironment } from '../api/api';
 import { ApplicationsChanger } from '../application/applicationsChanger';
 import {
-    FormControl,
     IconButton,
     makeStyles,
-    MenuItem,
-    Select,
     Theme
 } from '@material-ui/core';
-import { useGlobalContext } from '../stores/notifications';
 import { createStyles } from '@material-ui/styles';
 import { SettingsMenu } from './settingsMenu';
 
@@ -35,42 +33,26 @@ const useStyles = makeStyles((theme: Theme) =>
         icon: {
             fill: 'white',
         },
-
-        formControl: {
-            marginRight: theme.spacing(3),
-            minWidth: 120,
-            float: 'left',
-        },
-
-        menuItem: {
-            justifyContent: 'flex-end',
-        },
     })
 );
 
 export const TopRightMenu: React.FunctionComponent<Props> = (props) => {
+    const { enqueueSnackbar } = useSnackbar();
     const classes = useStyles();
     const _props = props!;
-    const { setNotification } = useGlobalContext();
-
-    const items = [
-        <MenuItem className={classes.menuItem} key='changeCustomer' value='changeCustomer'>
-            Change Customer
-        </MenuItem>
-    ];
 
     return (
         <>
             <ApplicationsChanger applications={_props.applications} applicationId={_props.applicationId} environment={_props.environment} />
             <IconButton aria-label="notification" onClick={() => {
-                setNotification('TODO: Something with notifications', 'info');
+                enqueueSnackbar('TODO: Something with notifications', { variant: 'error' });
             }}
                 className={classes.root}
             >
                 <NotificationsIcon className={classes.icon} />
             </IconButton>
             <IconButton aria-label="account" onClick={() => {
-                setNotification('TODO: Something with account info', 'info');
+                enqueueSnackbar('TODO: Something with account info', { variant: 'error' });
             }}
                 className={classes.root}
             >
