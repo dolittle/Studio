9f2a707997745a75f91a277a22717a282675d2b9
Chris <chris@dolittle.com>
2022-02-22 16:28:34 +0100

    Removing unused files (#155)
    - Removing backend mocks
    - Removing old vanir stuff

--- Source/SelfService/BackendMock/configuration/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-import convict from 'convict';
-
-export { Configuration } from './Configuration';
-export { printHelpOrValidationError } from './Help';
