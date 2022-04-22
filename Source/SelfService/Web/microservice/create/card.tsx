4ed94ea0f875aaef5db47fff9b8d6f5cf6b558d5
Pavneet Singh Saund <pav.saund@gmail.com>
2022-04-22 08:39:11 +0200

    Step 5: styles - card

--- Source/SelfService/Web/microservice/create/card.tsx
+++ Source/SelfService/Web/microservice/create/card.tsx
@@ -1,44 +1,38 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import React from 'react';
-import { Theme } from '@mui/material/styles';
-import makeStyles from '@mui/styles/makeStyles';
-import createStyles from '@mui/styles/createStyles';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { aTagBackgroundColor } from '../../theme/viewCard';
-import { CardHeader, Avatar, IconButton } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CardHeader } from '@mui/material';
 import { red } from '@mui/material/colors';
 import { useGlobalContext } from '../../stores/notifications';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        media: {
-            height: 0,
-            paddingTop: '56.25%', // 16:9
-        },
-        expand: {
-            transform: 'rotate(0deg)',
-            marginLeft: 'auto',
-            transition: theme.transitions.create('transform', {
-                duration: theme.transitions.duration.shortest,
-            }),
-        },
-        expandOpen: {
-            transform: 'rotate(180deg)',
-        },
-        avatar: {
-            backgroundColor: red[500],
-        },
-        button: {
-            color: aTagBackgroundColor,
-        }
-    }),
-);
+const styles = {
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: (theme) => theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+    button: {
+        color: aTagBackgroundColor,
+    }
+};
 
 type Props = {
     kind: string
@@ -49,7 +43,6 @@ type Props = {
 };
 
 export const SimpleCard: React.FunctionComponent<Props> = (props) => {
-    const classes = useStyles();
     const { setNotification } = useGlobalContext();
     const name = props!.name;
     const description = props!.description;
@@ -89,7 +82,7 @@ export const SimpleCard: React.FunctionComponent<Props> = (props) => {
                 <Button size="small" onClick={() => {
                     setNotification('TODO: Learn more', 'info');
                 }}>Learn More</Button>
-                <Button className={classes.button} size="small" onClick={onClick}>Create</Button>
+                <Button sx={styles.button} size="small" onClick={onClick}>Create</Button>
             </CardActions>
         </Card>
     );
