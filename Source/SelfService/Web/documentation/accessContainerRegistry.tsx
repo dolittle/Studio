03c3580f97d84f1d8ff342312c7961b881ab1215
goeran <mail@goeran.no>
2022-05-03 15:39:36 +0200

    Code towards explaining how to login with docker login

--- Source/SelfService/Web/documentation/accessContainerRegistry.tsx
+++ Source/SelfService/Web/documentation/accessContainerRegistry.tsx
@@ -1,10 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import { Info } from '../stores/documentationInfo';
+
+import { getContainerRegistry } from '../api/cicd';
 
 type Vars = {
     acrId: string
@@ -29,6 +31,13 @@ az acr login -n ${vars.acrId} --subscription ${vars.subscriptionId}
 ~~~sh
 az acr repository list --name ${vars.acrId} -otable
 ~~~
+
+
+# Login to your registry in docker
+~~~sh
+docker login ${vars.acrId}.todo
+~~~
+
 `;
     return markdown.trim();
 };
@@ -41,11 +50,32 @@ export const Doc: React.FunctionComponent<Props> = (props) => {
     const _props = props!;
     const info = _props.info;
 
+    const [loaded, setLoaded] = useState(false);
+    const [containerRegistry, setContainerRegistry] = useState({});
+
+    useEffect(() => {
+        Promise.all([
+            getContainerRegistry(info.applicationId),
+        ]).then(values => {
+            setContainerRegistry(values[0]);
+            setLoaded(true);
+        });
+
+    }, []);
+
+    if (!loaded) {
+        return null;
+    }
+
+
     const vars = {
         acrId: info.containerRegistryName,
         subscriptionId: info.subscriptionId,
     } as Vars;
 
+    console.log(containerRegistry);
+    console.log(JSON.parse(atob(containerRegistry['.dockerconfigjson'])).auths);
+    // JSON.parse(atob(a[".dockerconfigjson"])).auths
     const data = template(vars);
     return (
         <ReactMarkdown remarkPlugins={[gfm]} >
