e568513b9b587ca67d16945ce63c4ba3f7734066
Pavneet Singh Saund <pav.saund@gmail.com>
2022-05-23 13:49:24 +0200

    Add some spacing to the logsScreen page

--- Source/SelfService/Web/theme/theme.ts
+++ Source/SelfService/Web/theme/theme.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { createTheme } from '@mui/material/styles';
+import { createTheme, PaletteOptions } from '@mui/material/styles';
 import { TypographyOptions } from '@mui/material/styles/createTypography';
 
 const typography: TypographyOptions = {
@@ -56,56 +56,57 @@ const typography: TypographyOptions = {
         fontSize: '0.625rem', //10px
     },
 };
-export const themeDark = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#8C9AF8',
-            light: '#b3bbfb',
-            dark: '#6678f6',
-            contrastText: '#191A21',
-        },
-        secondary: {
-            main: '#76E8DB',
-            light: '#A3EFE7',
-            dark: '#48E0CF',
-            contrastText: '#191A21',
-        },
-        background: {
-            default: '#0F1014',
-            paper: '#191A21',
-        },
-        text: {
-            disabled: '#93959f',
-            // hint: '#CECFD0',
-            primary: '#fafafa',
-            secondary: '#cecfd0',
-        },
-        error: {
-            main: '#F88C8C',
-            light: '#FBB3B3',
-            dark: '#F66666',
-            contrastText: '#191A21',
-        },
-        warning: {
-            main: '#FF9366',
-            light: '#FFB799',
-            dark: '#FF6F33',
-            contrastText: '#191A21',
-        },
-        info: {
-            main: '#8cd0f8',
-            light: '#A3D9F9',
-            dark: '#70a6c6',
-            contrastText: '#191A21',
-        },
-        success: {
-            main: '#76e8a2',
-            light: '#91EDB5',
-            dark: '#5EBA82',
-            contrastText: '#191A21',
-        },
-        divider: '#2C2B33',
+const darkPalette: PaletteOptions = {
+    mode: 'dark',
+    primary: {
+        main: '#8C9AF8',
+        light: '#b3bbfb',
+        dark: '#6678f6',
+        contrastText: '#191A21',
     },
+    secondary: {
+        main: '#76E8DB',
+        light: '#A3EFE7',
+        dark: '#48E0CF',
+        contrastText: '#191A21',
+    },
+    background: {
+        default: '#0F1014',
+        paper: '#191A21',
+    },
+    text: {
+        disabled: '#93959f',
+        // hint: '#CECFD0',
+        primary: '#fafafa',
+        secondary: '#cecfd0',
+    },
+    error: {
+        main: '#F88C8C',
+        light: '#FBB3B3',
+        dark: '#F66666',
+        contrastText: '#191A21',
+    },
+    warning: {
+        main: '#FF9366',
+        light: '#FFB799',
+        dark: '#FF6F33',
+        contrastText: '#191A21',
+    },
+    info: {
+        main: '#8cd0f8',
+        light: '#A3D9F9',
+        dark: '#70a6c6',
+        contrastText: '#191A21',
+    },
+    success: {
+        main: '#76e8a2',
+        light: '#91EDB5',
+        dark: '#5EBA82',
+        contrastText: '#191A21',
+    },
+    divider: '#2C2B33',
+};
+export const themeDark = createTheme({
+    palette: darkPalette,
     typography,
 });
