dde61a83c9be9cdda2372a59683898d57f4cc128
soumikgm <22228314+soumikgm@users.noreply.github.com>
2022-06-07 10:31:46 +0200

    remove code for button variants from app.tsx

--- Source/DesignSystemComponents/src/App.tsx
+++ Source/DesignSystemComponents/src/App.tsx
@@ -2,42 +2,11 @@ import React from 'react';
 import { Button } from './components/Button/Button';
 import { themeOptions as darkThemeOptions } from './muiDarkTheme';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material'
+import { CssBaseline } from '@mui/material';
 
 function App() {
-  const darkTheme = createTheme(darkThemeOptions)
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
-      <div className="filled">
-        <h2>Filled</h2>
-        <Button variant='filled' label='primary'/>
-        <Button variant='filled' label='secondary' color='secondary'/>
-        <Button variant='filled' label='error' color='error'/>
-        <Button variant='filled' label='warning' color='warning'/>
-        <Button variant='filled' label='info' color='info'/>
-        <Button variant='filled' label='success' color='success'/>
-      </div>
-      <div className="outlined">
-        <h2>Outlined</h2>
-        <Button variant='outlined' label='primary'/>
-        <Button variant='outlined' label='secondary' color='secondary'/>
-        <Button variant='outlined' label='error' color='error'/>
-        <Button variant='outlined' label='warning' color='warning'/>
-        <Button variant='outlined' label='info' color='info'/>
-        <Button variant='outlined' label='success' color='success'/>
-      </div>
-      <div className="text">
-        <h2>Text</h2>
-        <Button variant='text' label='primary'/>
-        <Button variant='text' label='secondary' color='secondary'/>
-        <Button variant='text' label='error' color='error'/>
-        <Button variant='text' label='warning' color='warning'/>
-        <Button variant='text' label='info' color='info'/>
-        <Button variant='text' label='success' color='success'/>
-      </div>
-    </ThemeProvider>
-  );
+    const darkTheme = createTheme(darkThemeOptions);
+    return <div>Nothing to see here.</div>;
 }
 
 export default App;
