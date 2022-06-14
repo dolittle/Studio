89a7d57c9162c0d703a1d59728ff755b12b33489
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-06-14 18:09:15 +0200

    Make shimmering lines appear while loading LogPanel

---
+++ Source/SelfService/Web/logging/shimmeringLogLines.tsx
@@ -0,0 +1,35 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { LogLine } from './logLine';
+
+const noopClickHandler = () => { };
+const shimmerLabels = {};
+const shimmerTimestamp = 0n;
+const shimmerLine = { sections: [], leading: { spaces: 0, tabs: 0 } };
+
+export type ShimmeringLogLinesProps = {
+    enableShowLineContextButton: boolean;
+};
+
+export const ShimmeringLogLines = (props: ShimmeringLogLinesProps) =>
+    <>
+        <LogLine
+            loading
+            enableShowLineContextButton={props.enableShowLineContextButton}
+            onClickShowLineContext={noopClickHandler}
+            labels={shimmerLabels}
+            timestamp={shimmerTimestamp}
+            line={shimmerLine}
+        />
+        <LogLine
+            loading
+            enableShowLineContextButton={props.enableShowLineContextButton}
+            onClickShowLineContext={noopClickHandler}
+            labels={shimmerLabels}
+            timestamp={shimmerTimestamp}
+            line={shimmerLine}
+        />
+    </>;
