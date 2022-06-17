28e029c41332b3e302311df3e03850c4ab5b1473
N00bG1rl <403helen@gmail.com>
2022-06-17 11:43:32 +0200

    Seperate LogContextDialog into its own component

--- Source/SelfService/Web/logging/logPanel.tsx
+++ Source/SelfService/Web/logging/logPanel.tsx
@@ -15,13 +15,7 @@ import {
     Paper,
     Switch,
     Typography,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
 } from '@mui/material';
-
-import { ButtonText } from '../theme/buttonText';
 
 import { ObservableLogLines, ObservablePartialLogLines } from './loki/logLines';
 import { DataLabels } from './loki/types';
@@ -29,57 +23,8 @@ import { DataLabels } from './loki/types';
 import { LogFilterObject } from './logFilter/logFilterPanel';
 import { ColoredLine } from './lineParsing';
 import { LogLines } from './logLines';
-import { LogsInRange, LoadMoreLogs } from './logsInRange';
-
-
-type LogContextDialogState = {
-    show: boolean;
-    application: string,
-    applicationId: string,
-    environment: string;
-    microservice: string,
-    microserviceId: string,
-    from: bigint,
-    to: bigint,
-};
-
-const LogContextDialog = (state: LogContextDialogState, setState: React.Dispatch<React.SetStateAction<LogContextDialogState>>, showTimestamp: boolean) =>
-    <Dialog open={state.show} maxWidth='xl' fullWidth scroll='paper' onClose={() => setState({ ...state, show: false })}>
-        <DialogTitle>Detailed view</DialogTitle>
-        <LogsInRange
-            applicationId={state.applicationId}
-            environment={state.environment}
-            filters={{ dateRange: { start: state.from, stop: state.to }, searchTerms: [], microservice: [{ id: state.microserviceId, name: state.microservice }] }}
-            from={state.from}
-            to={state.to}
-            firstFetchLimit={10}
-            moreFetchLimit={10}
-            render={(logs, loadMoreLogs) => (
-                <>
-                    <DialogContent>
-                        <LogLines
-                            logs={logs}
-                            showTimestamp={showTimestamp}
-                            sx={{
-                                '& > div:first-of-type': {
-                                    color: '#75e8db', // TODO: Where does this color come from?
-                                },
-                            }}
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <ButtonText
-                            onClick={() => setState({ ...state, show: false })}
-                        >Close</ButtonText>
-                        <ButtonText
-                            onClick={() => loadMoreLogs()}
-                        >Show more</ButtonText>
-                    </DialogActions>
-                </>
-            )}
-        />
-    </Dialog>;
-
+import { LoadMoreLogs } from './logsInRange';
+import { LogContextDialog, LogContextDialogState } from './logContexDialog';
 
 /**
  * The props for a {@link LogPanel} component.
@@ -134,22 +79,27 @@ export const LogPanel = (props: LogPanelProps) => {
         to: 0n,
     });
 
-    const handleOnClickShowLineContext = useCallback((timestamp: bigint, labels: DataLabels) =>
-        setLogContextDialog({
-            show: true,
-            application: labels.application,
-            applicationId: labels.application_id,
-            environment: labels.environment,
-            microservice: labels.microservice,
-            microserviceId: labels.microservice_id,
-            from: timestamp - 86_400_000_000_000n,
-            to: timestamp,
-        })
-        , [setLogContextDialog]);
+    const handleOnClickShowLineContext = useCallback(
+        (timestamp: bigint, labels: DataLabels) =>
+            setLogContextDialog({
+                show: true,
+                application: labels.application,
+                applicationId: labels.application_id,
+                environment: labels.environment,
+                microservice: labels.microservice,
+                microserviceId: labels.microservice_id,
+                from: timestamp - 86_400_000_000_000n,
+                to: timestamp,
+            }),
+        [setLogContextDialog]
+    );
 
-    const handleInViewChange = useCallback((visible: boolean) => {
-        if (visible && props.autoLoadMoreLogs === true) props.loadMoreLogs?.();
-    }, [props.autoLoadMoreLogs, props.loadMoreLogs]);
+    const handleInViewChange = useCallback(
+        (visible: boolean) => {
+            if (visible && props.autoLoadMoreLogs === true) props.loadMoreLogs?.();
+        },
+        [props.autoLoadMoreLogs, props.loadMoreLogs]
+    );
 
     if (props.logs.failed) {
         return (
@@ -157,7 +107,8 @@ export const LogPanel = (props: LogPanelProps) => {
                 <Grid item xs={12} md={6}>
                     <Alert severity='error' variant='outlined'>
                         <AlertTitle>Something went wrong</AlertTitle>
-                        Please try again later. If the problem persists, please <Link href='mailto:support@dolittle.com'>contact support</Link>.
+                        Please try again later. If the problem persists, please{' '}
+                        <Link href='mailto:support@dolittle.com'>contact support</Link>.
                     </Alert>
                 </Grid>
             </Grid>
@@ -179,15 +130,21 @@ export const LogPanel = (props: LogPanelProps) => {
     }
 
     const microservices =
-        props.filters.microservice === undefined || props.filters.microservice?.length === 0
+        props.filters.microservice === undefined ||
+        props.filters.microservice?.length === 0
             ? 'all Microservices'
             : props.filters.microservice?.length === 1
-                ? `${props.filters.microservice?.[0].name} Microservice`
-                : `${props.filters.microservice?.map((_) => _.name).join(', ')} Microservices`;
+            ? `${props.filters.microservice?.[0].name} Microservice`
+            : `${props.filters.microservice
+                  ?.map((_) => _.name)
+                  .join(', ')} Microservices`;
 
     const title = (
         <Typography variant='body2' color='textSecondary' fontStyle='italic' mt={1}>
-            Displaying <b>{props.filters.dateRange === 'live' ? 'live logs' : 'date range logs'}</b> for {props.application} Application, {props.environment} Environment, {microservices}
+            Displaying{' '}
+            <b>{props.filters.dateRange === 'live' ? 'live logs' : 'date range logs'}</b>{' '}
+            for {props.application} Application, {props.environment} Environment,{' '}
+            {microservices}
         </Typography>
     );
 
@@ -211,7 +168,11 @@ export const LogPanel = (props: LogPanelProps) => {
                                             control={
                                                 <Switch
                                                     checked={showTimestamp}
-                                                    onChange={(event) => setShowTimestamp(event.target.checked)}
+                                                    onChange={(event) =>
+                                                        setShowTimestamp(
+                                                            event.target.checked
+                                                        )
+                                                    }
                                                 />
                                             }
                                         />
@@ -222,7 +183,11 @@ export const LogPanel = (props: LogPanelProps) => {
                                             control={
                                                 <Switch
                                                     checked={showMicroservice}
-                                                    onChange={(event) => setShowMicroservice(event.target.checked)}
+                                                    onChange={(event) =>
+                                                        setShowMicroservice(
+                                                            event.target.checked
+                                                        )
+                                                    }
                                                 />
                                             }
                                         />
@@ -236,7 +201,9 @@ export const LogPanel = (props: LogPanelProps) => {
                             showContextButton
                             showTimestamp={showTimestamp}
                             showMicroservice={showMicroservice}
-                            showDateRangeHeaderAndFooter={props.filters.dateRange !== 'live'}
+                            showDateRangeHeaderAndFooter={
+                                props.filters.dateRange !== 'live'
+                            }
                             onClickShowContextButton={handleOnClickShowLineContext}
                         />
                         <InView
