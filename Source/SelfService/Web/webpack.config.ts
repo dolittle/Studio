b377f00685477835a1fa026443e8884e811fd290
N00bG1rl <403helen@gmail.com>
2022-06-21 11:27:30 +0200

    Update studio header information

--- Source/SelfService/Web/webpack.config.ts
+++ Source/SelfService/Web/webpack.config.ts
@@ -3,28 +3,28 @@
 
 const webpack = require('@shared/web').webpack;
 module.exports = (env, argv) => {
-    return webpack(env, argv, '/selfservice/', config => {
-        config.devServer.proxy = {
-            '/selfservice/api': {
-                target: 'http://localhost:3007',
-                pathRewrite: { '^/selfservice/api': '' },
-            },
-            '/api/system/monitoring/metrics/v1/': {
-                target: 'http://localhost:8801',
-                pathRewrite: { '^/api/system/monitoring/metrics/v1/': '/prometheus/api/v1/' },
-                headers: {
-                    'x-scope-orgid': 'tenant-453e04a7-4f9d-42f2-b36c-d51fa2c83fa3'
-                }
-            },
-            '/api/system/monitoring/logs/v1/': {
-                target: 'http://localhost:8802',
-                pathRewrite: { '^/api/system/monitoring/logs/v1/': '/loki/api/v1/' },
-                headers: {
-                    'x-scope-orgid': 'tenant-453e04a7-4f9d-42f2-b36c-d51fa2c83fa3'
-                },
-                ws: true,
-            },
-        };
-        config.devServer.before = (app, server, compiler) => { };
-    }, 9007, 'Studio');
+  return webpack(env, argv, '/selfservice/', config => {
+    config.devServer.proxy = {
+      '/selfservice/api': {
+        target: 'http://localhost:3007',
+        pathRewrite: { '^/selfservice/api': '' },
+      },
+      '/api/system/monitoring/metrics/v1/': {
+        target: 'http://localhost:8801',
+        pathRewrite: { '^/api/system/monitoring/metrics/v1/': '/prometheus/api/v1/' },
+        headers: {
+          'x-scope-orgid': 'tenant-453e04a7-4f9d-42f2-b36c-d51fa2c83fa3'
+        }
+      },
+      '/api/system/monitoring/logs/v1/': {
+        target: 'http://localhost:8802',
+        pathRewrite: { '^/api/system/monitoring/logs/v1/': '/loki/api/v1/' },
+        headers: {
+          'x-scope-orgid': 'tenant-453e04a7-4f9d-42f2-b36c-d51fa2c83fa3'
+        },
+        ws: true,
+      },
+    };
+    config.devServer.before = (app, server, compiler) => { };
+  }, 9007, 'Dolittle Studio');
 };
