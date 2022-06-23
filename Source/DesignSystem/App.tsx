cc5deb49016a712f1e13037aff578e6a9e03410c
soumikgm <22228314+soumikgm@users.noreply.github.com>
2022-06-23 17:18:29 +0200

    return an empty component from App.tsx

--- Source/DesignSystem/App.tsx
+++ Source/DesignSystem/App.tsx
@@ -3,8 +3,8 @@ import { themeOptions as darkThemeOptions } from './muiDarkTheme';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
-    const darkTheme = createTheme(darkThemeOptions);
-    return <div>Nothing to see here.</div>;
+  const darkTheme = createTheme(darkThemeOptions);
+  return <></>;
 }
 
 export default App;
