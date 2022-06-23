bfcea79ffb8f7e29fb96108ec9afa9a6e1363687
soumikgm <22228314+soumikgm@users.noreply.github.com>
2022-06-23 14:36:08 +0200

    remove unused imports from app.tsx

--- Source/DesignSystem/src/App.tsx
+++ Source/DesignSystem/src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Button } from './components/Button/Button';
 import { themeOptions as darkThemeOptions } from './muiDarkTheme';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
 
 function App() {
     const darkTheme = createTheme(darkThemeOptions);
