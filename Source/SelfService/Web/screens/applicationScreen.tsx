d312834b72b2e85de958f4987472db14444d8f37
Soumik Ghosh Moulic <22228314+soumikgm@users.noreply.github.com>
2022-07-08 17:32:18 +0200

    Trigger CI build by deleting a comma

--- Source/SelfService/Web/screens/applicationScreen.tsx
+++ Source/SelfService/Web/screens/applicationScreen.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React, { useEffect, useState } from 'react';
-import { Route, Switch, } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 
 import { ShortInfoWithEnvironment } from '../api/api';
