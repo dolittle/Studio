d45cb956c1130d12eae3b97e84344409b9773b39
N00bG1rl <403helen@gmail.com>
2022-08-08 10:29:15 +0300

    Fixed menu item styles

--- Source/SelfService/Web/logging/logFilter/filterSelect.tsx
+++ Source/SelfService/Web/logging/logFilter/filterSelect.tsx
@@ -2,7 +2,34 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+
+import { themeDark } from '../../theme/theme';
 import { Select, SelectProps } from '@mui/material';
+
+const styles = {
+    'padding': '5px 14px',
+    'borderRadius': '4px',
+    'transition': 'all .3s',
+    '&:before, &:hover:not(.Mui-disabled):before, &:after': {
+        border: 'none',
+    },
+    ':hover': {
+        'background': 'rgba(140, 154, 248, 0.08)',
+        '& svg': {
+            color: themeDark.palette.primary.main
+        }
+    },
+    'typography': 'button',
+    '& svg': {
+        right: '5px'
+    },
+    '.MuiSelect-select:focus': {
+        backgroundColor: 'transparent'
+    },
+    '.MuiSelect-iconOpen': {
+        color: themeDark.palette.primary.main
+    },
+};
 
 export type FilterSelectProps<T> = SelectProps<T>;
 
@@ -12,9 +39,6 @@ export const FilterSelect = <T,>(props: FilterSelectProps<T>) =>
         variant='standard'
         sx={{
             ...props.sx,
-            '&:before, &:hover:not(.Mui-disabled):before, &:after': {
-                border: 'none',
-            },
-            'typography': 'button',
+            ...styles
         }}
     />;
