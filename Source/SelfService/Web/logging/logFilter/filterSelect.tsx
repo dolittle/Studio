0ed85ae86211363abd9a651f14f5f4d437f85ffd
N00bG1rl <403helen@gmail.com>
2022-08-12 14:06:02 +0300

    Code review changes

--- Source/SelfService/Web/logging/logFilter/filterSelect.tsx
+++ Source/SelfService/Web/logging/logFilter/filterSelect.tsx
@@ -3,42 +3,42 @@
 
 import React from 'react';
 
-import { themeDark } from '../../theme/theme';
-import { Select, SelectProps } from '@mui/material';
+import { Button, Select, SelectProps, Theme } from '@mui/material';
 
 const styles = {
-    'padding': '5px 14px',
-    'borderRadius': '4px',
-    'transition': 'all .3s',
-    '&:before, &:hover:not(.Mui-disabled):before, &:after': {
-        border: 'none',
-    },
-    ':hover': {
-        'background': 'rgba(140, 154, 248, 0.08)',
+    wrapper: {
+        '.MuiSelect-select:focus': {
+            backgroundColor: 'transparent'
+        },
+        '.MuiSelect-iconOpen': {
+            color: (theme: Theme) => theme.palette.primary.main
+        },
         '& svg': {
-            color: themeDark.palette.primary.main
-        }
+            right: '-0.3125rem'
+        },
+        '&:before, &:hover:not(.Mui-disabled):before, &:after': {
+            border: 'none',
+        },
+        ':hover': {
+            '& svg': {
+                color: (theme: Theme) => theme.palette.primary.main
+            }
+        },
     },
-    'typography': 'button',
-    '& svg': {
-        right: '5px'
-    },
-    '.MuiSelect-select:focus': {
-        backgroundColor: 'transparent'
-    },
-    '.MuiSelect-iconOpen': {
-        color: themeDark.palette.primary.main
-    },
+    inputField: {
+        textDecoration: 'none',
+        typography: 'button'
+    }
 };
 
 export type FilterSelectProps<T> = SelectProps<T>;
 
 export const FilterSelect = <T,>(props: FilterSelectProps<T>) =>
-    <Select
-        {...props}
-        variant='standard'
-        sx={{
-            ...props.sx,
-            ...styles
-        }}
-    />;
+    <Button variant='text' disableRipple sx={styles.wrapper}>
+        <Select
+            {...props}
+            variant='standard'
+            disableUnderline
+            sx={{ ...styles.inputField, ...props.sx }}
+        />
+    </Button>;
