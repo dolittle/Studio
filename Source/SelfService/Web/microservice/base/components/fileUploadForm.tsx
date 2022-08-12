0b400f1479bf964b516823f5165547a599064802
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-08-12 16:43:25 +0200

    Some small last cleanups

--- Source/SelfService/Web/microservice/base/components/fileUploadForm.tsx
+++ Source/SelfService/Web/microservice/base/components/fileUploadForm.tsx
@@ -22,9 +22,8 @@ export type FileUploadFormRef = {
  * @param props
  * @returns
  */
-// eslint-disable-next-line react/display-name
 export const FileUploadForm = React.forwardRef<FileUploadFormRef, FileUploadFormProps>(
-    (props: FileUploadFormProps, ref: React.ForwardedRef<FileUploadFormRef>) => {
+    function FileUploadForm(props: FileUploadFormProps, ref: React.ForwardedRef<FileUploadFormRef>) {
         const formRef = useRef<HTMLFormElement>(null);
         const fileInputRef = useRef<HTMLInputElement>(null);
 
