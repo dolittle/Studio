6be989314066af9bc8cf5fe595595f3669b47c82
N00bG1rl <403helen@gmail.com>
2022-08-16 18:24:25 +0300

    Removed unnecessary index.scss import

--- Source/SelfService/Web/index.tsx
+++ Source/SelfService/Web/index.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.scss';
 import { App } from './App';
 
 const container = document.getElementById('root');
