5fa2aa1c437bf54925983f3ebe9e0cf9945e3739
Elias <elias@aux.dk>
2022-09-21 13:42:53 +0200

    Add Summary implentation

--- Source/DesignSystem/atoms/Metrics/Summary.stories.tsx
+++ Source/DesignSystem/atoms/Metrics/Summary.stories.tsx
@@ -8,14 +8,15 @@ import { Summary } from './Summary';
 export default {
     title: 'Metrics/Summary',
     component: Summary,
-}
+};
 
 const Template: ComponentStory<typeof Summary> = (props) => <Summary {...props} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
-    now: 10.1,
-    avg: 12,
+    now: { value: 10.1123456789774261321, tooltip: 'hello now' },
+    avg: { value: 12 },
     max: 87,
-    tooltip: "Some tooltip"
+    digits: 2,
+    unit: '%'
 };
--- Source/DesignSystem/atoms/Metrics/Summary.tsx
+++ Source/DesignSystem/atoms/Metrics/Summary.tsx
@@ -1,25 +1,37 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import React from 'react';
+import Tooltip from '@mui/material/Tooltip';
 
-export type Summary = {
-    label: string
-    render: () => React.ReactNode
-    sx?: any
+const number = (summary: SummaryValue, digits?: number) => {
+    const value = typeof summary === 'number' ? summary : summary.value;
+    const formatted = digits !== undefined ? value.toFixed(digits) : value.toString();
+
+    if (typeof summary === 'number' || summary.tooltip === undefined) {
+        return <span>{formatted}</span>;
+    }
+
+    return (
+        <Tooltip title={summary.tooltip}>
+            <span>{formatted}</span>
+        </Tooltip>
+    );
 };
 
+type SummaryValue = number | { value: number, tooltip?: string };
+
 export type SummaryProps = {
-    now: number
-    avg: number
-    max: number
-    tooltip: string
-    sx?: any
+    now: SummaryValue
+    avg: SummaryValue
+    max: SummaryValue
+    digits?: number;
+    unit?: string
 };
 
 export const Summary = (props: SummaryProps) => {
     return (
         <>
-            "hello from Summary"
+            { number(props.now, props.digits) } | { number(props.avg, props.digits) } | { number(props.max, props.digits) } {props.unit ?? ''}
         </>
     );
 };
