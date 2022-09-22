99edaf3658af7299709cc0fc9f31edb97280c27c
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-09-22 10:17:46 +0200

    Update rest of stories

--- Source/DesignSystem/atoms/Metrics/Summary.stories.tsx
+++ Source/DesignSystem/atoms/Metrics/Summary.stories.tsx
@@ -1,19 +1,15 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { componentStories } from '@dolittle/design-system';
+
 import { Summary } from './Summary';
 
-export default {
-    title: 'Metrics/Summary',
-    component: Summary,
-};
+const { metadata, createStory } = componentStories(Summary);
 
-const Template: ComponentStory<typeof Summary> = (props) => <Summary {...props} />;
+export default metadata;
 
-export const Normal = Template.bind({});
-Normal.args = {
+export const Normal = createStory({
     now: 10.1123456789774261321,
     avg: 12,
     max: 87,
@@ -21,13 +17,11 @@ Normal.args = {
     description: 'CPU usage',
     period: 'from last restart',
     unit: '%',
-};
+});
 
-export const Default = Template.bind({});
-Default.args = {
+export const Default = createStory({
     now: 10.1123456789774261321,
     avg: 12,
     max: 87,
     period: 'last 24h',
-};
-
+});
