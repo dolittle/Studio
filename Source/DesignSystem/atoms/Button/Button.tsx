66158ecbf340974ab7da59e7d82d5b617380756c
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-09-22 10:01:00 +0200

    Fix props for Button Component
    It wasn't handling clicks.

--- Source/DesignSystem/atoms/Button/Button.tsx
+++ Source/DesignSystem/atoms/Button/Button.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import { Button as MuiButton, SxProps } from '@mui/material';
 
 type ButtonProps = {
@@ -13,33 +13,20 @@ type ButtonProps = {
     startWithIcon?: any;
     endWithIcon?: any;
     sx?: SxProps;
-    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-export const Button = (props: ButtonProps) => {
-
-    const _props = props!;
-    const variant = _props.variant === 'filled' ? 'contained' : _props.variant ?? 'contained';
-    const disabled = _props.disabled ?? false;
-    const label = _props.label ?? 'Button';
-    const color = _props.color ?? 'primary';
-    const size = _props.size ?? 'small';
-    const startWithIcon = _props.startWithIcon;
-    const endWithIcon = _props.endWithIcon;
-    const sx = _props.sx;
-
-    return (
-        <MuiButton
-            variant={variant}
-            disabled={disabled}
-            color={color}
-            size={size}
-            startIcon={startWithIcon}
-            endIcon={endWithIcon}
-            sx={sx}
-            disableElevation
-        >
-            {label}
-        </MuiButton>
-    );
-};
+export const Button = (props: ButtonProps) =>
+    <MuiButton
+        variant={props.variant === 'filled' ? 'contained' : props.variant}
+        disabled={props.disabled}
+        color={props.color}
+        size={props.size ?? 'small'}
+        startIcon={props.startWithIcon}
+        endIcon={props.endWithIcon}
+        sx={props.sx}
+        onClick={props.onClick}
+        disableElevation
+    >
+        {props.label}
+    </MuiButton>;
