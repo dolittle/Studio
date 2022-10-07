25025d9652b5d62f17238fbefc6ce9274bc01447
N00bG1rl <403helen@gmail.com>
2022-10-07 14:09:38 +0300

    Replaced Theme tabs with Storybook

--- Source/SelfService/Web/microservice/purchaseOrder/create.tsx
+++ Source/SelfService/Web/microservice/purchaseOrder/create.tsx
@@ -1,21 +1,21 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { useSnackbar } from 'notistack';
-
 import { Grid, Button, Typography } from '@mui/material';
-import { TabPanel } from '../../utils/materialUi';
 
 import { Guid } from '@dolittle/rudiments';
+import { Tabs } from '@dolittle/design-system/atoms/Tabs/Tabs';
 
 import { savePurchaseOrderMicroservice } from '../../stores/microservice';
 import { HttpResponseApplication } from '../../api/application';
 import { MicroservicePurchaseOrder } from '../../api/index';
 import { getLatestRuntimeInfo } from '../../api/api';
 import { Configuration } from './configuration';
-import { Tab, Tabs } from '../../theme/tabs';
+
+import { Notification } from '../../theme/Notification';
 
 type CreateProps = {
     application: HttpResponseApplication;
@@ -24,7 +24,6 @@ type CreateProps = {
 
 export const Create = ({ application: { id, customerId }, environment }: CreateProps) => {
     const history = useHistory();
-    const { enqueueSnackbar } = useSnackbar();
 
     const microserviceId = Guid.create().toString();
     const headImage = 'dolittle/integrations-m3-purchaseorders:latest';
@@ -51,8 +50,9 @@ export const Create = ({ application: { id, customerId }, environment }: CreateP
         },
     };
 
-    const handleChange = () => {
-        enqueueSnackbar('Health Status is only available after microservice is created.', { variant: 'error' });
+    const handleCancel = () => {
+        const href = `/microservices/application/${id}/${environment}/create`;
+        history.replace(href);
     };
 
     const _onSave = (ms: MicroservicePurchaseOrder): void => {
@@ -64,46 +64,25 @@ export const Create = ({ application: { id, customerId }, environment }: CreateP
     };
 
     return (
-        <Grid container direction='column' justifyContent='flex-start'>
-            <Typography variant='h1' my={2}>Create purchase order API</Typography>
-
-            <Grid container spacing={3}>
-                <Grid item xs={10}>
-                    <Tabs
-                        value={0}
-                        onChange={handleChange}
-                    >
-                        <Tab label='Configuration' />
-                        <Tab label='Health Status' />
-                    </Tabs>
-                </Grid>
-
-                <Grid
-                    item xs={2}
-                >
-                    <Grid
-                        container
-                        direction='row'
-                        justifyContent='space-around'
-                        alignItems='flex-end'
-                    >
-                        <Button
-                            onClick={() => {
-                                const href = `/microservices/application/${id}/${environment}/create`;
-                                history.replace(href);
-                            }}
-                        >
-                            cancel
-                        </Button>
-                    </Grid>
-                </Grid>
+        <>
+            <Grid container direction='column'>
+                <Typography variant='h1' my={2}>Create purchase order API</Typography>
+                <Button sx={{ alignSelf: 'flex-end' }} onClick={handleCancel}>Cancel</Button>
             </Grid>
 
-
-
-            <TabPanel value={0} index={0}>
-                <Configuration onSave={_onSave} microservice={ms} />
-            </TabPanel>
-        </Grid >
+            <Tabs
+                sx={{ mb: 3.25 }}
+                tabs={[
+                    {
+                        label: 'Configuration',
+                        render: () => <Configuration onSave={_onSave} microservice={ms} />,
+                    },
+                    {
+                        label: 'Health Status',
+                        render: () => <Notification title='Health Status is only available after microservice is created.' />,
+                    }
+                ]}
+            />
+        </>
     );
 };
