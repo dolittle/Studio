1ea14dfdca8c01e95ab3f24eeff66d0f64461665
N00bG1rl <403helen@gmail.com>
2022-10-12 20:21:44 +0300

    Added LoadingSpinner component to Storybook

---
+++ Source/DesignSystem/atoms/LoadingSpinner/LoadingSpinner.stories.tsx
@@ -0,0 +1,12 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { componentStories } from '@dolittle/design-system';
+
+import { LoadingSpinner } from './LoadingSpinner';
+
+const { metadata, createStory } = componentStories(LoadingSpinner);
+
+export default metadata;
+
+export const Loading = createStory({});
