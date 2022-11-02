583352fb38914b8c40c5a42b5a01495122ea3dfa
N00bG1rl <403helen@gmail.com>
2022-11-02 16:25:22 +0200

    Removed state from reusable Select comp

--- Source/DesignSystem/atoms/Forms/Select.tsx
+++ Source/DesignSystem/atoms/Forms/Select.tsx
@@ -1,7 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useState } from 'react';
+import React from 'react';
+
 import { MenuItem, TextField, TextFieldProps } from '@mui/material';
 
 /**
@@ -11,29 +12,22 @@ export type SelectProps = {
     options: any;
 } & TextFieldProps;
 
-export const Select = (props: SelectProps) => {
-    const [selectedValue, setSelectedValue] = useState(props.value ?? '');
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSelectedValue(event.target.value);
-    };
-
-    return (
-        <TextField
-            id={props.id}
-            select
-            label={props.label}
-            value={selectedValue}
-            size='small'
-            onChange={handleChange}
-            sx={props.sx}
-            disabled={props.disabled}
-        >
-            {props.options.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                    {option.value}
-                </MenuItem>
-            ))}
-        </TextField>
-    );
-};
+export const Select = (props: SelectProps) => (
+    <TextField
+        id={props.id}
+        select
+        value={props.value}
+        label={props.label}
+        disabled={props.disabled}
+        onChange={props.onChange}
+        variant="outlined"
+        size='small'
+        sx={props.sx}
+    >
+        {props.options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+                {option.value}
+            </MenuItem>
+        ))}
+    </TextField>
+);
