443a01a796adfd4a9846454140e9d4a5f403ad4c
N00bG1rl <403helen@gmail.com>
2022-11-15 21:46:09 +0200

    Removed unused code

--- Source/SelfService/Web/microservice/microserviceView/configuration/configuration.tsx
+++ Source/SelfService/Web/microservice/microserviceView/configuration/configuration.tsx
@@ -1,35 +1,21 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { useSnackbar } from 'notistack';
-
-import {
-    deleteConfigFile,
-    getConfigFilesNamesList,
-    getServerUrlPrefix,
-    IngressURLWithCustomerTenantID,
-    SimpleIngressPath,
-    updateConfigFiles
-} from '../../../api/api';
+import { getServerUrlPrefix } from '../../../api/api';
 import { HttpResponseApplication } from '../../../api/application';
 
 import { MicroserviceStore } from '../../../stores/microservice';
 
 import { Box, Divider, Grid, Typography } from '@mui/material';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 import { ButtonText } from '../../../theme/buttonText';
 import { DownloadButton } from '../../../theme/downloadButton';
-import { SelectFileConfirmationModal } from '../../../theme/selectFileConfirmationModal';
 import { DownloadButtons } from '../../components/downloadButtons';
 
-import ConfigFilesTable from '../../base/components/configFilesTable';
-import { ConfigView } from '../../base/configView';
 import { LiveIngressView } from '../../base/liveIngressView';
-import { FileUploadForm, FileUploadFormRef } from '../../base/components/fileUploadForm';
 
 import { SetupSection } from './setupSection';
 import { FilesSection } from './filesSection';
@@ -48,84 +34,13 @@ const styles = {
     }
 };
 
-const MAX_CONFIGMAP_ENTRY_SIZE = 3145728;
-
 export const Configuration = ({
     application, applicationId, environment, microserviceId, currentMicroservice }: ConfigurationProps) => {
 
     const history = useHistory();
 
-    const [filesNamesList, setFilesNamesList] = useState<string[]>([]);
-    const [configFileModalVisibility, setConfigFileModalVisibility] = useState<boolean>(false);
-
-    const [file, setFile] = useState<File>(new File([], ''));
-    const [validFile, setValidFile] = useState<boolean>(false);
-    const { enqueueSnackbar } = useSnackbar();
-
     // This is reused. consider moving
     const configMapPrefix = `${environment.toLowerCase()}-${currentMicroservice.name.toLowerCase()}`;
-
-    const fileUploadRef = useRef<FileUploadFormRef>(null);
-
-    useEffect(() => {
-        fetchConfigFilesNamesList()
-            .catch(console.error);
-    }, []);
-
-    const fetchConfigFilesNamesList = async () => {
-        const result = await getConfigFilesNamesList(applicationId, environment, microserviceId);
-
-        if (!result.data) {
-            enqueueSnackbar(`Could not fetch config files`, { variant: 'error', persist: false });
-            return;
-        }
-
-        setFilesNamesList(result.data);
-    };
-
-    const deleteFileFromMicroservice = async (fileName: string) => {
-        if (!fileName) {
-            enqueueSnackbar('filename not valid', { variant: 'error', persist: false });
-            return;
-        }
-
-        if (confirm(`Are you sure you want to delete ${fileName}?`)) {
-            await deleteConfigFile(applicationId, environment, microserviceId, fileName);
-
-            fetchConfigFilesNamesList()
-                .catch(console.error);
-        }
-    };
-
-    const sizeValidation = (file): boolean => {
-        if (file.size > MAX_CONFIGMAP_ENTRY_SIZE) {
-            setValidFile(false);
-            enqueueSnackbar(`file cannot be larger than ${MAX_CONFIGMAP_ENTRY_SIZE} bytes. Please select another file`, { variant: 'error', persist: false });
-            return false;
-        }
-
-        setValidFile(true);
-        return true;
-    };
-
-    const validateAndShowConfirmationDialog = (file: File) => {
-        sizeValidation(file);
-        setFile(file);
-        setConfigFileModalVisibility(true);
-    };
-
-    const saveConfigFile = async (formData: FormData) => {
-        const upsert = await updateConfigFiles(applicationId, environment, microserviceId, formData);
-
-        if (upsert.success === false) {
-            enqueueSnackbar(upsert.error, { variant: 'error', persist: false });
-            setValidFile(false);
-        } else {
-            fetchConfigFilesNamesList();
-        }
-
-        setConfigFileModalVisibility(false);
-    };
 
     const handleEnvironmentClick = async () => {
         const href = `/microservices/application/${applicationId}/${environment}/view/${microserviceId}/environment-variables`;
@@ -134,15 +49,6 @@ export const Configuration = ({
 
     return (
         <>
-            <SelectFileConfirmationModal
-                open={configFileModalVisibility}
-                disableAdd={!validFile}
-                fileSize={file.size}
-                fileName={file.name}
-                onCancel={() => setConfigFileModalVisibility(false)}
-                onAdd={() => fileUploadRef.current?.confirmSelectedFile()}
-            />
-
             <SetupSection
                 application={application}
                 applicationId={applicationId}
@@ -154,7 +60,7 @@ export const Configuration = ({
             <FilesSection
                 applicationId={applicationId}
                 environment={environment}
-                currentMicroservice={currentMicroservice}
+                microserviceName={currentMicroservice.name}
                 microserviceId={microserviceId}
             />
 
@@ -184,15 +90,6 @@ export const Configuration = ({
 
                 <Grid container spacing={3}>
                     <Grid item>
-                        <ButtonText
-                            startIcon={<AddCircleIcon />}
-                            onClick={() => fileUploadRef.current?.promptForFile()}
-                        >
-                            Add files
-                        </ButtonText>
-                    </Grid>
-
-                    <Grid item>
                         <DownloadButton
                             onClick={(event: React.MouseEvent<HTMLElement>) => {
                                 const configMapName = `${configMapPrefix}-config-files`;
@@ -203,13 +100,7 @@ export const Configuration = ({
                             Download config files yaml
                         </DownloadButton>
                     </Grid>
-
-                    <FileUploadForm ref={fileUploadRef} onFileAdded={saveConfigFile} onFileSelected={validateAndShowConfirmationDialog} />
                 </Grid>
-
-                <ConfigFilesTable filesNames={filesNamesList} onDeleteFileClick={deleteFileFromMicroservice}></ConfigFilesTable>
-
-                <Divider style={{ backgroundColor: '#3B3D48', marginTop: '40px', marginBottom: '20px' }} />
             </Box>
         </>
     );
