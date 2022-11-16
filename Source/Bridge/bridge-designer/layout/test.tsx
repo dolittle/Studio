154ed67fc8eceac34a854b247806c0beea0b1bfc
Pavneet Singh Saund <pav.saund@gmail.com>
2022-11-16 16:26:52 +0000

    scaffolded a create next project

---
+++ Source/Bridge/bridge-designer/layout/test.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+import {Button} from '@dolittle/design-system/atoms/Button/Button';
+
+export  const Test = () => {
+
+    return (<Button variant={'text'} label={'BUTTON'} />);
+};
