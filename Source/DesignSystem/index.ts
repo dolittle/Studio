d6ceede1403e432834f671c39ebc20fa2b4d4e4e
Pavneet Singh Saund <pav.saund@gmail.com>
2022-11-21 16:05:02 +0000

    explicitly export type to satisfy --isolatedModules error
    Error shown in console during build: "Type error: Re-exporting a type when the '--isolatedModules' flag is provided requires using 'export type'."

--- Source/DesignSystem/index.ts
+++ Source/DesignSystem/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export { componentStories, ComponentStories } from './componentStories';
+export { componentStories, type ComponentStories } from './componentStories';
 export { themeDark } from './theming/theme';
 
