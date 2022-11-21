6b8d62900489f1ccba0795b52f321505afb96548
Pavneet Singh Saund <pav.saund@gmail.com>
2022-11-21 16:23:46 +0000

    Load theme from Design System with base-style

--- Source/Bridge/bridge-designer/pages/_app.tsx
+++ Source/Bridge/bridge-designer/pages/_app.tsx
@@ -2,7 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/system';
+import { CssBaseline } from '@mui/material';
+import { themeDark } from '@dolittle/design-system';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+    return (
+        <ThemeProvider theme={themeDark}>
+            <Component {...pageProps} />
+            <CssBaseline />
+        </ThemeProvider>
+    );
 }
