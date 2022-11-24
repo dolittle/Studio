913a06f582c72b2d20cdcca36ad4a2a8497c4ce9
Pavneet Singh Saund <pav.saund@gmail.com>
2022-11-24 16:52:54 +0000

    Fetch random cat fact page

--- Source/Bridge/bridge-frontend/pages/cats/index.tsx
+++ Source/Bridge/bridge-frontend/pages/cats/index.tsx
@@ -1,16 +1,29 @@
-import { useGetCats } from '../../api/cats/useGetCats';
+import { Button } from '@dolittle/design-system/atoms/Button/Button';
+import { useCatFact } from '../../api/cats/useCatFact';
 import { Page } from '../../components/layout/page';
+import { Refresh } from '@mui/icons-material';
+
 
 export default function CatsPage() {
-    const cats = useGetCats();
-    console.log(cats);
+    const catFactQuery = useCatFact();
+    console.log(catFactQuery);
 
     return (
         <Page>
             <h1>Cats</h1>
-            {cats.isLoading && "Loading..."}
-            {cats.isError && "Something went wrong!"}
-            {cats.data?.data.map(cat => <div>{cat}</div>)}
+            <br />
+            <Button
+                variant='filled'
+                label='Refresh'
+                disabled={catFactQuery.isFetching}
+                endWithIcon={<Refresh fontSize='small' />}
+                onClick={() => catFactQuery.refetch()}
+            />
+            <br />
+            <br />
+            {catFactQuery.isLoading && "Loading..."}
+            {catFactQuery.isError && "Something went wrong!"}
+            {catFactQuery.data?.data.map(cat => <div>{cat}</div>)}
         </Page>
     );
 };
