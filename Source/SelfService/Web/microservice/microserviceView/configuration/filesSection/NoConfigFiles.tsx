bdab3e44e57cb12e9cf8633a1288a5d1d728946f
N00bG1rl <403helen@gmail.com>
2022-11-25 10:43:03 +0200

    Moved NoCongigFiles to differrent component

---
+++ Source/SelfService/Web/microservice/microserviceView/configuration/filesSection/NoConfigFiles.tsx
@@ -0,0 +1,42 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { Paper, Typography } from '@mui/material';
+import { AddCircle } from '@mui/icons-material';
+
+import { Button } from '@dolittle/design-system/atoms/Button';
+
+type NoConfigFilesProps = {
+    handleOnClick: any;
+};
+
+export const NoConfigFiles = ({ handleOnClick }: NoConfigFilesProps) => {
+    return (
+        <Paper sx={{ p: 2 }}>
+            <Typography variant='h2'>No configuration files yet...</Typography>
+
+            <Typography variant='body1' sx={{ my: 2 }}>
+                {`To add your first configuration file, select 'add file'. You may select more than one at a time. Each file must be less than 3.145MB.`}
+            </Typography>
+
+            <Button
+                variant='filled'
+                label='Add File(s)'
+                startWithIcon={<AddCircle />}
+                fullWidth
+                color='secondary'
+                sx={{
+                    'backgroundColor': 'rgba(140, 154, 248, 0.08)',
+                    'color': 'primary.main',
+                    'minHeight': 30,
+                    '&:hover': {
+                        backgroundColor: 'rgba(140, 154, 248, 0.15)'
+                    }
+                }}
+                onClick={() => handleOnClick()}
+            />
+        </Paper>
+    );
+};
--- Source/SelfService/Web/microservice/microserviceView/configuration/filesSection/filesSection.tsx
+++ Source/SelfService/Web/microservice/microserviceView/configuration/filesSection/filesSection.tsx
@@ -21,6 +21,7 @@ import { getConfigFilesNamesList, getServerUrlPrefix, updateConfigFile, deleteCo
 
 import { RestartMicroserviceDialog } from '../../RestartMicroserviceDialog';
 import { DataTable, ConfigFilesTableRow } from './dataTable';
+import { NoConfigFiles } from './NoConfigFiles';
 
 const MAX_CONFIGMAP_ENTRY_SIZE = 3145728;
 
@@ -280,30 +281,7 @@ export const FilesSection = ({ applicationId, environment, microserviceName, mic
                 <Box component={Paper} sx={{ width: 1, height: 1 }}>
                     {dataTableRows.length > 0 ?
                         <DataTable rows={dataTableRows} handleSelectionModelChange={setDataRowSelected} selectionModel={dataRowSelected} /> :
-                        <Paper sx={{ p: 2 }}>
-                            <Typography variant='h2'>No configuration files yet...</Typography>
-
-                            <Typography variant='body1' sx={{ my: 2 }}>
-                                {`To add your first configuration file, select 'add file'. You may select more than one at a time. Each file must be less than 3.145MB.`}
-                            </Typography>
-
-                            <Button
-                                variant='filled'
-                                label='Add File(s)'
-                                startWithIcon={<AddCircle />}
-                                fullWidth
-                                color='secondary'
-                                sx={{
-                                    'backgroundColor': 'rgba(140, 154, 248, 0.08)',
-                                    'color': 'primary.main',
-                                    'minHeight': 30,
-                                    '&:hover': {
-                                        backgroundColor: 'rgba(140, 154, 248, 0.15)'
-                                    }
-                                }}
-                                onClick={() => fileUploadRef.current?.showPrompt()}
-                            />
-                        </Paper>
+                        <NoConfigFiles handleOnClick={() => fileUploadRef.current?.showPrompt()} />
                     }
                 </Box>
             </Accordion>
