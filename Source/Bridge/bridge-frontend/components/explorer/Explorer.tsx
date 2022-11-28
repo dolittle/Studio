bc33ec8392f0e4ccb57d836f7da6ee476779f27b
Pavneet Singh Saund <pav.saund@gmail.com>
2022-11-28 23:52:00 +0000

    Fetch all table metadata for a specific environment

--- Source/Bridge/bridge-frontend/components/explorer/Explorer.tsx
+++ Source/Bridge/bridge-frontend/components/explorer/Explorer.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import { useEnvironments } from '../../api/environments/useEnvironments';
+import { useEnvironmentTablesMetadata } from '../../api/tableMetadata/useEnvironmentTablesMetadata';
+
 export const Explorer = () => {
+    const [selectedEnvironment, setSelectedEnvironment] = useState<string | undefined>(undefined);
+    const { data: environments } = useEnvironments();
+    const { data: tablesMetadata } = useEnvironmentTablesMetadata({ environment: selectedEnvironment || "" })
     return (
         <div>
-            The contents of the explorer page
+            <h2>Environments</h2>
+            {environments && environments.map((env) =>
+                <div key={env.name} onClick={() => setSelectedEnvironment(env.name)}>{env.name}</div>
+            )}
+
+            <h3>Table Metadata</h3>
+            {tablesMetadata && tablesMetadata.map((metadata) => <div key={metadata.name}>{metadata.name}</div>)}
         </div>
     );
 };
