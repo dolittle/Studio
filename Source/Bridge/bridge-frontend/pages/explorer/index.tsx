0c2818cb6badeb0fc0a0b4ed50049ab3fbc16e70
Pavneet Singh Saund <pav.saund@gmail.com>
2022-11-28 23:52:00 +0000

    Fetch all table metadata for a specific environment

--- Source/Bridge/bridge-frontend/pages/explorer/index.tsx
+++ Source/Bridge/bridge-frontend/pages/explorer/index.tsx
@@ -1,14 +1,12 @@
-import { useEnvironments } from '../../api/environments/useEnvironments';
 import { Explorer } from '../../components/explorer/Explorer';
 import { Page } from '../../components/layout/Page';
 
 export default function ExplorerPage() {
-    const { data } = useEnvironments();
     return (
         <Page>
             <h1>Explorer</h1>
             <Explorer />
-            {data && data.map((env) => <div key={env.name}>{env.name}</div>)}
+
         </Page>
     );
 };
