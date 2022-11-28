84c38862aba92dbc3e8eb2a512a9cb7d83d175fd
Magne Helleborg <magne.helleborg@gmail.com>
2022-11-28 14:36:23 +0100

    Add generated client for bridge-api
    Command:
    docker run -v ./generated:/generated openapitools/openapi-generator-cli:v6.2.1 generate -g typescript-fetch -i http://host.docker.internal:5000/swagger/v1/swagger.json -o /generated

---
+++ Source/Bridge/bridge-frontend/api/generated/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ConfigurationApi';
+export * from './EnvironmentApi';
+export * from './LoginApi';
+export * from './M3EnvironmentApi';
+export * from './ProgramMetadataApi';
+export * from './TableMetadataApi';
