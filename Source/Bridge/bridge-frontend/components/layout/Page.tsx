b3d163f0a7b21095c9850d746ab9da406d867d81
Pavneet Singh Saund <pav.saund@gmail.com>
2022-11-29 15:52:38 +0000

    Fix tsconfig errors and set "noImplicitAny" to  "true" for bridge-frontend

--- Source/Bridge/bridge-frontend/components/layout/Page.tsx
+++ Source/Bridge/bridge-frontend/components/layout/Page.tsx
@@ -1,8 +1,9 @@
-// import { NavigationMenu } from '../common/NavigationMenu';
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 export type PageProps = {
     children?: React.ReactNode
-}
+};
 
 export const Page = ({ children }: PageProps) => {
     return (
--- Source/Bridge/bridge-frontend/pages/designer/index.tsx
+++ Source/Bridge/bridge-frontend/pages/designer/index.tsx
@@ -1,3 +1,5 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import { Designer } from '../../components/designer/Designer';
 import { Page } from '../../components/layout/Page';
 
