ee8c10228f1986c343985eb4c039d239e8fec40b
N00bG1rl <403helen@gmail.com>
2022-11-30 21:55:14 +0200

    Added IconButton to Storybook stories

---
+++ Source/DesignSystem/atoms/IconButton/IconButton.stories.tsx
@@ -0,0 +1,60 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { Box, Fade, Slide, Typography } from '@mui/material';
+import { DownloadRounded, CloseRounded } from '@mui/icons-material';
+
+import { IconButton } from './IconButton';
+
+export default {
+    title: 'IconButton',
+    component: IconButton
+};
+
+export const IconButtons = () => {
+    const [onClose, setOnClose] = React.useState(true);
+    const [onDownload, setOnDownload] = React.useState(true);
+
+    return (
+        <Box sx={{ display: 'flex', alignContent: 'center', textAlign: 'center' }}>
+            <Box sx={{ mr: 8 }}>
+                <Typography variant='body1' color='secondary'>Default Icon</Typography>
+
+                <IconButton
+                    icon={
+                        <Fade
+                            in={onClose}
+                            timeout={500}
+                            onExited={() => setTimeout(() => setOnClose(true), 1500)}
+                        >
+                            {<CloseRounded />}
+                        </Fade>
+                    }
+                    onClick={() => setOnClose(false)}
+                    sx={{ mt: 2 }}
+                />
+            </Box>
+
+            <Box>
+                <Typography variant='body1' color='secondary'>Download Icon</Typography>
+
+                <IconButton
+                    icon={
+                        <Slide
+                            direction='up'
+                            in={onDownload}
+                            timeout={500}
+                            onExited={() => setTimeout(() => setOnDownload(true), 1500)}
+                        >
+                            {<DownloadRounded />}
+                        </Slide>
+                    }
+                    sx={{ mt: 2 }}
+                    onClick={() => setOnDownload(false)}
+                />
+            </Box>
+        </Box>
+    );
+};
