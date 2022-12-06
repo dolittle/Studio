5fc395594ab53fb2a0a6e9f5f81bbc0bf1e554ed
Pavneet Singh Saund <pav.saund@gmail.com>
2022-12-06 11:06:10 +0000

    Change title to trigger build

--- Source/Bridge/bridge-frontend/pages/index.tsx
+++ Source/Bridge/bridge-frontend/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js! 🙌</a>
+                    Welcome to <a href="https://nextjs.org">Next.js! 👍</a>
                 </h1>
 
                 <p className={styles.description}>
