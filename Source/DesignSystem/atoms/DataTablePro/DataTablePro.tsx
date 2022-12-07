c8b2019a29f6a5674c070efd136b71e5f8e93345
N00bG1rl <403helen@gmail.com>
2022-12-07 20:08:10 +0200

    Edit envVariables data table localy

--- Source/DesignSystem/atoms/DataTablePro/DataTablePro.tsx
+++ Source/DesignSystem/atoms/DataTablePro/DataTablePro.tsx
@@ -5,19 +5,24 @@ import React from 'react';
 
 import { Paper, SxProps } from '@mui/material';
 
-import { DataGridPro, GridRowModel, GridSelectionModel } from '@mui/x-data-grid-pro';
+import { DataGridPro, GridColDef, GridRowModel, GridSelectionModel } from '@mui/x-data-grid-pro';
 import { LicenseInfo } from '@mui/x-license-pro';
 
 LicenseInfo.setLicenseKey('4a83cf3032e8518adfbe8694d092262dTz00ODUxMyxFPTE2OTEyMjE0ODgxODUsUz1wcm8sTE09c3Vic2NyaXB0aW9uLEtWPTI=');
 
 type DataTableProProps = {
-    rows: any[];
-    columns: any[];
+    rows: GridRowModel[];
+    columns: GridColDef[];
     isRowSelectable?: boolean;
+    isRowCheckable?: boolean;
     selectionModel?: GridSelectionModel;
     getRowId?: (row: GridRowModel) => string;
     handleSelectionModelChange?: React.Dispatch<React.SetStateAction<GridSelectionModel>>;
     sx?: SxProps
+
+    processRowUpdate?: any;
+    experimentalFeatures?: any;
+    onProcessRowUpdateError?: any;
 };
 
 export const DataTablePro = (props: DataTableProProps) =>
@@ -33,7 +38,11 @@ export const DataTablePro = (props: DataTableProProps) =>
             autoHeight={true}
             headerHeight={46}
             disableColumnMenu
+            disableSelectionOnClick
             hideFooter
             sx={props.sx}
+            processRowUpdate={props.processRowUpdate}
+            experimentalFeatures={props.experimentalFeatures}
+            onProcessRowUpdateError={props.onProcessRowUpdateError}
         />
     </Paper>;
