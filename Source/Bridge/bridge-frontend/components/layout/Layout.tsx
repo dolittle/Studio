327c535c35010c131048a8dce25fcbe1927d83d9
Pavneet Singh Saund <pav.saund@gmail.com>
2022-12-12 10:57:34 +0000

    Separate out container & presentation components

--- Source/Bridge/bridge-frontend/components/layout/Layout.tsx
+++ Source/Bridge/bridge-frontend/components/layout/Layout.tsx
@@ -1,39 +1,24 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import { Box } from '@mui/material';
-import { ChangeEvent } from 'react';
-import { Select, SelectProps } from 'Source/DesignSystem/atoms/Forms/Select';
-import { useEnvironments } from '../../api/environments/useEnvironments';
 import { M3EnvironmentListDto } from '../../api/generated';
-import { Navigation } from './Navigation/Navigation';
+import { Sidebar } from './Sidebar/Sidebar';
 
 export type OnEnvironmentSelected = (environment: M3EnvironmentListDto) => void;
 
 export type LayoutProps = {
-    onEnvironmentSelected?: OnEnvironmentSelected;
-    selectedEnvironment?: string | null | undefined;
+    withSidebar?: boolean;
+    sidebarSlot?: React.ReactNode;
     children?: React.ReactNode;
 };
 
-export const Layout = ({ children, selectedEnvironment, onEnvironmentSelected }: LayoutProps) => {
-    const { data: environments } = useEnvironments();
-    const selectProps: SelectProps = {
-        label: 'Select environment' ,
-        options: environments?.map((env) => ({ value: env.name ?? '' })) ?? [],
-        onChange: (event: ChangeEvent<HTMLInputElement>) => {
-            const selected = environments?.find(e => e.name === event.target.value);
-            if(selected) {
-                onEnvironmentSelected?.(selected);
-            }
-        },
-        value: selectedEnvironment
-    };
-
+export const Layout = ({
+    withSidebar = true,
+    sidebarSlot, children,
+}: LayoutProps) => {
     return (
         <Box sx={{ display: 'flex' }}>
-            <Navigation>
-                <Select {...selectProps}  />
-            </Navigation>
+            {withSidebar && <Sidebar>{ sidebarSlot}</Sidebar>}
             <Box component='section' sx={{ flexGrow: 1, mx: 3 }}>
                 {children}
             </Box>
--- Source/Bridge/bridge-frontend/components/layout/Sidebar/Sidebar.tsx
+++ Source/Bridge/bridge-frontend/components/layout/Sidebar/Sidebar.tsx
@@ -1,23 +1,19 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Box, Divider, Drawer, Toolbar, List, ListItem } from '@mui/material';
+import { Drawer } from '@mui/material';
+import { Logo } from './Logo';
+import { DolittleLogoMedium } from './logos';
 
 
 const drawerWidth = 200;
 
-export type NavigationElement = {
-    text: string;
-    navigateTo: string;
-    icon?: React.ReactNode;
-};
-
-export type NavigationProps = {
-    navigationElements?: NavigationElement[];
+export type SidebarProps = {
+    // navigationElements?: NavigationElement[];
     children?: React.ReactNode;
 };
 
-export const Navigation = (props: NavigationProps) => {
+export const Sidebar = (props: SidebarProps) => {
     return (
         <Drawer
             sx={{
@@ -31,14 +27,10 @@ export const Navigation = (props: NavigationProps) => {
             variant='permanent'
             anchor='left'
         >
-            <Box
-                sx={{
-                    p: 2,
-                    mb: 2
-                }}
-            >LOGO</Box>
+            <Logo logo={<DolittleLogoMedium/>} />
             {props.children}
         </Drawer>
     );
 };
+
 
