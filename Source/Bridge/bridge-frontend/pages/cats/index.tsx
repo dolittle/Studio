5d934aaff10214e3bcaf69ea6f5bc5cc14d673f8
Pavneet Singh Saund <pav.saund@gmail.com>
2022-12-13 16:47:30 +0000

    Add Layout to the Cat page

--- Source/Bridge/bridge-frontend/pages/cats/index.tsx
+++ Source/Bridge/bridge-frontend/pages/cats/index.tsx
@@ -4,28 +4,30 @@ import { Button } from '@dolittle/design-system/atoms/Button/Button';
 import { useCatFact } from '../../api/cats/useCatFact';
 import { Page } from '../../components/layout/Page';
 import { Refresh } from '@mui/icons-material';
+import { Layout } from '../../components/layout/Layout';
 
 
 export default function CatsPage() {
     const catFactQuery = useCatFact();
-    console.log(catFactQuery);
 
     return (
-        <Page>
-            <h1>Cats</h1>
-            <br />
-            <Button
-                variant='filled'
-                label='Refresh'
-                disabled={catFactQuery.isFetching}
-                endWithIcon={<Refresh fontSize='small' />}
-                onClick={() => catFactQuery.refetch()}
-            />
-            <br />
-            <br />
-            {catFactQuery.isLoading && 'Loading...'}
-            {catFactQuery.isError && 'Something went wrong!'}
-            {catFactQuery.data?.data.map((cat,i) => <div key={i}>{cat}</div>)}
-        </Page>
+        <Layout>
+            <Page>
+                <h1>Cats</h1>
+                <br />
+                <Button
+                    variant='filled'
+                    label='Refresh'
+                    disabled={catFactQuery.isFetching}
+                    endWithIcon={<Refresh fontSize='small' />}
+                    onClick={() => catFactQuery.refetch()}
+                />
+                <br />
+                <br />
+                {catFactQuery.isLoading && 'Loading...'}
+                {catFactQuery.isError && 'Something went wrong!'}
+                {catFactQuery.data?.data.map((cat, i) => <div key={i}>{cat}</div>)}
+            </Page>
+        </Layout>
     );
 };
