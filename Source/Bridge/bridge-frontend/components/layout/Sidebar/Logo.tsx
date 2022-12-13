075cc51bfb171315da08cd13f095dd0aa5d5b266
Pavneet Singh Saund <pav.saund@gmail.com>
2022-12-13 16:49:49 +0000

    Make Logo clickable and navigate to root

--- Source/Bridge/bridge-frontend/components/layout/Sidebar/Logo.tsx
+++ Source/Bridge/bridge-frontend/components/layout/Sidebar/Logo.tsx
@@ -1,9 +1,11 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import { Box } from '@mui/material';
+import Link from '../../navigation/Link';
 
 export type LogoProps = {
     logo?: React.ReactNode;
+    href?: string;
 };
 
 export const Logo = (props: LogoProps) => {
@@ -12,5 +14,5 @@ export const Logo = (props: LogoProps) => {
             p: 2,
             mb: 2
         }}
-    >{props.logo}</Box>;
+    >{props.href ? (<Link href={props.href}>{props.logo}</Link>) : props.logo}</Box>;
 };
