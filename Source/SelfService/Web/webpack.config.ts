9f0a9e7aa9f9c09889e01a6f54cd65898e757d90
N00bG1rl <403helen@gmail.com>
2022-12-23 15:26:57 +0200

    Remove extra comment from webpack.config

--- Source/SelfService/Web/webpack.config.ts
+++ Source/SelfService/Web/webpack.config.ts
@@ -152,7 +152,6 @@ function webpack(env: Args, argv: Args) {
             new webpackImported.DefinePlugin({
                 'process.env.MUI_LICENSE_KEY': JSON.stringify(process.env.MUI_LICENSE_KEY)
             }),
-            // new Dotenv(),
         ],
         optimization: {
             minimizer: [
@@ -168,40 +167,41 @@ function webpack(env: Args, argv: Args) {
             runtimeChunk: true,
             splitChunks: {
                 chunks: 'initial',
-                // sizes are compared against source before minification
-                maxSize: 200000, // splits chunks if bigger than 200k, adjust as required (maxSize added in webpack v4.15)
+                // Sizes are compared against source before minification
+                maxSize: 200000, // Splits chunks if bigger than 200k, adjust as required (maxSize added in webpack v4.15)
                 cacheGroups: {
                     default: false, // Disable the built-in groups default & vendors (vendors is redefined below)
                     // This is the HTTP/1.1 optimised cacheGroup configuration
                     vendors: {
-                        // picks up everything from node_modules as long as the sum of node modules is larger than minSize
+                        // Picks up everything from node_modules as long as the sum of node modules is larger than minSize
                         test: /[\\/]node_modules[\\/]/,
                         name: 'vendors',
                         priority: 19,
-                        enforce: true, // causes maxInitialRequests to be ignored, minSize still respected if specified in cacheGroup
-                        minSize: 30000 // use the default minSize
+                        enforce: true, // Causes maxInitialRequests to be ignored, minSize still respected if specified in cacheGroup
+                        minSize: 30000 // Use the default minSize
                     },
                     vendorsAsync: {
-                        // vendors async chunk, remaining asynchronously used node modules as single chunk file
+                        // Vendors async chunk, remaining asynchronously used node modules as single chunk file
                         test: /[\\/]node_modules[\\/]/,
                         name: 'vendors.async',
                         chunks: 'async',
                         priority: 9,
                         reuseExistingChunk: true,
-                        minSize: 10000 // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
+                        minSize: 10000 // Use smaller minSize to avoid too much potential bundle bloat due to module duplication.
                     },
                     commonsAsync: {
-                        // commons async chunk, remaining asynchronously used modules as single chunk file
+                        // Commons async chunk, remaining asynchronously used modules as single chunk file
                         name: 'commons.async',
                         minChunks: 2, // Minimum number of chunks that must share a module before splitting
                         chunks: 'async',
                         priority: 0,
                         reuseExistingChunk: true,
-                        minSize: 10000 // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
+                        minSize: 10000 // Use smaller minSize to avoid too much potential bundle bloat due to module duplication.
                     }
                 }
             }
         },
     };
 }
+
 export default webpack;
