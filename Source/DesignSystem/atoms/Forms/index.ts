cbcc53583414d858754304c3800e84ca8e3c5106
N00bG1rl <403helen@gmail.com>
2022-12-27 09:00:22 +0200

    Removed extra sx/style props

--- Source/DesignSystem/atoms/Forms/index.ts
+++ Source/DesignSystem/atoms/Forms/index.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export { Checkbox, CheckboxProps } from './Checkbox';
+export { Checkbox } from './Checkbox';
 export { Form, FormProps } from './Form';
 export { Input, InputProps } from './Input';
 export { Select } from './Select';
