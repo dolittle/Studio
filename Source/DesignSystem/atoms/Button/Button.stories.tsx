50c721f17f510b03251a959f83b30bfdb79b7539
Helen Marttila <403helen@gmail.com>
2022-12-27 04:13:48 +0200

    Fixed Apostrophes (upper comas?) to backticks.

--- Source/DesignSystem/atoms/Button/Button.stories.tsx
+++ Source/DesignSystem/atoms/Button/Button.stories.tsx
@@ -19,13 +19,13 @@ const { metadata, createStory } = componentStories(Button, {
 metadata.parameters = {
     docs: {
         description: {
-            component: 'A button triggers an event or action. Its label should let the user know what will happen next.
+            component: `A button triggers an event or action. Its label should let the user know what will happen next.
 
 **Styling:** Buttons come in three style variations. All variations can be used with or without icons to help lift the UI and quickly visually communicate to the user what the button will do.
 - *Filled buttons* are reserved for primary actions. They should be used to call attention to an action on a form or to highlight the strongest call to action on the page. Filled buttons should appear only once per container (not including the application header or in a modal dialog). Not every screen requires a primary action, or filled button. Filled buttons use our primary main color. 
 - *Outlined buttons* are reserved for login screens. The empty fill allows third-party icons to be used in their original styling. 
-- *Text buttons* are used for secondary actions, such as ‘cancel’ or to carry out an optional action within the page. They are commonly found in dialogs, cards or sometimes toolbars. Text buttons may use our primary main color or the inherit color of the page depending on whether or not the user’s attention should be drawn to the button or if the button needs to be distinguished from other content on the page. 
-',
+- *Text buttons* are used for secondary actions, such as 'cancel' or to carry out an optional action within the page. They are commonly found in dialogs, cards or sometimes toolbars. Text buttons may use our primary main color or the inherit color of the page depending on whether or not the user’s attention should be drawn to the button or if the button needs to be distinguished from other content on the page. 
+`,
         }
     }
 };
