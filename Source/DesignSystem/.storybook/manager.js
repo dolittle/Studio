9a98a2ca96aa856c2ad0f1b1dd071e496d186abd
N00bG1rl <403helen@gmail.com>
2022-12-29 11:23:58 +0200

    Add some branding info to Storybook

--- Source/DesignSystem/.storybook/manager.js
+++ Source/DesignSystem/.storybook/manager.js
@@ -3,4 +3,6 @@ import { themes } from '@storybook/theming';
 
 addons.setConfig({
     theme: themes.dark,
+    brandTitle: 'Dolittle Studio Styleguide',
+    brandUrl: 'https://www.dolittle.com/',
 });
