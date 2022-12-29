e77cc8eb783fc7208c550e81efd3185d440cf277
N00bG1rl <403helen@gmail.com>
2022-12-29 08:39:45 +0200

    Add dark theme to Storybook

--- Source/DesignSystem/.storybook/main.js
+++ Source/DesignSystem/.storybook/main.js
@@ -1,65 +1,66 @@
 const path = require('path');
 
 module.exports = {
-  stories: [
-    {
-        directory: '../atoms',
-        files: '**/*.stories.tsx',
-        titlePrefix: 'Atoms',
-    },
-    {
-        directory: '../molecules',
-        files: '**/*.stories.tsx',
-        titlePrefix: 'Molecules',
-    },
-  ],
-
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app'
-  ],
-
-  framework: '@storybook/react',
-
-  core: {
-    builder: '@storybook/builder-webpack5'
-  },
-
-  staticDirs: [
-      '../public'
-  ],
-
-  webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)?$/,
-      exclude: /node_modules/,
-      include: path.resolve(__dirname, "../"),
-      use: [
+    stories: [
         {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [
-              require('@babel/preset-typescript').default,
-              [require('@babel/preset-react').default, { runtime: 'automatic' }],
-              require('@babel/preset-env').default,
-            ],
-          },
+            directory: '../atoms',
+            files: '**/*.stories.tsx',
+            titlePrefix: 'Atoms',
         },
-      ],
-    })
+        {
+            directory: '../molecules',
+            files: '**/*.stories.tsx',
+            titlePrefix: 'Molecules',
+        },
+    ],
 
-    config.resolve.extensions.push('.ts', '.tsx')
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/theming',
+        '@storybook/preset-create-react-app',
+    ],
 
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: 'javascript/auto',
-    })
+    framework: '@storybook/react',
 
-    config.resolve.extensions.push('.mjs')
+    core: {
+        builder: '@storybook/builder-webpack5'
+    },
 
-    return config
-  }
+    staticDirs: [
+        '../public'
+    ],
+
+    webpackFinal: (config) => {
+        config.module.rules.push({
+            test: /\.(ts|tsx)?$/,
+            exclude: /node_modules/,
+            include: path.resolve(__dirname, "../"),
+            use: [
+                {
+                    loader: require.resolve('babel-loader'),
+                    options: {
+                        presets: [
+                            require('@babel/preset-typescript').default,
+                            [require('@babel/preset-react').default, { runtime: 'automatic' }],
+                            require('@babel/preset-env').default,
+                        ],
+                    },
+                },
+            ],
+        })
+
+        config.resolve.extensions.push('.ts', '.tsx')
+
+        config.module.rules.push({
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: 'javascript/auto',
+        })
+
+        config.resolve.extensions.push('.mjs')
+
+        return config
+    }
 };
