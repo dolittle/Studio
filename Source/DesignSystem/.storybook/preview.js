8aaea04fbc87b65ae295ef16392afec7a0d8f8d5
N00bG1rl <403helen@gmail.com>
2022-12-29 08:39:45 +0200

    Add dark theme to Storybook

--- Source/DesignSystem/.storybook/preview.js
+++ Source/DesignSystem/.storybook/preview.js
@@ -1,23 +1,30 @@
-import React from "react"
+import React from 'react';
+
 import '../theming/fonts';
 import { themeDark } from '../theming/theme';
+
+import { themes } from '@storybook/theming';
+
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material'
 
 export const decorators = [
-  Story => (
-    <ThemeProvider theme={themeDark}>
-      <CssBaseline/>
-      <Story />
-    </ThemeProvider>
-  ),
+    Story => (
+        <ThemeProvider theme={themeDark}>
+            <CssBaseline />
+            <Story />
+        </ThemeProvider>
+    ),
 ];
 
 export const parameters = {
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+    docs: {
+        theme: themes.dark,
+    },
+};
