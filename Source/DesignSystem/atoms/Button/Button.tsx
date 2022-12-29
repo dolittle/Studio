3b2c9b946fd6beaa1b3bd251de328681a27fac22
N00bG1rl <403helen@gmail.com>
2022-12-29 13:06:27 +0200

    Add jsDoc to Button.tsx

--- Source/DesignSystem/atoms/Button/Button.tsx
+++ Source/DesignSystem/atoms/Button/Button.tsx
@@ -6,22 +6,80 @@ import React, { MouseEventHandler, ReactElement } from 'react';
 import { Button as MuiButton, SvgIconProps, SxProps } from '@mui/material';
 
 export type ButtonProps = {
-    variant: 'filled' | 'text' | 'outlined';
+    /**
+     * Required - button must contain text.
+     */
     label: string;
+
+    /**
+     * Optional. The variant of the button.
+     * @default 'text'
+     */
+    variant?: 'filled' | 'text' | 'outlined';
+
+    /**
+     * Optional. Whether the button is disabled or not.
+     * @default false
+     */
     disabled?: boolean;
+
+    /**
+     * Optional. The color of the button.
+     * @default 'primary'
+     */
     color?: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning';
+
+    /**
+     * Optional. The size of the button.
+     * Small is same size as medium as it is overridden in the Theme.
+     * @default 'small'
+     */
     size?: 'small' | 'medium' | 'large';
+
+    /**
+     * Optional. Whether the button should take up the full width of its container.
+     * @default false
+     */
     isFullWidth?: boolean;
+
+    /**
+     * An optional icon to start with. Supports only React svg icons.
+     * @default undefined
+     */
     startWithIcon?: ReactElement<SvgIconProps>;
+
+    /**
+     * An optional icon to end with. Supports only React svg icons.
+     * @default undefined
+     */
     endWithIcon?: ReactElement<SvgIconProps>;
+
+    /**
+     * Optional. The type of the button.
+     * @default 'button'
+     */
     type?: 'button' | 'submit' | 'reset';
     onClick?: MouseEventHandler<HTMLButtonElement>;
+
+    /**
+     * Optional. The sx prop for the button.
+     * @default undefined
+     */
     sx?: SxProps;
 };
 
-export const Button = ({ variant, label, disabled, color, size, isFullWidth, startWithIcon, endWithIcon, type, onClick, sx }: ButtonProps) =>
+/**
+ * A button component. Children is not a valid prop.
+ * @param {...ButtonProps} props - The {@link ButtonProps}.
+ * @returns {ReactElement} A new {@link Button} component.
+ * @example
+ * <Button label='Click me' disabled />
+ * <Button label='Click me' variant='filled' isFullWidth startWithIcon={<AddCircle />} />
+ * <Button label='Click me' type='submit' />
+ */
+export const Button = ({ variant, label, disabled, color, size, isFullWidth, startWithIcon, endWithIcon, type, onClick, sx }: ButtonProps): ReactElement =>
     <MuiButton
-        variant={variant === 'filled' ? 'contained' : variant}
+        variant={variant === 'filled' ? 'contained' : variant || 'text'}
         disabled={disabled}
         color={color}
         size={size ?? 'small'}
