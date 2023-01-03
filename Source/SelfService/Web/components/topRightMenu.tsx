55a74ae9d05a7eb79fca7439b9ed66965d31d781
N00bG1rl <403helen@gmail.com>
2023-01-03 07:58:34 +0200

    Remove unused styles

--- Source/SelfService/Web/components/topRightMenu.tsx
+++ Source/SelfService/Web/components/topRightMenu.tsx
@@ -2,30 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
-import { ShortInfoWithEnvironment } from '../api/api';
-import { ApplicationsChanger } from '../application/applicationsChanger';
-import { SxProps } from '@mui/material';
 
-type Props = {
+import { ShortInfoWithEnvironment } from '../api/api';
+
+import { ApplicationsChanger } from '../application/applicationsChanger';
+
+type TopRightMenuProps = {
     applications: ShortInfoWithEnvironment[];
     applicationId: string;
     environment: string;
 };
 
-const styles = {
-    iconButton: {
-        padding: 0,
-        marginRight: 1,
-    } as SxProps,
-    icon: {
-        fill: 'white',
-    } as SxProps,
-};
-
-export const TopRightMenu: React.FunctionComponent<Props> = (props) => {
-    const _props = props!;
-
-    return <>
-        <ApplicationsChanger applications={_props.applications} applicationId={_props.applicationId} environment={_props.environment} />
-    </>;
-};
+export const TopRightMenu = ({ applications, applicationId, environment }: TopRightMenuProps) =>
+    <ApplicationsChanger applications={applications} applicationId={applicationId} environment={environment} />;
