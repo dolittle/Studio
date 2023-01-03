0656c0c564899a01179dd8ed37db687fc654b358
Liz <103487842+Liz-Brenner@users.noreply.github.com>
2023-01-03 09:58:18 +0100

    Update AlertBox.stories.tsx

--- Source/DesignSystem/atoms/AlertBox/AlertBox.stories.tsx
+++ Source/DesignSystem/atoms/AlertBox/AlertBox.stories.tsx
@@ -12,13 +12,13 @@ metadata.parameters = {
         description: {
             component: `An alert displays a short, important message in a way that attracts the user’s attention without interrupting the user’s tasks. These should not be confused with snackbars, which are used to signal feedback around specific system tasks that a user has just completed or attempted to complete. Alerts are used to indicate system status and feedback as a whole.
             
-**Styling:** The default style for alerts is outlined with an icon to indicate what severity level the alert is. Alerts can contain a title that provide additional information to the severity level. An alert can have an action such as a close button.
+**Styling:** The default style for alerts is outlined with an icon to indicate what severity level the alert is. Alerts can contain a title that provides additional information about the severity level. An alert can have an action such as a close button.
 
-**Severity types:** We have four severity types: error alerts, warning alerts, info alerts, and success alerts. 
+**Severity levels:** We have four severity levels: error alerts, warning alerts, info alerts, and success alerts. 
 - *Error alerts* let the user know something is wrong. Explain the problem and provide the user with a next step, an alternative solution or contact information to our help services. Keep the message simple and direct. Don’t use technical details or blame the user. 
-- *Warning alerts* anticipate a change that will impact the system and therefore user’s experience. Inform but don’t alarm. If action is required from the user, clearly state what is needed and tell them what to expect after. Provide a cancel option if possible. 
+- *Warning alerts* anticipate a change that will impact the system and therefore user’s experience. Inform but don’t alarm. If an action is required from the user, clearly state what is needed and tell them what to expect after. Provide a cancel option if possible. 
 - *Success alerts* indicate the system is functioning as it should.  
-- *Info alerts* provide the user with additional helpful information that don’t require action.
+- *Info alerts* provide the user with additional helpful information that doesn't require action.
 `,
         }
     }
