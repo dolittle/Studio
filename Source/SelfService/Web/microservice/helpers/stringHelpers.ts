a10e43cca898bb3193169a98fabc2c2c577761ea
N00bG1rl <403helen@gmail.com>
2023-01-05 09:14:06 +0200

    Add microservice runtime version helper

--- Source/SelfService/Web/microservice/helpers/stringHelpers.ts
+++ Source/SelfService/Web/microservice/helpers/stringHelpers.ts
@@ -6,7 +6,17 @@ export const capitalize = (s: string) => {
     return s.charAt(0).toUpperCase() + s.slice(1);
 };
 
-export const removeFromString = (s: string, remove: RegExp) => {
+export const removeStringPattern = (s: string, remove: RegExp) => {
     if (typeof s !== 'string') return '';
     return s.replace(remove, '');
+};
+
+export const removeStringPatternAndCapitalize = (s: string, remove: RegExp) => {
+    return capitalize(removeStringPattern(s, remove));
+};
+
+// Return only microservice runtime image number from: dolittle/runtime:8.6.0
+export const getRuntimeNumberFromString = (s: string) => {
+    if (typeof s !== 'string') return 'N/A';
+    return removeStringPatternAndCapitalize(s, /dolittle\/runtime:/gi);
 };
