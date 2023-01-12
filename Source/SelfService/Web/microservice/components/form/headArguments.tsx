f8fa2b766391a83532bfeab2662a014d48a7bd1b
N00bG1rl <403helen@gmail.com>
2023-01-12 07:37:23 +0200

    Add validation to headArguments

--- Source/SelfService/Web/microservice/components/form/headArguments.tsx
+++ Source/SelfService/Web/microservice/components/form/headArguments.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 import { Box, TextField } from '@mui/material';
 import { AddCircleRounded, DeleteRounded } from '@mui/icons-material';
@@ -15,8 +15,23 @@ type HeadArgumentsProps = {
 };
 
 export const HeadArguments = ({ cmdArgs, setCmdArgs, disabled }: HeadArgumentsProps) => {
+    const [emptyFieldError, setEmptyFieldError] = useState<string[]>([]);
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, argIndex: number) => {
+    const emptyFieldList: string[] = [];
+
+    const handleChangeArg = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, argIndex: number) => {
+        // Remove error if field is no longer empty
+        if (event.target.value) {
+            const newError = [...emptyFieldError];
+            const id = 'headArg' + argIndex.toString();
+            const index = newError.indexOf(id);
+            if (index > -1) newError.splice(index, 1);
+            setEmptyFieldError(newError);
+        }
+
+        // Prevents user from entering a multible spaces
+        if (event.target.value.includes(`  `)) return;
+
         const newArgs = [...cmdArgs];
         newArgs[argIndex] = event.target.value;
         setCmdArgs(newArgs);
@@ -36,34 +51,45 @@ export const HeadArguments = ({ cmdArgs, setCmdArgs, disabled }: HeadArgumentsPr
 
     return (
         <Box>
-            {cmdArgs.map((arg, argIndex) => (
-                <Box sx={{ display: 'flex', alignItems: 'center' }} key={argIndex}>
-                    <TextField
-                        id={'headArg' + argIndex.toString()}
-                        label='CMD Argument'
-                        value={arg}
-                        disabled={disabled}
-                        onChange={event => handleChange(event, argIndex)}
-                        size='small'
-                        variant='outlined'
-                        sx={{ width: 220 }}
-                    />
-                    <Button
-                        label='Remove'
-                        secondary
-                        disabled={disabled}
-                        startWithIcon={<DeleteRounded />}
-                        onClick={() => handleRemoveArg(argIndex)}
-                        sx={{ height: 29, ml: 2 }}
-                    />
-                </Box>
-            ))}
+            {cmdArgs.map((arg, argIndex) => {
+                const id = 'headArg' + argIndex.toString();
+
+                if (!arg) emptyFieldList.push(id);
+                const hasEmptyField = emptyFieldError.includes(id);
+
+                return (
+                    <Box sx={{ display: 'flex', alignItems: 'center' }} key={argIndex}>
+                        <TextField
+                            id={id}
+                            label='CMD Argument'
+                            value={arg}
+                            autoFocus
+                            onBlur={() => setEmptyFieldError(emptyFieldList)}
+                            error={hasEmptyField}
+                            helperText={hasEmptyField ? 'Please enter a CMD argument.' : ''}
+                            disabled={disabled}
+                            onChange={event => handleChangeArg(event, argIndex)}
+                            size='small'
+                            variant='outlined'
+                            sx={{ width: 220 }}
+                        />
+                        <Button
+                            label='Remove'
+                            secondary
+                            disabled={disabled}
+                            startWithIcon={<DeleteRounded />}
+                            onClick={() => handleRemoveArg(argIndex)}
+                            sx={{ height: 29, ml: 2 }}
+                        />
+                    </Box>
+                );
+            })}
 
             <Button
                 label='Add CMD argument'
                 secondary
                 startWithIcon={<AddCircleRounded />}
-                disabled={disabled}
+                disabled={disabled || emptyFieldError.length > 0}
                 onClick={handleAddArg}
                 sx={{ justifyContent: 'start', mt: 2.5 }}
             />
