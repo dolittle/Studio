a406fbb76bfa8416bfe796e054c2f93e3a82dbcc
N00bG1rl <403helen@gmail.com>
2023-01-16 23:30:25 +0200

    Use React.forwardRef to display Select Tooltip

--- Source/DesignSystem/atoms/Forms/Select.tsx
+++ Source/DesignSystem/atoms/Forms/Select.tsx
@@ -1,11 +1,9 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-import { FormControl, MenuItem, TextField, SxProps } from '@mui/material';
-
-import { Tooltip } from '@dolittle/design-system';
+import { FormControl, MenuItem, TextField, TextFieldProps, SxProps } from '@mui/material';
 
 import { useController, FieldProps } from './helpers';
 
@@ -14,37 +12,38 @@ import { useController, FieldProps } from './helpers';
  */
 export type SelectProps = {
     options: { value: string }[];
-    tooltipTitle?: string;
-    tooltipText?: string;
     sx?: SxProps;
 } & FieldProps;
 
-export const Select = (props: SelectProps) => {
-    const { field } = useController(props);
+/**
+ * Creates an select input field to be used in a {@link Form}.
+ * @param props The {@link SelectProps} for the input.
+ * @returns A new {@link Select} component.
+ */
+export const Select = forwardRef<HTMLInputElement, SelectProps>(({ options, sx, ...selectProps }, ref) => {
+    const { field } = useController(selectProps);
 
     return (
-        <FormControl sx={{ width: 220, ...props.sx }}>
-            <Tooltip
-                id={`${props.id}-tooltip`}
-                tooltipTitle={props.tooltipTitle}
-                tooltipText={props.tooltipText}
+        <FormControl sx={{ width: 220, ...sx }}>
+            <TextField
+                {...field}
+                {...selectProps as TextFieldProps}
+                ref={ref}
+                select
+                label={selectProps.label}
+                value={field.value}
+                disabled={selectProps.disabled}
+                size='small'
+                fullWidth
             >
-                <TextField
-                    {...field}
-                    select
-                    label={props.label}
-                    value={field.value}
-                    disabled={props.disabled}
-                    size='small'
-                    fullWidth
-                >
-                    {props.options.map(option => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.value}
-                        </MenuItem>
-                    ))}
-                </TextField>
-            </Tooltip>
+                {options.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.value}
+                    </MenuItem>
+                ))}
+            </TextField>
         </FormControl>
     );
-};
+});
+
+Select.displayName = 'Select';
