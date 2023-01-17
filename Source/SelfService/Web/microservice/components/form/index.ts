3e60add1c368f4db77ae96d607b503c7b5e8d4c4
N00bG1rl <403helen@gmail.com>
2023-01-17 20:45:38 +0200

    Refactor SetupSection

--- Source/SelfService/Web/microservice/components/form/index.ts
+++ Source/SelfService/Web/microservice/components/form/index.ts
@@ -2,6 +2,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 export { ConfigurationSetupField } from './configurationSetupField';
-export { ContainerImageField } from './containerImageField';
 export { PublicUrlField } from './publicUrlField';
 export { HasM3ConnectorField } from './hasM3ConnectorField';
