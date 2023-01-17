ca60749dd9b81ff22a67e31ac63c0cbf77e95e6f
N00bG1rl <403helen@gmail.com>
2023-01-17 20:58:23 +0200

    Refactor Configuration Setup fields

--- Source/SelfService/Web/microservice/components/form/index.ts
+++ Source/SelfService/Web/microservice/components/form/index.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export { ConfigurationSetupField } from './configurationSetupField';
 export { PublicUrlField } from './publicUrlField';
 export { HasM3ConnectorField } from './hasM3ConnectorField';
