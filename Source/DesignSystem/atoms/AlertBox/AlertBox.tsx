2d01c6c1c74c384af0d1751946312724d2f4dbdb
N00bG1rl <403helen@gmail.com>
2023-01-25 15:08:14 +0200

    Add JsDoc to AlertBox

--- Source/DesignSystem/atoms/AlertBox/AlertBox.tsx
+++ Source/DesignSystem/atoms/AlertBox/AlertBox.tsx
@@ -1,22 +1,58 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import { Alert, AlertTitle, SxProps, Typography } from '@mui/material';
 
 import { IconButton } from '@dolittle/design-system/atoms/IconButton/IconButton';
 
 export type AlertBoxProps = {
+    /**
+     * Required. The severity of the alert.
+     */
     severity: 'error' | 'warning' | 'info' | 'success';
+
+    /**
+     * Required. The title of the alert.
+     * @type {string}
+     */
     title: string;
-    message: React.ReactElement | string;
+
+    /**
+     * Required. The message of the alert.
+     * @type {string}
+     * @type {ReactElement}
+     */
+    message: string | ReactElement;
+
+    /**
+     * Set if the alert is dismissable.
+     * @default false
+     * @type {boolean}
+     */
     isDismissable?: boolean;
     onDismiss?: () => void;
+
+    /**
+     * The sx prop lets you add custom styles to the component, overriding the styles defined by Material-UI.
+     * @type {SxProps}
+     */
     sx?: SxProps;
 };
 
-export const AlertBox = ({ severity, isDismissable, onDismiss, sx, title, message }: AlertBoxProps) =>
+/**
+ * A AlertBox component.
+ * @param {...AlertBoxProps} props - The {@link AlertBoxProps}.
+ * @returns {ReactElement} A new {@link AlertBox} component.
+ * @example
+ * <AlertBox
+ *    severity='error'
+ *    title='Error'
+ *    message='Something went wrong.'
+ * />
+ */
+export const AlertBox = ({ severity, isDismissable, onDismiss, sx, title, message }: AlertBoxProps): ReactElement =>
     <Alert
         variant='outlined'
         severity={severity}
