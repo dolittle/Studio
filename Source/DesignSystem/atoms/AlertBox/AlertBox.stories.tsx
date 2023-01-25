9b59e6654d228ddab9e1296294996983b73d3b78
N00bG1rl <403helen@gmail.com>
2023-01-25 14:12:19 +0200

    Fix Link usage

--- Source/DesignSystem/atoms/AlertBox/AlertBox.stories.tsx
+++ Source/DesignSystem/atoms/AlertBox/AlertBox.stories.tsx
@@ -1,6 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import React from 'react';
+
+import { Link } from '@mui/material';
+
 import { componentStories } from '@dolittle/design-system';
 
 import { AlertBox } from './AlertBox';
@@ -24,53 +28,46 @@ metadata.parameters = {
     }
 };
 
-
 export default metadata;
+
+const ErrorMessage = () =>
+    <>
+        Please try again later. If problem persists, please contact <Link href='#'>Dolittle support</Link>.
+    </>;
+
+const InfoMessage = () =>
+    <>
+        For more information, please contact <Link href='#'>Dolittle support</Link>.
+    </>;
 
 export const Error = createStory({
     severity: 'error',
     title: 'Something went wrong',
-    message: 'Please try again later. If problem persists, please',
-    endWithLink: {
-        linkHref: 'mailto: support@dolittle.com',
-        linkText: 'contact support'
-    }
+    message: <ErrorMessage />
 });
 
 export const Warning = createStory({
     severity: 'warning',
     title: 'This is a warning.',
-    message: 'Please try again later. If problem persists, please',
-    endWithLink: {
-        linkHref: 'mailto: support@dolittle.com',
-        linkText: 'contact support'
-    }
+    message: <ErrorMessage />
 });
 
 export const Info = createStory({
     severity: 'info',
     title: 'This is an info message.',
-    message: 'For more information, please',
-    endWithLink: {
-        linkHref: 'https://www.dolittle.com/',
-        linkText: 'look our website'
-    }
+    message: <InfoMessage />
 });
 
 export const Success = createStory({
     severity: 'success',
     title: 'All good!',
-    message: 'You are all set up and ready to go!',
+    message: 'You are all set up and ready to go!'
 });
 
 export const WithAction = createStory({
     severity: 'info',
     title: 'This is an info message.',
     message: 'For more information, please',
-    endWithLink: {
-        linkHref: 'https://www.dolittle.com/',
-        linkText: 'look our website'
-    },
     isDismissable: true,
     onDismiss: () => alert('Close action was clicked.')
 });
--- Source/DesignSystem/atoms/AlertBox/AlertBox.tsx
+++ Source/DesignSystem/atoms/AlertBox/AlertBox.tsx
@@ -3,55 +3,31 @@
 
 import React from 'react';
 
-import { Alert, AlertTitle, Box, Link, SxProps, Typography } from '@mui/material';
+import { Alert, AlertTitle, SxProps, Typography } from '@mui/material';
 
 import { IconButton } from '@dolittle/design-system/atoms/IconButton/IconButton';
-
-type LinkContent = {
-    linkText: string;
-    linkHref: string;
-};
 
 export type AlertBoxProps = {
     severity: 'error' | 'warning' | 'info' | 'success';
     title: string;
-    message: string;
-    endWithLink?: LinkContent;
+    message: React.ReactElement | string;
     isDismissable?: boolean;
     onDismiss?: () => void;
     sx?: SxProps;
 };
 
-const spaceChar = '\u2002';
-const periodChar = '.';
-
-const MessageEndLink = ({ linkText, linkHref }: LinkContent) =>
-    <Typography variant='body2'>
-        {spaceChar}
-        <Link href={linkHref}>{linkText}</Link>
-        {periodChar}
-    </Typography>;
-
-export const AlertBox = ({ severity, isDismissable, onDismiss, sx, title, message, endWithLink }: AlertBoxProps) =>
+export const AlertBox = ({ severity, isDismissable, onDismiss, sx, title, message }: AlertBoxProps) =>
     <Alert
         variant='outlined'
         severity={severity}
         action={isDismissable && <IconButton onClick={onDismiss} />}
         sx={{
             'display': 'inline-flex',
-            'textAlign': 'left',
-            'position': 'relative',
             'borderColor': severity === 'error' ? 'error.dark' : null,
-            '& .MuiAlert-action': {
-                pt: 0
-            },
+            '& .MuiAlert-action': { pt: 0 },
             ...sx
         }}
     >
         <AlertTitle>{title}</AlertTitle>
-
-        <Box sx={{ display: 'inline-flex' }}>
-            <Typography variant='body2'>{message}</Typography>
-            {endWithLink && <MessageEndLink {...endWithLink} />}
-        </Box>
+        <Typography variant='body2'>{message}</Typography>
     </Alert>;
