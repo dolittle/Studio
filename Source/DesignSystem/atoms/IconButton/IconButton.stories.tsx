f778b539c0bc81455decb0f0e261ad2a28b8aa24
N00bG1rl <403helen@gmail.com>
2023-01-27 12:44:47 +0200

    Add IconButtons controls to stories

--- Source/DesignSystem/atoms/IconButton/IconButton.stories.tsx
+++ Source/DesignSystem/atoms/IconButton/IconButton.stories.tsx
@@ -5,11 +5,39 @@ import React from 'react';
 
 import { DownloadRounded } from '@mui/icons-material';
 
-import { componentStories } from '@dolittle/design-system';
-
-import { IconButton } from './IconButton';
+import { componentStories, IconButton } from '@dolittle/design-system';
 
 const { metadata, createStory } = componentStories(IconButton);
+
+metadata.argTypes = {
+    label: {
+        type: 'string',
+        defaultValue: 'Download',
+        control: {
+            type: 'text'
+        }
+    },
+    icon: {
+        control: false,
+        table: {
+            type: {
+                summary: 'ReactElement<SvgIconProps>'
+            }
+        }
+    },
+    color: {
+        defaultValue: 'inherit'
+    },
+    size: {
+        defaultValue: 'small'
+    },
+    href: {
+        control: false
+    },
+    onClick: {
+        control: false
+    }
+};
 
 export default metadata;
 
@@ -17,6 +45,16 @@ export const Inherit = createStory({
     label: 'Download',
     icon: <DownloadRounded />
 });
+
+// TODO: Haven't figured out how to display react elements in the docs tab yet
+// so this is a workaround.
+Inherit.parameters = {
+    docs: {
+        source: {
+            code: `<IconButton label='Download' icon={<DownloadRounded />} />`
+        }
+    }
+};
 
 export const Secondary = createStory({
     color: 'primary'
