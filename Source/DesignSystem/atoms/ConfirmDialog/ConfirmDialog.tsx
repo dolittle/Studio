2f740699207369065066e49fb6775ba1b990423a
N00bG1rl <403helen@gmail.com>
2023-02-22 21:24:27 +0200

    Add JsDocs to ConfirmDialog

--- Source/DesignSystem/atoms/ConfirmDialog/ConfirmDialog.tsx
+++ Source/DesignSystem/atoms/ConfirmDialog/ConfirmDialog.tsx
@@ -25,18 +25,63 @@ const styles = {
 };
 
 export type ConfirmDialogProps = {
+    /**
+     * Required. The id is used to identify the dialog and its children. It is used for accessibility and testing.
+     *
+     * It should be unique for each dialog.
+     */
     id: string;
-    title: string;
-    description?: string;
+
+    /**
+     *  The title should capture the essence of the message. It should be short and to the point.
+     *
+     *  Do not repeat the body information in the title of the dialog.
+     */
+    title?: string;
+
+    /**
+     * Required. The description should provide more information about the action that is going to be made.
+     */
+    description: string;
+
+    /**
+     * It can be used to provide a list of items that will be affected by the action.
+     */
     children?: React.ReactNode;
+
+    /**
+     * The cancel text that dismisses the dialog.
+     * @default Cancel
+     */
     cancelText?: string;
+
+    /**
+     * If the action or output is irreversible or will cause significant changes, consider using a Danger color.
+     * @default primary
+     */
+    confirmTextColor?: 'primary' | 'subtle' | 'error' | 'success' | 'warning';
+
+
     confirmText: string;
+
+    /**
+     * The dialog will be open if this is true.
+     * @default false
+     */
     isOpen?: boolean;
+
+    /**
+     * The callback that is called when the dialog is closed.
+     */
     onCancel: () => void;
+
+    /**
+     * The callback that is called when the dialog is confirmed.
+     */
     onConfirm: () => void;
 };
 
-export const ConfirmDialog = ({ id, title, description, children, cancelText, confirmText, isOpen, onCancel, onConfirm }: ConfirmDialogProps) =>
+export const ConfirmDialog = ({ id, title, description, children, confirmTextColor, cancelText, confirmText, isOpen, onCancel, onConfirm }: ConfirmDialogProps) =>
     <Dialog
         open={isOpen ?? false}
         onClose={onCancel}
@@ -60,6 +105,6 @@ export const ConfirmDialog = ({ id, title, description, children, cancelText, co
 
         <DialogActions sx={{ mr: 1 }}>
             <Button onClick={onCancel} label={cancelText ?? 'Cancel'} color='subtle' />
-            <Button onClick={onConfirm} label={confirmText} />
+            <Button onClick={onConfirm} label={confirmText} color={confirmTextColor ?? 'primary'} />
         </DialogActions>
     </Dialog>;
