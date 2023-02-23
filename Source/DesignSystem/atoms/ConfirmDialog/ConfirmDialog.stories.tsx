cfbed80976413ee95cbe7b4fe1c958f425823a00
N00bG1rl <403helen@gmail.com>
2023-02-23 11:18:46 +0200

    Move extra content into helper.ts file

--- Source/DesignSystem/atoms/ConfirmDialog/ConfirmDialog.stories.tsx
+++ Source/DesignSystem/atoms/ConfirmDialog/ConfirmDialog.stories.tsx
@@ -9,29 +9,14 @@ import { Divider, List, ListItem } from '@mui/material';
 
 import { Button, ConfirmDialog } from '@dolittle/design-system';
 
+import { confirmDialogDescription, listArray } from './helper';
+
 export default {
     title: 'Confirm Dialog',
     component: ConfirmDialog,
     parameters: {
         docs: {
-            description: {
-                component: `Dialogs are windows that appear in front of all content to provide immediate and critical information or 
-                to ask the user for a decision. Dialogs disable all other functionality until a user engages with it by either confirming,
-                dismissing, or taking the required action. Dialogs are purposefully interruptive so they should be used sparingly.
-
-**Title**
-- In some cases the description may be enough information for the user to make a decision and a title is not needed.
-- If a title is required use a clear question or statement with an explanation in the content area. Avoid apologies, 
-ambiguity or questions such as "warning!" or "are you sure?".
-
-**Types of Dialogs**
-- *Alert dialogs* are urgent interruptions, requiring acknowledgement or action, that inform the user about a situation. 
-    Most alerts do not need titles. They summarize a decision in a sentence or two by either asking a question or making a 
-    statement related to the action buttons.
-    Alert dialogs must always contain an action item such as "agree", "accept", or "continue" and a dismissive action such as "cancel", 
-    "disagree" or "deny".
-`,
-            },
+            description: { component: confirmDialogDescription },
         },
     },
     args: {
@@ -67,12 +52,6 @@ const Template: ComponentStory<typeof ConfirmDialog> = args => {
 
 export const Default = Template.bind({});
 
-const listArray = [
-    'List item 1', 'List item 2', 'List item 3', 'List item 4', 'List item 5', 'List item 6', 'List item 7',
-    'List item 8', 'List item 9', 'List item 10', 'List item 11', 'List item 12', 'List item 13', 'List item 14',
-    'List item 15', 'List item 16', 'List item 17', 'List item 18', 'List item 19', 'List item 20', 'List item 21',
-    'List item 22', 'List item 23', 'List item 24', 'List item 25', 'List item 26', 'List item 27', 'List item 28',
-];
 export const WithChildrenContent = Template.bind({});
 WithChildrenContent.args = {
     children: (
