58969ba1b3991fcc01c637a08620a9d81b81a8dc
N00bG1rl <403helen@gmail.com>
2023-02-24 14:16:01 +0200

    Design System Input component improvements

--- Source/DesignSystem/atoms/Forms/Input.stories.tsx
+++ Source/DesignSystem/atoms/Forms/Input.stories.tsx
@@ -3,49 +3,109 @@
 
 import React from 'react';
 
-import { componentStories, Form, Input } from '@dolittle/design-system';
+import { Search } from '@mui/icons-material';
+
+import { componentStories, Form, Input, Tooltip } from '@dolittle/design-system';
 
 const { metadata, createStory } = componentStories(Input, {
     actions: {
-        onChange: 'changed'
+        onChange: 'changed',
     },
     decorator: (Story) => (
         <Form initialValues={{
-            name: '',
-            nameWithDefault: 'Default Application Name',
+            withSideTooltip: 'Click to see tooltip...',
         }}>
-            <Story />
+            {Story()}
         </Form>
-    )
+    ),
 });
+
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+metadata.parameters = {
+    controls: { include: ['id', 'label', 'autoFocus', 'startAdornment', 'placeholder', 'disabled', 'required', 'pattern', 'onChange'] },
+};
+
+metadata.argTypes = {
+    autoFocus: {
+        table: { defaultValue: { summary: 'false' } },
+    },
+    startAdornment: {
+        options: ['Path', 'Search'],
+        mapping: {
+            Path: '/',
+            Search: <Search />,
+        },
+    },
+    disabled: {
+        table: { defaultValue: { summary: 'false' } },
+    },
+    required: {
+        control: { type: 'boolean' },
+        table: { defaultValue: { summary: 'false' } },
+    },
+    pattern: {
+        options: ['Email', 'Password'],
+        mapping: {
+            Email: emailRegex,
+            Password: passwordRegex,
+        },
+    },
+};
+
+metadata.args = {
+    id: 'defaultInput',
+    label: 'Default input',
+    autoFocus: false,
+    placeholder: undefined,
+    startAdornment: undefined,
+    disabled: false,
+    required: false,
+    pattern: undefined,
+};
 
 export default metadata;
 
-export const Default = createStory({
-    id: 'name',
-    label: 'Application Name',
-});
+export const Default = createStory();
 
 export const Required = createStory({
-    id: 'name',
-    label: 'Application Name',
+    id: 'requiredInput',
+    label: 'Required input',
     required: true,
 });
 
 export const RequiredWithCustomMessage = createStory({
-    id: 'name',
-    label: 'Application Name',
-    required: 'You must provide an application name',
+    id: 'requiredInputWithCustomMessage',
+    label: 'Custom required message',
+    required: 'You can provide a custom message string here.',
 });
 
-export const Disabled = createStory({
-    id: 'name',
-    label: 'Application Name',
-    disabled: true,
+export const RequiredWithCustomRegexPattern = createStory({
+    id: 'requiredWithCustomRegexPattern',
+    label: 'Custom regex pattern',
+    pattern: /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
+    required: 'Only lowercase letters and numbers are allowed.',
 });
 
-export const DisabledWithValue = createStory({
-    id: 'nameWithDefault',
-    label: 'Application Name',
-    disabled: true,
+export const WithDashedBorder = createStory({
+    id: 'dashedInput',
+    label: 'With dashed border',
+    dashedBorder: true,
 });
+
+export const WithSideTooltip = createStory({
+    id: 'withSideTooltip',
+    label: 'With side tooltip',
+});
+WithSideTooltip.decorators = [
+    (Story) => (
+        <Tooltip
+            id='inputTooltip'
+            tooltipTitle='Title'
+            tooltipText='Use this tooltip to provide additional information about the input.'
+        >
+            {Story()}
+        </Tooltip>
+    ),
+];
