2ff18e3c032019e5340f98f5ca91dffd0494691a
N00bG1rl <403helen@gmail.com>
2023-02-24 13:47:31 +0200

    Design System Link compenent improvements

--- Source/DesignSystem/atoms/Link/Link.tsx
+++ Source/DesignSystem/atoms/Link/Link.tsx
@@ -1,21 +1,21 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { ReactElement } from 'react';
+import React from 'react';
 
 import { Link as MuiLink } from '@mui/material';
 
 /**
  * The props for a {@link Link} component.
  */
-type LinkProps = {
+export type LinkProps = {
     /**
-     * Required. The URL to link to.
+     * The URL to link to.
      */
     href: string;
 
     /**
-     * Mark this as 'true' if the link opens in a new window or browser tab.
+     * Mark this as `true` if the link opens in a new window or browser tab.
      *
      * Also add an aria-label to inform screen reader users.
      * @default false
@@ -23,7 +23,7 @@ type LinkProps = {
     target?: boolean;
 
     /**
-     *  If the link opens in a new window or browser tab, add an aria-label to inform screen reader users.
+     *  If the link opens in a new window or browser tab, add an `ariaLabel` to inform screen reader users.
      * @default undefined
      */
     ariaLabel?: string;
@@ -32,31 +32,28 @@ type LinkProps = {
      * The color of the link.
      * @default primary
      */
-    color?: 'primary' | 'secondary' | 'text.primary' | 'text.secondary' | 'error';
+    color?: 'primary' | 'subtle' | 'secondary';
 
     /**
-     * Required. The text to display in the component.
+     * Link text to display.
      */
     message: string;
 };
 
 /**
- * A Link component.
- * @param {...LinkProps} props - The {@link LinkProps}.
- * @returns {ReactElement} A new {@link Link} component.
- * @example
- * <Link href='https://dolittle.io' message='Dolittle' ariaLabel='To learn more, visit our website which opens in a new window.' />
+ * The link component is used to navigate to a different page or to a different part of the current page.
+ * @param {LinkProps} props - The {@link LinkProps} that contains the properties for the link component.
+ * @returns A {@link Link} component.
  */
-export const Link = ({ href, ariaLabel, target, color, message }: LinkProps): ReactElement =>
+export const Link = ({ href, ariaLabel, target, color, message }: LinkProps) =>
     <MuiLink
-        color={color}
+        color={color === 'subtle' ? 'text.primary' : color}
         href={href}
         aria-label={ariaLabel}
         target={target ? '_blank' : undefined}
-        rel='noreferrer'
+        rel={target ? 'noopener noreferrer' : undefined}
         fontSize='inherit'
-        underline='hover'
-        sx={{ '&:hover': { opacity: 0.8 } }}
+        sx={{ '&:hover': { opacity: 0.9 } }}
     >
         {message}
     </MuiLink>;
