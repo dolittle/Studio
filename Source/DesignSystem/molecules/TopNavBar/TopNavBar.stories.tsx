3aed905f23eafa8664ec22c956f08dff7f67efac
N00bG1rl <403helen@gmail.com>
2023-03-01 10:21:34 +0200

    Create Top Nav Bar components

---
+++ Source/DesignSystem/molecules/TopNavBar/TopNavBar.stories.tsx
@@ -0,0 +1,12 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { componentStories, TopNavBar } from '@dolittle/design-system';
+
+const { metadata, createStory } = componentStories(TopNavBar);
+
+export default metadata;
+
+export const Default = createStory();
