759870df83043b267ce9274f33f7f0b62bdb9b52
N00bG1rl <403helen@gmail.com>
2023-03-03 16:38:41 +0200

    Add Icon component to Design System

---
+++ Source/DesignSystem/atoms/Icon/Icon.tsx
@@ -0,0 +1,17 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { SvgIcons, SvgIconsDefinition } from '@dolittle/design-system';
+
+type IconProps = {
+    icon: SvgIconsDefinition['icon'];
+    color?: 'inherit' | 'primary';
+    size?: 'small' | 'medium';
+};
+
+export const Icon = ({ icon, color = 'inherit', size = 'small' }: IconProps) => {
+    const clonedIcon = React.cloneElement(SvgIcons[icon], { fontSize: size, color });
+    return <>{clonedIcon}</>;
+};
