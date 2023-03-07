4fcb77b219fbcbf33f8c124ce7bcbc4364e8156d
Pavneet Singh Saund <pav.saund@gmail.com>
2023-03-07 15:48:47 +0000

    Add simple api to get connections

--- Source/SelfService/Web/integrations/connections/index.tsx
+++ Source/SelfService/Web/integrations/connections/index.tsx
@@ -2,11 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import React, { useEffect, useState } from 'react';
 
+import { Connection, connectionsGet } from '../../apis/integrations/connections';
 import { NoConnections } from './noConnections';
 import { Page } from '../../components/layout/page';
 
 export const Connections = () => {
     const [connections, setConnections] = useState<Connection[]>([]);
+
+    useEffect(() => {
+        connectionsGet()
+            .then(connections => {
+                setConnections(connections);
+            })
+            .catch(err => console.log('something went wrong', err));
+    }, []);
 
     return (
         <Page title='Connections'>
