d9d9b63bac0d34733a627dcdb23fe694eec04f95
N00bG1rl <403helen@gmail.com>
2023-03-08 18:54:55 +0200

    Add 'hover' & 'selected' colors to Card

--- Source/DesignSystem/molecules/Card/Card.tsx
+++ Source/DesignSystem/molecules/Card/Card.tsx
@@ -4,6 +4,7 @@
 import React, { useState } from 'react';
 
 import { Card as MuiCard, CardHeader, CardActionArea, CardActions, CardContent, Collapse, List, Typography } from '@mui/material';
+import { alpha } from '@mui/material/styles';
 
 import { Button, Icon } from '@dolittle/design-system';
 
@@ -11,27 +12,34 @@ export const Card = () => {
     const [isCardexpanded, setCardIsExpanded] = useState(false);
 
     return (
-        <MuiCard sx={{ maxWidth: 316 }} elevation={8}>
-            <CardActionArea
-                sx={{
-                    'px': 3,
-                    'py': 3.25,
-                }}
-                onClick={() => setCardIsExpanded(!isCardexpanded)}
-            >
+        <MuiCard
+            elevation={0}
+            sx={{
+                'maxWidth': 316,
+                'border': '1px solid',
+                'borderColor': isCardexpanded ? 'primary.main' : 'transparent',
+                'backgroundColor': isCardexpanded ? theme => `${alpha(theme.palette.primary.main, 0.10)}` : 'background.paper',
+                '&:hover': {
+                    backgroundColor: theme => `${alpha(theme.palette.primary.main, 0.16)}`,
+                    borderColor: 'primary.main',
+                },
+            }}
+        >
+            <CardActionArea onClick={() => setCardIsExpanded(!isCardexpanded)} sx={{ p: 3 }}>
                 <CardHeader avatar={<Icon color='primary' icon='DnsRounded' />} sx={{ p: 0, pb: 3.25 }} />
 
-                <CardContent sx={{ p: 0, pb: 3 }} >
+                <CardContent sx={{ p: 0, pb: 3 }}>
                     <Typography variant='h4' sx={{ pb: 3.5 }}>On Premise</Typography>
 
-                    <Typography variant='body1' sx={{ pb: 3 }}>
+                    <Typography variant='body1'>
                         Your team will be responsible for hosting, establishing backups and making sure the connector is running.
                     </Typography>
                 </CardContent>
 
                 <Collapse in={isCardexpanded} timeout='auto' unmountOnExit>
-                    <CardContent>
+                    <CardContent sx={{ p: 0, pb: 3 }}>
                         <Typography variant='subtitle2' gutterBottom>{`What you'll need`}</Typography>
+
                         {/* TODO: Map them */}
                         <List sx={{ listStyle: 'disc', px: 3, pb: 3 }}>
                             <li><Typography>Docker</Typography></li>
