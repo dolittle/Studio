3e4863534f16327007f5278ae418045cef4b51ff
N00bG1rl <403helen@gmail.com>
2023-03-08 16:13:44 +0200

    Hard coded Card

--- Source/DesignSystem/molecules/Card/Card.stories.tsx
+++ Source/DesignSystem/molecules/Card/Card.stories.tsx
@@ -1,3 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import { componentStories, Card } from '@dolittle/design-system';
+
+const { metadata, createStory } = componentStories(Card);
+
+export default metadata;
+
+export const Default = createStory();
