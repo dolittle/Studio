c68c6be29cd8a8f16bcaa71872fcb82cfa5c70e8
Pavneet Singh Saund <pav.saund@gmail.com>
2023-03-08 10:15:12 +0000

    Add well-known enum for react query cache keys

---
+++ Source/SelfService/Web/apis/integrations/CacheKeys.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export enum CACHE_KEYS {
+    Connections_GET= 'connections_get',
+    Connection_GET= 'connection_get',
+};
--- Source/SelfService/Web/apis/integrations/connections.hooks.ts
+++ Source/SelfService/Web/apis/integrations/connections.hooks.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import { useQuery } from '@tanstack/react-query';
+import { CACHE_KEYS } from './CacheKeys';
 import { connectionsGet } from './connections';
 
 export const useConnectionsGet = () => {
-    return useQuery(['connections'], connectionsGet);
+    return useQuery([CACHE_KEYS.Connections_GET], connectionsGet);
 };
 
