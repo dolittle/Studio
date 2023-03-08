526b5085d12114cbc27bf53cf6e43294cd18b30e
N00bG1rl <403helen@gmail.com>
2023-03-08 19:59:22 +0200

    Make Card reusable

--- Source/DesignSystem/molecules/Card/Card.stories.tsx
+++ Source/DesignSystem/molecules/Card/Card.stories.tsx
@@ -5,6 +5,22 @@ import { componentStories, Card } from '@dolittle/design-system';
 
 const { metadata, createStory } = componentStories(Card);
 
+metadata.args = {
+    icon: 'DnsRounded',
+    title: 'On Premise',
+    description: 'Your team will be responsible for hosting, establishing backups and making sure the connector is running.',
+    listTitle: `What you'll need`,
+    listItems: [
+        'Docker',
+        'Firewall access',
+        'Ion M3 (and optionally the meta data publisher)',
+        'Admin level access to M3',
+        'Approximately 16gb ram'
+    ],
+    footerTitle: 'Approximate setup time',
+    footerText: '10 min',
+};
+
 export default metadata;
 
 export const Default = createStory();
