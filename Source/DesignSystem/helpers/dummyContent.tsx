2f5d49ede3e6f4ecbdd3f54bf5bdd2b19a71ac20
N00bG1rl <403helen@gmail.com>
2023-03-08 02:05:52 +0200

    Add 'current route' to Layout

--- Source/DesignSystem/helpers/dummyContent.tsx
+++ Source/DesignSystem/helpers/dummyContent.tsx
@@ -3,12 +3,14 @@
 
 import React, { useState } from 'react';
 
+import { Route, Routes } from 'react-router-dom';
+
 import { Menu, Typography } from '@mui/material';
 import { ArrowDropDownRounded, ArrowDropUpRounded } from '@mui/icons-material';
 
 import { Button, IconButton, Icon } from '@dolittle/design-system';
 
-import { RouterLinkListItem } from './ReactRouter';
+import { Content, RouterLinkListItem } from './ReactRouter';
 
 export const MainLinks = () =>
     <>
@@ -19,9 +21,9 @@ export const MainLinks = () =>
 
 export const MobileSecondaryLinks = () =>
     <>
-        <RouterLinkListItem to='Secondary-link-1' text='Secondary link 1' icon={<Icon icon='DescriptionRounded' />} />
-        <RouterLinkListItem to='Secondary-link-2' text='Secondary link 2' icon={<Icon icon='SupervisedUserCircleRounded' />} />
-        <RouterLinkListItem to='Secondary-link-3' text='Secondary link 3' icon={<Icon icon='LogoutRounded' />} />
+        <RouterLinkListItem to='secondary-link-1' text='Secondary link 1' icon={<Icon icon='DescriptionRounded' />} />
+        <RouterLinkListItem to='secondary-link-2' text='Secondary link 2' icon={<Icon icon='SupervisedUserCircleRounded' />} />
+        <RouterLinkListItem to='secondary-link-3' text='Secondary link 3' icon={<Icon icon='LogoutRounded' />} />
     </>;
 
 export const MoreOptionsMenu = () => {
@@ -110,37 +112,43 @@ export const SideBarSecondaryLinks = () =>
     </>;
 
 export const DummyMainContent = () =>
-    <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
-        fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
-        aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
-        cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
-        at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
-        Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
-        numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
-        asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
-        assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
-        soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
-        ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-        soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
-        Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
-        delectus quo eius exercitationem tempore. Delectus sapiente, provident
-        corporis dolorum quibusdam aut beatae repellendus est labore quisquam
-        praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa
-        deleniti modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non
-        fugiat praesentium doloremque architecto laborum aliquid. Quae, maxime
-        recusandae? Eveniet dolore molestiae dicta blanditiis est expedita eius
-        debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
-        praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat
-        voluptate iure labore, repellendus beatae quia unde est aliquid dolor
-        molestias libero. Reiciendis similique exercitationem consequatur, nobis
-        placeat illo laudantium! Enim perferendis nulla soluta magni error,
-        provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui,
-        iure suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
-        Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore commodi
-        reprehenderit rerum reiciendis! Quidem alias repudiandae eaque eveniet
-        cumque nihil aliquam in expedita, impedit quas ipsum nesciunt ipsa ullam
-        consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
-        Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem
-        dolores sunt inventore perferendis, aut sapiente modi nesciunt.
-    </Typography>;
+    <>
+        <Routes>
+            <Route path='*' element={<Content />} />
+        </Routes>
+
+        <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
+            fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
+            aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
+            cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
+            at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
+            Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
+            numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
+            asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
+            assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
+            soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
+            ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
+            soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
+            Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
+            delectus quo eius exercitationem tempore. Delectus sapiente, provident
+            corporis dolorum quibusdam aut beatae repellendus est labore quisquam
+            praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa
+            deleniti modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non
+            fugiat praesentium doloremque architecto laborum aliquid. Quae, maxime
+            recusandae? Eveniet dolore molestiae dicta blanditiis est expedita eius
+            debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
+            praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat
+            voluptate iure labore, repellendus beatae quia unde est aliquid dolor
+            molestias libero. Reiciendis similique exercitationem consequatur, nobis
+            placeat illo laudantium! Enim perferendis nulla soluta magni error,
+            provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui,
+            iure suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
+            Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore commodi
+            reprehenderit rerum reiciendis! Quidem alias repudiandae eaque eveniet
+            cumque nihil aliquam in expedita, impedit quas ipsum nesciunt ipsa ullam
+            consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
+            Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem
+            dolores sunt inventore perferendis, aut sapiente modi nesciunt.
+        </Typography>
+    </>;
