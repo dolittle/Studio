5a9b6afa683e0772b611a52a848037bd190c113a
Pavneet Singh Saund <pav.saund@gmail.com>
2023-03-09 15:40:52 +0000

    Add generated api's for integrations based on Swagger/OpenAPI and typescript fetch

---
+++ Source/SelfService/Web/apis/integrations/generated/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ConfigurationApi';
+export * from './ConnectionsApi';
+export * from './EnvironmentApi';
+export * from './LoginApi';
+export * from './ProgramMetadataApi';
+export * from './TableMetadataApi';
