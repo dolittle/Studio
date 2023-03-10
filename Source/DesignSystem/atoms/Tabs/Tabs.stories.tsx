44aba3c8a2753b7b6dee1975b5932d64b07e39a7
N00bG1rl <403helen@gmail.com>
2023-03-10 19:06:28 +0200

    Improve Tabs component

--- Source/DesignSystem/atoms/Tabs/Tabs.stories.tsx
+++ Source/DesignSystem/atoms/Tabs/Tabs.stories.tsx
@@ -2,9 +2,33 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+
 import { componentStories, Tabs } from '@dolittle/design-system';
 
 const { metadata, createStory } = componentStories(Tabs);
+
+metadata.parameters = {
+    controls: { include: [] },
+    docs: {
+        source: {
+            code: `
+tabs: [
+        {
+            label: 'First tab',
+            render: () => <h1>Hello</h1>
+        },
+        {
+            label: 'Second tab',
+            render: () => <h1>World</h1>
+        },
+        {
+            label: 'Third tab',
+            render: () => <h1>Hello World!</h1>
+        },
+]`
+        },
+    },
+};
 
 export default metadata;
 
@@ -20,7 +44,7 @@ export const Normal = createStory({
         },
         {
             label: 'Third tab',
-            render: () => <h1>!</h1>
-        }
+            render: () => <h1>Hello World!</h1>
+        },
     ]
 });
