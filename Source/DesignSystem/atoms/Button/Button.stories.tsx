3bc905de1ce41dfdc4e0a4d8ce181aedadeab0f6
Liz <60577502+lizbrenner@users.noreply.github.com>
2023-03-14 14:07:13 +0100

    add descriptions to individual button stories

--- Source/DesignSystem/atoms/Button/Button.stories.tsx
+++ Source/DesignSystem/atoms/Button/Button.stories.tsx
@@ -13,14 +13,15 @@ import { AddCircle } from '@mui/icons-material';
 
 import { Button, ButtonProps, availableIcons, SvgIcons } from '@dolittle/design-system';
 
-import { buttonDescription, spacing } from './helpers';
+export const spacing = { '& button': { mr: 3, mb: 3 } };
 
 export default {
     title: 'Button',
     component: Button,
     parameters: {
         docs: {
-            description: { component: buttonDescription },
+            description: { component: `A button triggers an event or action. Its label should let the user know what will happen next. Buttons come in three style variations. 
+            All variations can be used with or without icons to help lift the UI and quickly visually communicate to the user what the button will do.` },
         },
     },
     argTypes: {
@@ -85,6 +86,17 @@ export const Text = () => (
 );
 Text.decorators = [Story => <Box sx={spacing}>{Story()}</Box>];
 
+Text.parameters = {
+    docs: {
+        description: {
+            story: `Text buttons are used for secondary actions, such as 'cancel' or to carry out an optional action within the page.
+            They are commonly found in dialogs, cards or sometimes toolbars. Text buttons may use our primary main color or the inherit color
+            of the page depending on whether or not the user's attention should be drawn to the button or if the button needs to be distinguished
+            from other content on the page`
+        }
+    }
+};
+
 export const Filled = () => (
     <>
         <Button label='primary' variant='filled' />
@@ -97,6 +109,16 @@ export const Filled = () => (
 );
 Filled.decorators = [Story => <Box sx={spacing}>{Story()}</Box>];
 
+Filled.parameters = {
+    docs: {
+        description: {
+            story: `Filled buttons are reserved for primary actions. They should be used to call attention to an action on a form or to
+            highlight the strongest call to action on the page. Filled buttons should appear only once per container (not including the
+            application header or in a modal dialog). Not every screen requires a primary action, or filled button. Filled buttons use our primary main color.`
+        }
+    }
+};
+
 export const Outlined = () => (
     <>
         <Button label='primary' variant='outlined' />
@@ -108,6 +130,14 @@ export const Outlined = () => (
     </>
 );
 Outlined.decorators = [Story => <Box sx={spacing}>{Story()}</Box>];
+
+Outlined.parameters = {
+    docs: {
+        description: {
+            story: `Outlined buttons are reserved for login screens. The empty fill allows third-party icons to be used in their original styling.`
+        }
+    }
+};
 
 export const Fullwidth = () =>
     <Button label='full width button with custom style' variant='fullwidth' startWithIcon={<AddCircle />} />;
