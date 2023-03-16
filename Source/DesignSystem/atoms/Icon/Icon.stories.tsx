e10ed8efb6ffaaf1f1a39107d92e2e7bb41fd07a
Liz <60577502+lizbrenner@users.noreply.github.com>
2023-03-16 12:06:54 +0100

    add icon guidelines

--- Source/DesignSystem/atoms/Icon/Icon.stories.tsx
+++ Source/DesignSystem/atoms/Icon/Icon.stories.tsx
@@ -1,2 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+//TODO: Implement component
+//metadata.parameters = {
+//     docs: {
+//         description: { component: `Icons are purely decorative and used to life the UI. Icons can help users more easily scan a long list. 
+//They can also be used to help separate groups of items or data such as cards or statuses. Avoid unneccessarily adding icons so as not to clutter the UI and overwhelm the user. 
+//Be sure to indicate its decorative nature in the ALT tag with a null text.` },
+//     },
+//     layout: 'centered',
+// };
