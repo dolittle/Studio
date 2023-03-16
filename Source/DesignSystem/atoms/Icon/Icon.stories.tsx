9089e38188854e3bc5b7adb1c5bf9ccafc047251
Liz <60577502+lizbrenner@users.noreply.github.com>
2023-03-16 15:46:24 +0100

    correct Github error messages

--- Source/DesignSystem/atoms/Icon/Icon.stories.tsx
+++ Source/DesignSystem/atoms/Icon/Icon.stories.tsx
@@ -4,9 +4,9 @@
 //TODO: Implement component
 //metadata.parameters = {
 //     docs: {
-//         description: { component: `Icons are purely decorative and used to life the UI. Icons can help users more easily scan a long list. 
-//They can also be used to help separate groups of items or data such as cards or statuses. Avoid unneccessarily adding icons so as not to clutter the UI and overwhelm the user. 
+//         description: { component: `Icons are purely decorative and used to life the UI. Icons can help users more easily scan a long list.
+//They can also be used to help separate groups of items or data such as cards or statuses. Avoid unneccessarily adding icons so as not to clutter the UI and overwhelm the user.
 //Be sure to indicate its decorative nature in the ALT tag with a null text.` },
 //     },
 //     layout: 'centered',
-// };
+//};
