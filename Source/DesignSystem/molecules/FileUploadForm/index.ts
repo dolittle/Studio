90676fc594ad59ace97d9913cef7198a88d68605
N00bG1rl <403helen@gmail.com>
2023-03-16 12:50:47 +0200

    Use FileUploadForm from Design System

--- Source/DesignSystem/molecules/FileUploadForm/index.ts
+++ Source/DesignSystem/molecules/FileUploadForm/index.ts
@@ -1,4 +1,4 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export { FileUploadForm, FileUploadFormProps } from './FileUploadForm';
+export { FileUploadForm, FileUploadFormProps, FileUploadFormRef } from './FileUploadForm';
