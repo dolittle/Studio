a5e76d97f9069d00d7f3cc2f5d5378eec2c4fb1b
N00bG1rl <403helen@gmail.com>
2023-03-16 23:06:56 +0200

    Improve FileUploadForm.stories

--- Source/DesignSystem/molecules/FileUploadForm/FileUploadForm.stories.tsx
+++ Source/DesignSystem/molecules/FileUploadForm/FileUploadForm.stories.tsx
@@ -1,10 +1,11 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { ComponentMeta } from '@storybook/react';
 
+import { List } from '@mui/material';
 import { UploadRounded } from '@mui/icons-material';
 
 import { Button, FileUploadForm, FileUploadFormProps } from '@dolittle/design-system';
@@ -15,9 +16,7 @@ export default {
     title: 'File Upload Form',
     component: FileUploadForm,
     parameters: {
-        controls: {
-            include: ['allowMultipleFiles', 'hideForm']
-        },
+        controls: { include: [] },
         docs: {
             description: {
                 component: `A file upload box is a component that allows the user to upload files to the system.`,
@@ -25,10 +24,8 @@ export default {
         },
     },
     args: {
+        ref: (fileUploadRef: FileUploadFormRef) => fileUploadRef,
         onSelected: file => console.log(file),
-        allowMultipleFiles: true,
-        hideForm: false,
-        validFileExtensions: [],
     },
 } as ComponentMeta<typeof FileUploadForm>;
 
@@ -36,11 +33,20 @@ export const Default = (args: FileUploadFormProps) => <FileUploadForm {...args} 
 
 export const HiddenForm = () => {
     const fileUploadRef = useRef<FileUploadFormRef>(null);
+    const [files, setFiles] = useState<File[]>([]);
+
+    const handleFileUpload = (selected: File | FileList) => {
+        const files = selected instanceof FileList ? Array.from(selected) : [selected];
+
+        for (const file of files) {
+            setFiles(prev => [...prev, file]);
+        }
+    };
 
     return (
         <>
             <Button
-                label='Upload file'
+                label='Upload file(s)'
                 type='submit'
                 startWithIcon={<UploadRounded />}
                 onClick={() => fileUploadRef.current?.showPrompt()}
@@ -48,32 +54,28 @@ export const HiddenForm = () => {
 
             <FileUploadForm
                 ref={fileUploadRef}
-                onSelected={file => console.log(file)}
+                onSelected={handleFileUpload}
                 allowMultipleFiles
                 hideForm
             />
+
+            {files &&
+                <List>
+                    {files.map(file => <li key={file.name}>{file.name}</li>)}
+                </List>
+            }
         </>
     );
 };
 
-export const WithValidFileExtensions = () => {
+export const WithValidation = () => {
     const fileUploadRef = useRef<FileUploadFormRef>(null);
 
     return (
-        <>
-            <Button
-                label='Upload file'
-                type='submit'
-                startWithIcon={<UploadRounded />}
-                onClick={() => fileUploadRef.current?.showPrompt()}
-            />
-
-            <FileUploadForm
-                ref={fileUploadRef}
-                onSelected={file => console.log(file)}
-                allowMultipleFiles
-                validFileExtensions={['json', 'yaml', 'yml']}
-            />
-        </>
+        <FileUploadForm
+            ref={fileUploadRef}
+            onSelected={file => console.log(file)}
+            validFileExtensions={['json', 'yaml', 'yml']}
+        />
     );
 };
