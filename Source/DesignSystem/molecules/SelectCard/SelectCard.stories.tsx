ca1536ab4257bb05f817a3051a2518f345d9de86
Liz <60577502+lizbrenner@users.noreply.github.com>
2023-03-17 14:56:29 +0100

    add select guidelines

--- Source/DesignSystem/molecules/SelectCard/SelectCard.stories.tsx
+++ Source/DesignSystem/molecules/SelectCard/SelectCard.stories.tsx
@@ -6,6 +6,17 @@ import { componentStories, SelectCard } from '@dolittle/design-system';
 const { metadata, createStory } = componentStories(SelectCard);
 
 metadata.title = 'Select Card';
+metadata.parameters = {
+    docs: {
+        description: { component: `Selection cards are used when the user needs to select an option but needs additional information in order to make an informed decision. 
+        The card can expand when the user clicks the select button, displaying more information that would help the user decide how to proceed with the task at hand. 
+        Cards are to be used as entry points to more complex scenarios. A common scenario for a selection card is when a decision fork needs to be made, such as at the start of a wizard. 
+        The card's UI changes based on its state: enabled, hovered, selected, disabled.
+**Structure:** A selection card contains an icon at the top (optional), card header, body content, and button. The button should indicate when the the card has been selected. 
+Clicking a selected card a second time should deselect the card. ` },
+    },
+};
+
 
 metadata.args = {
     icon: 'Dolittle',
