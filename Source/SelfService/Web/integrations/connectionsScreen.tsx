50c6432bd7a59c8c898e62463010eba317cc32d0
N00bG1rl <403helen@gmail.com>
2023-03-17 11:26:17 +0200

    Start m3 stepper wizard

--- Source/SelfService/Web/integrations/connectionsScreen.tsx
+++ Source/SelfService/Web/integrations/connectionsScreen.tsx
@@ -3,6 +3,8 @@
 
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import { Box, Grid, Typography } from '@mui/material';
 
 import { Button, SelectCard } from '@dolittle/design-system';
@@ -47,40 +49,44 @@ const dolittleCloudList = [
     'Admin level access to M3',
 ];
 
-export const ConnectionsScreen = () =>
-    <>
-        <Typography variant='h1'>New M3 Connection</Typography>
+export const ConnectionsScreen = () => {
+    const navigate = useNavigate();
 
-        <Typography variant='h4' sx={styles.subtitle}>Before we get started, where would you like to host your M3 connector?</Typography>
+    return (
+        <>
+            <Typography variant='h1'>New M3 Connection</Typography>
+            <Typography variant='h4' sx={styles.subtitle}>Before we get started, where would you like to host your M3 connector?</Typography>
 
-        <Grid container spacing={3} sx={styles.container}>
-            <Grid item>
-                <SelectCard
-                    icon='DnsRounded'
-                    title='On Premise'
-                    description='Your team will be responsible for hosting, establishing backups and making sure the connector is running.'
-                    listTitle={`What you'll need`}
-                    listItems={onPremiseList}
-                    footerTitle='Approximate setup time'
-                    footerText='10 min'
-                />
+            <Grid container spacing={3} sx={styles.container}>
+                <Grid item>
+                    <SelectCard
+                        icon='DnsRounded'
+                        title='On Premise'
+                        description='Your team will be responsible for hosting, establishing backups and making sure the connector is running.'
+                        listTitle={`What you'll need`}
+                        listItems={onPremiseList}
+                        footerTitle='Approximate setup time'
+                        footerText='10 min'
+                    />
+                </Grid>
+
+                <Grid item>
+                    <SelectCard
+                        icon='CloudRounded'
+                        title='In the Dolittle Cloud'
+                        description='We handle the hosting, establish backups and make sure the connector is running.'
+                        listTitle={`What you'll need`}
+                        listItems={dolittleCloudList}
+                        footerTitle='Approximate setup time'
+                        footerText='10 min'
+                    />
+                </Grid>
             </Grid>
 
-            <Grid item>
-                <SelectCard
-                    icon='CloudRounded'
-                    title='In the Dolittle Cloud'
-                    description='We handle the hosting, establish backups and make sure the connector is running.'
-                    listTitle={`What you'll need`}
-                    listItems={dolittleCloudList}
-                    footerTitle='Approximate setup time'
-                    footerText='10 min'
-                />
-            </Grid>
-        </Grid>
-
-        <Box sx={styles.footer}>
-            <Button label='cancel' color='subtle' onClick={() => { }} />
-            <Button label='get started' variant='filled' disabled onClick={() => { }} />
-        </Box>
-    </>;
+            <Box sx={styles.footer}>
+                <Button label='cancel' color='subtle' onClick={() => { }} />
+                <Button label='get started' variant='filled' onClick={() => navigate('wizard')} />
+            </Box>
+        </>
+    );
+};
---
+++ Source/SelfService/Web/integrations/wizard/steps/wizardStepTwo.tsx
@@ -0,0 +1,12 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { Stepper } from '@dolittle/design-system';
+
+export const WizardStepTwo = () => {
+    return (
+        <div>WizardStepTwo</div>
+    );
+};
