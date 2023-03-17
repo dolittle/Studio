52adcda915284258b2e7420b7c1a35eead35472b
Liz <60577502+lizbrenner@users.noreply.github.com>
2023-03-17 13:26:18 +0100

    add link guidelines

--- Source/DesignSystem/atoms/Link/Link.stories.tsx
+++ Source/DesignSystem/atoms/Link/Link.stories.tsx
@@ -14,6 +14,11 @@ metadata.args = {
 };
 
 export default metadata;
+metadata.parameters = {
+    docs: {
+        description: { component: `Links are used to navigate the user to another part of the application or to an external resource. There are three variants of links: external, secondary and subtle.` },
+    },
+};
 
 export const Default = createStory();
 
@@ -24,14 +29,41 @@ export const ExternalLink = createStory({
     message: 'Go to Dolittle website',
 });
 
+ExternalLink.parameters = {
+    docs: {
+        description: {
+            story: `External links are commonly used to reference external documentation or link to a support email. Primary links should be displayed in the primary color with an underline.
+            External links should always open in a new tab.`
+        }
+    }
+};
+
 export const SecondaryLink = createStory({
     href: '#',
     color: 'secondary',
     message: 'Informative link',
 });
 
+SecondaryLink.parameters = {
+    docs: {
+        description: {
+            story: `Secondary links link to additional information within the interface. They often provide supporting details or data a user can view. A common example is showing more rows of data within a card or data.
+            Secondary links should be displayed in the secondary color with an underline.`
+        }
+    }
+};
+
 export const SubtleLink = createStory({
     href: '#',
     color: 'subtle',
     message: 'Link, that is not so important',
 });
+
+SubtleLink.parameters = {
+    docs: {
+        description: {
+            story: `Subtle links can link to internal or external pages. They are used when we do not want to steal focus in the interface, especially when there is a lot of context for the user to read.
+            Subtle links should be displayed in the inherit color with an underline.`
+        }
+    }
+};
