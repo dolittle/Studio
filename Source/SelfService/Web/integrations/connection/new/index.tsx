b1870544742b84cb5b3d59c40b0d729729cfd219
Pavneet Singh Saund <pav.saund@gmail.com>
2023-03-22 11:29:10 +0000

    Move 'new connection' route to own folder

--- Source/SelfService/Web/integrations/connection/new/index.tsx
+++ Source/SelfService/Web/integrations/connection/new/index.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
-import { Page } from '../../components/layout/page';
+import { Page } from '../../../components/layout/page';
 
 export const NewConnection = () => {
 
