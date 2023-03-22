c424f10f6e4258f1acc92eb93e179303a5f17619
Pavneet Singh Saund <pav.saund@gmail.com>
2023-03-22 12:11:01 +0000

    Initial test component for Accordion List => needs to be moved to Design System

--- Source/SelfService/Web/integrations/accordion/components/newConnectionAccordion.tsx
+++ Source/SelfService/Web/integrations/accordion/components/newConnectionAccordion.tsx
@@ -6,13 +6,13 @@ import React, { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
 
 import { Accordion } from '@dolittle/design-system';
+import { AccordionProps } from 'Source/DesignSystem/atoms/Accordion/Accordion';
 
 type NewConnectionAccordionProps = {
     id: string;
     title: string;
     description?: string;
     children: React.ReactNode;
-
     onExpanded?: any;
 };
 
@@ -38,3 +38,39 @@ export const NewConnectionAccordion = ({ id, title, description, children }: New
         </Accordion>
     );
 };
+
+export type AccordionListProps = {
+    items: NewConnectionAccordionProps[];
+    singleExpandMode: boolean;
+};
+
+export const AccordionList = ({singleExpandMode, items}: AccordionListProps) => {
+
+    const [expanded, setExpanded] = useState<string | false>(false);
+
+    const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    return (
+        <>
+        {items.map(i => (
+            <Accordion
+                key={i.id}
+                id={i.id}
+                title={i.title}
+                expanded={singleExpandMode ? expanded === i.id : undefined}
+                onExpanded={handleChange(i.id)}
+                sx={{ mt: 8 }}
+            >
+                <Box sx={{ ml: 3 }}>
+                    <Typography sx={{ maxWidth: 660 }}>{i.description}</Typography>
+                    {i.children}
+                </Box>
+            </Accordion>
+        ))}
+        </>
+    );
+
+};
+
