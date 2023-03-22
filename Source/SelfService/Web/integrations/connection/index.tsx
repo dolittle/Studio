80de7ed8a2bac4a137e98cb8d6093093418ef72b
Pavneet Singh Saund <pav.saund@gmail.com>
2023-03-22 16:32:42 +0000

    Use the connection id query to get a single connection

--- Source/SelfService/Web/integrations/connection/index.tsx
+++ Source/SelfService/Web/integrations/connection/index.tsx
@@ -3,13 +3,16 @@
 
 import React from 'react';
 import { useParams, Outlet, useRoutes } from 'react-router-dom';
+import { useConnectionsIdGet } from '../../apis/integrations/connectionsApi.hooks';
 import { DebugRouter } from '../../components/debugRouter';
 import { Page } from '../../components/layout/page';
 import { routes } from './routes';
 
 export const Connection = () => {
-    const { connectionsId } = useParams();
+    const { connectionId } = useParams();
     const routesElement = useRoutes(routes);
+    const query = useConnectionsIdGet({ id: connectionId || '' });
+    console.log(query.data?.value);
 
     return (
         <DebugRouter>
