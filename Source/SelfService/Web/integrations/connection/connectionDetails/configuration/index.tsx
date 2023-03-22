faa2d84a822c0ccfea44981d4b814e9e3cb5c1a9
Pavneet Singh Saund <pav.saund@gmail.com>
2023-03-22 11:25:19 +0000

    Scaffold connection details sub components

---
+++ Source/SelfService/Web/integrations/connection/connectionDetails/configuration/index.tsx
@@ -0,0 +1,13 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+export const ConfigurationView = () => {
+    return (
+        <>
+            Configuration View
+        </>
+    );
+};
