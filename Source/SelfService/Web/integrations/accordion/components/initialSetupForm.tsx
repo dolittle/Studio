23d09e1f120aa4785bc0b4aef155f305b4741072
N00bG1rl <403helen@gmail.com>
2023-03-22 11:29:40 +0200

    Add Tooltips to Integration InitialSetupForm

--- Source/SelfService/Web/integrations/accordion/components/initialSetupForm.tsx
+++ Source/SelfService/Web/integrations/accordion/components/initialSetupForm.tsx
@@ -5,12 +5,21 @@ import React from 'react';
 
 import { Stack } from '@mui/material';
 
-import { Form, Input, Select } from '@dolittle/design-system';
+import { Form, Input, Select, Tooltip } from '@dolittle/design-system';
 
 const selectValues = [
     { value: 'On Premise', displayValue: 'On Premise' },
     { value: 'In the Dolittle Cloud', displayValue: 'In the Dolittle Cloud' }
 ];
+
+const ConnectorNameTooltipText = () =>
+    <>
+        Provide a name for this M3 connector instance. You can have multiple M3 connectors.
+        We recommend naming your connector based on its intended use. For example, <i>M3 Connector Test</i> or <i>M3 Connector Production</i>.
+    </>;
+
+const hostingTooltipText = `Currently, you can only setup the connection with on premise hosting. Soon, we will support setup in the 
+cloud where Dolittle takes care of hosting, establishing backups and making sure the connector is running.`;
 
 type InitialSetupParameters = {
     connectorName: string;
@@ -27,7 +36,12 @@ export const InitialSetupForm = () =>
         sx={{ mt: 6.5 }}
     >
         <Stack spacing={3.5}>
-            <Input id='connectorName' label='Connector Name' required />
-            <Select id='selectHosting' label='Hosting' options={selectValues} required disabled />
+            <Tooltip tooltipTitle='CONNECTOR NAME' tooltipText={<ConnectorNameTooltipText />}>
+                <Input id='connectorName' label='Connector Name' required />
+            </Tooltip>
+
+            <Tooltip tooltipTitle='HOSTING' tooltipText={hostingTooltipText} displayOnHover>
+                <Select id='selectHosting' label='Hosting' options={selectValues} required disabled />
+            </Tooltip>
         </Stack>
     </Form>;
