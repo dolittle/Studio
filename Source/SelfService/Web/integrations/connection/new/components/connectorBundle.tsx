699869b3086b221559be5c7da21a0530180379b1
N00bG1rl <403helen@gmail.com>
2023-03-24 17:40:21 +0200

    Replace Typography with MaxWidthTextBlock

--- Source/SelfService/Web/integrations/connection/new/components/connectorBundle.tsx
+++ Source/SelfService/Web/integrations/connection/new/components/connectorBundle.tsx
@@ -8,15 +8,14 @@ import { Typography } from '@mui/material';
 import { Button, Icon } from '@dolittle/design-system';
 
 import { CopyTextBox } from './copyTextBox';
-
-const hostConnectorBundleDescription = 'Download the connector bundle and run it. The bundle consists of a Docker compose file, shell file and README file with instructions.';
-const firewallConfigurationDescription = `You will most likely need to configure your organization's firewall access to enable connectivity with the bundle. If you are not
-responsible for firewall access, please share the information below with the responsible party:`;
+import { MaxWidthTextBlock } from './MaxWidthTextBlock';
 
 export const ConnectorBundle = () =>
     <>
         <Typography variant='subtitle2' gutterBottom>Download connector bundle</Typography>
-        <Typography sx={{ maxWidth: 660 }}>{hostConnectorBundleDescription}</Typography>
+        <MaxWidthTextBlock>
+            Download the connector bundle and run it. The bundle consists of a Docker compose file, shell file and README file with instructions.
+        </MaxWidthTextBlock>
 
         <Button
             label='Download Connector Bundle'
@@ -26,7 +25,10 @@ export const ConnectorBundle = () =>
         />
 
         <Typography variant='subtitle2' gutterBottom>Network firewall configuration</Typography>
-        <Typography sx={{ maxWidth: 660 }}>{firewallConfigurationDescription}</Typography>
+        <MaxWidthTextBlock>
+            {`You will most likely need to configure your organization's firewall access to enable connectivity with the bundle. If you are not
+                responsible for firewall access, please share the information below with the responsible party:`}
+        </MaxWidthTextBlock>
 
         <CopyTextBox />
     </>;
