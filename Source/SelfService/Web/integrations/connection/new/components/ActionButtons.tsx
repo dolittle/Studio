9ad687137462e0636ef1f19f8e5c8813ae03909c
N00bG1rl <403helen@gmail.com>
2023-03-24 12:02:33 +0200

    Wrap newM3Connection with Form

---
+++ Source/SelfService/Web/integrations/connection/new/components/ActionButtons.tsx
@@ -0,0 +1,26 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { Box } from '@mui/material';
+
+import { Button } from '@dolittle/design-system';
+
+export const ActionButtons = () =>
+    <Box sx={{ my: 5 }}>
+        <Button
+            label='Save connection'
+            // disabled
+            //onClick={() => handleSave()}
+            type='submit'
+            sx={{ mr: 3 }}
+        />
+
+        <Button
+            label='Start Mapping Data'
+            variant='filled'
+            disabled
+            href='#'
+        />
+    </Box>;
