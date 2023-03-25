f5d59c4d25e2c42a17e43f005ed176077f3631e8
N00bG1rl <403helen@gmail.com>
2023-03-26 01:41:33 +0200

    Add usePageTitle hook

---
+++ Source/SelfService/Web/utils/usePageTitle.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { useEffect } from 'react';
+
+export function usePageTitle(title: string): void {
+    useEffect(() => {
+        const prevTitle = document.title;
+        document.title = `${prevTitle} - ${title}`;
+
+        return () => {
+            document.title = prevTitle;
+        };
+    }, []);
+};
