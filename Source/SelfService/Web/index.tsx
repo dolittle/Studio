8d2d610ce5df883c55e9daf4171b2c3ab465e145
N00bG1rl <403helen@gmail.com>
2023-03-25 23:21:37 +0200

    Refactor main index.tsx

--- Source/SelfService/Web/index.tsx
+++ Source/SelfService/Web/index.tsx
@@ -2,16 +2,18 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 
 import { App } from './App';
 import { uriWithBasePathPrefix } from './store';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const root = createRoot(document.getElementById('root') as Element);
+
 root.render(
     <BrowserRouter basename={uriWithBasePathPrefix('')} >
         <QueryParamProvider adapter={ReactRouter6Adapter}>
