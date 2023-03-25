28d3364b0b7ece30ae6db0f0b276e6d0957dc25a
N00bG1rl <403helen@gmail.com>
2023-03-26 01:44:35 +0200

    Add usePageTitle hook to Page layout

--- Source/SelfService/Web/components/layout/page.tsx
+++ Source/SelfService/Web/components/layout/page.tsx
@@ -2,14 +2,19 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+
 import { Typography } from '@mui/material';
+
+import { usePageTitle } from '../../utils/usePageTitle';
 
 export type PageProps = {
     title: string;
     children?: React.ReactNode;
 };
 
-export const Page = ({title, children}: PageProps) => {
+export const Page = ({ title, children }: PageProps) => {
+    usePageTitle(title);
+
     return (
         <>
             <Typography variant='h1' my={2}>{title}</Typography>
@@ -17,4 +22,3 @@ export const Page = ({title, children}: PageProps) => {
         </>
     );
 };
-
