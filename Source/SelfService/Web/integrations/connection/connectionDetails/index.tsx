9545e0c76d2f57b317417ea0ba5bee6cba80c3f6
N00bG1rl <403helen@gmail.com>
2023-03-28 10:51:24 +0300

    Add MessageDetails Tabs

--- Source/SelfService/Web/integrations/connection/connectionDetails/index.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/index.tsx
@@ -2,14 +2,47 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+
+import { Outlet, useNavigate } from 'react-router-dom';
+
+import { useConnectionId } from '../../routes.hooks';
+
+import { Tabs } from '@dolittle/design-system';
+
+import { Page } from '../../../components/layout/page';
 
 export const ConnectionDetails = () => {
-    return (
-        <>
-            Connection Details
+    const navigate = useNavigate();
+    const connectionId = useConnectionId();
 
+    //console.log('connectionId', connectionId);
+
+    // Get connection name
+    // Show status
+
+    // TODO: Don't know what to do with `render` yet.
+    return (
+        <Page title='Connection Details'>
+            <Tabs
+                tabs={[
+                    {
+                        label: 'Configuration',
+                        handleNavigate: () => navigate('configuration'),
+                        render: () => <></>,
+                    },
+                    {
+                        label: 'Messages',
+                        handleNavigate: () => navigate('messages'),
+                        render: () => <></>,
+                    },
+                    {
+                        label: 'Expose Data',
+                        handleNavigate: () => navigate('expose'),
+                        render: () => <></>,
+                    },
+                ]}
+            />
             <Outlet />
-        </>
+        </Page>
     );
 };
