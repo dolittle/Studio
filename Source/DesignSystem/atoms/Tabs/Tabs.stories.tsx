a384ca8fdab3a79602b4d34ee163a96ae147e212
N00bG1rl <403helen@gmail.com>
2023-03-28 10:50:43 +0300

    Code cleanup

--- Source/DesignSystem/atoms/Tabs/Tabs.stories.tsx
+++ Source/DesignSystem/atoms/Tabs/Tabs.stories.tsx
@@ -10,44 +10,29 @@ const { metadata, createStory } = componentStories(Tabs);
 metadata.parameters = {
     controls: { include: [] },
     docs: {
-        description: { component: `Tabs organize and allow navigation between groups of content that are related and at the same level of hierarchy. 
-        Only one tab can be active at a time. The left most tab is activated by default when a user visits a page with tabs. 
-        Keep tab labels between one to two words at most. They should be descriptive enough to let the user know what content is contained within that tab. ` },
-        source: {
-            code: `
-tabs: [
-        {
-            label: 'First tab',
-            render: () => <h1>Hello</h1>
-        },
-        {
-            label: 'Second tab',
-            render: () => <h1>World</h1>
-        },
-        {
-            label: 'Third tab',
-            render: () => <h1>Hello World!</h1>
-        },
-]`
+        description: {
+            component: `Tabs organize and allow navigation between groups of content that are related and at the same level of hierarchy. 
+                        Only one tab can be active at a time. The left most tab is activated by default when a user visits a page with tabs. 
+                        Keep tab labels between one to two words at most. They should be descriptive enough to let the user know what content is contained within that tab.`
         },
     },
 };
 
 export default metadata;
 
-export const Normal = createStory({
+export const Default = createStory({
     tabs: [
         {
             label: 'First tab',
-            render: () => <h1>Hello</h1>
+            render: () => <h1>Hello</h1>,
         },
         {
             label: 'Second tab',
-            render: () => <h1>World</h1>
+            render: () => <h1>World</h1>,
         },
         {
             label: 'Third tab',
-            render: () => <h1>Hello World!</h1>
+            render: () => <h1>Hello World!</h1>,
         },
-    ]
+    ],
 });
