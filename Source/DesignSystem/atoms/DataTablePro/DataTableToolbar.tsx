5a2b7aeb4b417e7396aba82f7b8a60181cb6c550
N00bG1rl <403helen@gmail.com>
2023-03-30 09:08:10 +0300

    Add DataTableToolbar to DesignSystem

---
+++ Source/DesignSystem/atoms/DataTablePro/DataTableToolbar.tsx
@@ -0,0 +1,43 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { Box, Divider, Typography } from '@mui/material';
+
+import { Button, ButtonProps } from '@dolittle/design-system';
+
+const styles = {
+    wrapper: {
+        minHeight: 64,
+        px: 1.25,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: 4,
+    },
+    buttonGroup: {
+        display: 'flex',
+        gap: 4,
+    },
+    bottomDivider: {
+        mx: 1,
+        borderColor: 'outlineborder',
+    },
+};
+
+export type DataTableToolbarProps = {
+    title: string;
+    buttons: ButtonProps[];
+};
+
+export const DataTableToolbar = ({ title, buttons }: DataTableToolbarProps) =>
+    <>
+        <Box sx={styles.wrapper}>
+            <Typography variant='subtitle2' noWrap>{title}</Typography>
+            <Box sx={styles.buttonGroup}>
+                {buttons.map((button, index) => <Button key={index} {...button} />)}
+            </Box>
+        </Box>
+        <Divider sx={styles.bottomDivider} />
+    </>;
