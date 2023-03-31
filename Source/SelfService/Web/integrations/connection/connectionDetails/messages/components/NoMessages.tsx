4b743f62e09b003372baf467de8901b504cebc81
N00bG1rl <403helen@gmail.com>
2023-03-31 17:14:27 +0300

    Improve wording

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/components/NoMessages.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/components/NoMessages.tsx
@@ -13,13 +13,12 @@ export type NoMessagesProps = {
 
 export const NoMessages = ({ onCreateNew }: NoMessagesProps) =>
     <NoEntityView
-        title='No messages created yet...'
+        title='No message types have been created yet...'
         createEntityProps={{
-            createEntityText: 'Create new message',
+            createEntityText: 'create new message types',
             createEntityIcon: <Icon icon='MessageRounded' />,
             onCreateEntity: onCreateNew,
         }}
-        description='After you create your first message it will appear here.'
-    // subDescription='The Dolittle platform currently supports M3 integrations. We are looking to expand this in the future to other ERP systems,
-    //     so be sure to check back in or let us know what your needs are!'
+        description='Once you create your first message type, it will appear here.'
+        subDescription={`To create a new message type, click on the ‘create new message type' button above.`}
     />;
