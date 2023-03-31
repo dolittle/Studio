d7c0944fe0ac82a86ddf33355818508263acf448
N00bG1rl <403helen@gmail.com>
2023-03-31 15:47:34 +0300

    Style update

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/TableSearchSection.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/TableSearchSection.tsx
@@ -3,10 +3,12 @@
 
 import React, { useState } from 'react';
 import { useDebounce } from 'use-debounce';
+
 import { Box } from '@mui/material';
 import { Search } from '@mui/icons-material';
-import { Input } from '@dolittle/design-system';
 import { TableListingEntry } from '../../../../../../apis/integrations/generated';
+
+import { Input, Icon } from '@dolittle/design-system';
 import { useConnectionsIdMessageMappingsTablesSearchGet } from '../../../../../../apis/integrations/mappableTablesApi.hooks';
 import { useConnectionId } from '../../../../../routes.hooks';
 
@@ -25,7 +27,7 @@ export const TableSearchSection = (props: TableSearchSectionProps) => {
     const [selectedTable, setSelectedTable] = useState<TableListingEntry>();
     const [debouncedSearchTerm] = useDebounce(searchInput, 500);
 
-    const query = useConnectionsIdMessageMappingsTablesSearchGet({id: connectionId || '', search: debouncedSearchTerm });
+    const query = useConnectionsIdMessageMappingsTablesSearchGet({ id: connectionId || '', search: debouncedSearchTerm });
     const searchResults = query.data?.value || [];
     const showTable = selectedTable?.name !== '';
 
@@ -35,12 +37,13 @@ export const TableSearchSection = (props: TableSearchSectionProps) => {
         <ContentSection title='Browse M3 Table names'>
             <Input
                 id='table-search'
-                label='Search for table name'
-                startAdornment={<Search />}
+                label=''
+                startAdornment={<Box sx={{ display: 'flex', alignItems: 'center' }}><Icon icon='Search' /> MIT</Box>}
                 // fullWidth
                 onChange={(e) => { setSearchInput(e.target.value); }}
+                sx={{ width: 1, height: 56 }}
             />
-            <Box>
+            <Box sx={{ mt: 3 }}>
                 {/* SHOW SEARCH RESULTS HERE */}
                 Results: {query.data?.value?.length}
                 <TableSearchResults
--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/index.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/index.tsx
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+
 import { Outlet } from 'react-router-dom';
 
 export const MessagesViewRoot = () => {
@@ -9,4 +10,3 @@ export const MessagesViewRoot = () => {
         <Outlet />
     );
 };
-
