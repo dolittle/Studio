fca5291562b533ef749c2cbb18ca002200074f1e
N00bG1rl <403helen@gmail.com>
2023-04-03 14:30:55 +0300

    Show MessageMappingTable

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/TableSection.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/TableSection.tsx
@@ -3,12 +3,16 @@
 
 import React from 'react';
 
-import { Button, Icon } from '@dolittle/design-system/';
+import { AlertBox, Button, Icon } from '@dolittle/design-system/';
 
 import { TableListingEntry } from '../../../../../../apis/integrations/generated';
+import { useConnectionsIdMessageMappingsTablesTableGet } from '../../../../../../apis/integrations/mappableTablesApi.hooks';
+
+import { useConnectionId } from '../../../../../routes.hooks';
 
 import { ViewModeProps } from '../ViewMode';
 import { ContentSection } from './ContentSection';
+import { MessageMappingTable } from './MessageMappingTable';
 
 export type TableSectionProps = ViewModeProps & {
     selectedTable: TableListingEntry;
@@ -16,6 +20,21 @@ export type TableSectionProps = ViewModeProps & {
 };
 
 export const TableSection = (props: TableSectionProps) => {
+    const connectionId = useConnectionId();
+
+    if (!connectionId || !props.selectedTable.name) {
+        return (
+            <AlertBox />
+        );
+    };
+
+    const { data: mappableTableResult, isLoading } = useConnectionsIdMessageMappingsTablesTableGet({
+        id: connectionId,
+        table: props.selectedTable.name,
+    });
+
+    const mappableTableColumns = mappableTableResult?.value?.columns || [];
+
     return (
         <ContentSection
             title={`${props.selectedTable.name} Table`}
@@ -30,7 +49,10 @@ export const TableSection = (props: TableSectionProps) => {
                 />
             }
         >
-            Table component here
+            <MessageMappingTable
+                mappableTableColumns={mappableTableColumns}
+                isLoading={isLoading}
+            />
         </ContentSection>
     );
 };
