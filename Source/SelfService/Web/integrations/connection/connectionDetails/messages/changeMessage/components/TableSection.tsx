3c5f1c9368aa4121beecc855dfb4d859425d9792
Pavneet Singh Saund <pav.saund@gmail.com>
2023-04-04 15:30:26 +0100

    Successfully map the fiieldName changes from table into state

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/TableSection.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/TableSection.tsx
@@ -8,14 +8,14 @@ import { Grid, LinearProgress } from '@mui/material';
 import { AlertBox, Button, Icon, MaxWidthTextBlock, Switch } from '@dolittle/design-system/';
 import { GridSelectionModel } from '@mui/x-data-grid-pro';
 
-import { TableListingEntry } from '../../../../../../apis/integrations/generated';
+import { FieldMapping, TableListingEntry } from '../../../../../../apis/integrations/generated';
 import { useConnectionsIdMessageMappingsTablesTableGet } from '../../../../../../apis/integrations/mappableTablesApi.hooks';
 
 import { useConnectionId } from '../../../../../routes.hooks';
 
 import { ViewModeProps } from '../ViewMode';
 import { ContentSection } from './ContentSection';
-import { MessageMappingTable } from './MessageMappingTable';
+import { DataGridTableListingEntry, MessageMappingTable } from './MessageMappingTable';
 import { SubmitButtonSection } from './SubmitButtonSection';
 
 export type TableSectionProps = ViewModeProps & {
@@ -27,6 +27,7 @@ export const TableSection = (props: TableSectionProps) => {
     const connectionId = useConnectionId();
     const [selectedRowIds, setSelectedRowIds] = useState<GridSelectionModel>([]);
     const [hideUnselectedRows, setHideUnselectedRows] = useState(false);
+    const [mappedFields, setMappedFields] = useState<Map<string, FieldMapping>>(new Map());
 
     if (!connectionId || !props.selectedTable.name) return <AlertBox />;
 
@@ -39,10 +40,37 @@ export const TableSection = (props: TableSectionProps) => {
     const requiredTableColumns = mappableTableResult?.value?.required || [];
     const preselectedInitialIds = requiredTableColumns.map(required => required.m3ColumnName!);
     const selectedIds = (selectedRowIds.length > 0) ? selectedRowIds : preselectedInitialIds as GridSelectionModel;
+
+    const gridMappableTableColumns: DataGridTableListingEntry[] = useMemo(
+        () => allMappableTableColumns.map(column => {
+            const id = column.m3ColumnName!;
+            const fieldName = mappedFields.get(column.m3ColumnName!)?.fieldName || '';
+            console.log('mapping field', id);
+            console.log('mapping fieldName', fieldName);
+            console.log('Available mappings', Array.from(mappedFields.values()));
+            return {
+                id,
+                fieldName,
+                ...column,
+            };
+        }),
+        [allMappableTableColumns, mappedFields]);
     const selectedTableColumns = useMemo(
-        () => allMappableTableColumns.filter(column => selectedIds.includes(column.m3ColumnName!)),
+        () => gridMappableTableColumns.filter(column => selectedIds.includes(column.m3ColumnName!)),
         [allMappableTableColumns, selectedIds]
     );
+
+    const onFieldMapped = (m3Field: string, mappedFieldName: any) => {
+        setMappedFields((prevMappedFields) => {
+            const newMappedFields = new Map(prevMappedFields);
+            if (mappedFieldName) {
+                newMappedFields.set(m3Field, { columnName: m3Field, fieldName: mappedFieldName });
+            } else {
+                newMappedFields.delete(m3Field);
+            }
+            return newMappedFields;
+        });
+    };
 
     return (
         <>
@@ -70,11 +98,12 @@ export const TableSection = (props: TableSectionProps) => {
                                 <Switch id='hideUnselectedRows' label='Hide Unselected Rows' onChange={() => setHideUnselectedRows(!hideUnselectedRows)} />
                             </Grid>
                             <MessageMappingTable
-                                mappableTableColumns={hideUnselectedRows ? selectedTableColumns : allMappableTableColumns}
+                                dataGridListing={hideUnselectedRows ? selectedTableColumns : gridMappableTableColumns}
                                 isLoading={isLoading}
                                 selectedIds={selectedIds}
                                 disabledRows={preselectedInitialIds}
                                 onSelectedIdsChanged={setSelectedRowIds}
+                                onFieldMapped={onFieldMapped}
                             />
                             <SubmitButtonSection mode={props.mode} isSubmitting={false} />
                         </>
