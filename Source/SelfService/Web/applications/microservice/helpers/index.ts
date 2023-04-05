503ae9ebfe54034119c2c9d1bccc2fbef7473752
Pavneet Singh Saund <pav.saund@gmail.com>
2023-04-05 11:30:52 +0100

    Move practical date formating functions into utils so it's available for both modules

--- Source/SelfService/Web/applications/microservice/helpers/index.ts
+++ Source/SelfService/Web/applications/microservice/helpers/index.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 export { capitalize, removeStringPattern, removeStringPatternAndCapitalize, getRuntimeNumberFromString } from './strings';
-export { formatTime, formatStartingDate } from './dates';
+export { formatTime, formatStartingDate } from '../../../utils/dates';
 export { DownloadLogs } from './downloads';
 export { formatBytes } from './numbers';
 export { isAlphaNumeric } from './regex';
