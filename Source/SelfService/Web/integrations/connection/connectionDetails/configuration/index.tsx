8eaca0c50ba00aef3adc2bd10339d99c6ebe8b33
N00bG1rl <403helen@gmail.com>
2023-04-05 14:56:00 +0300

    Add Configuration Accordions

--- Source/SelfService/Web/integrations/connection/connectionDetails/configuration/index.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/configuration/index.tsx
@@ -5,11 +5,33 @@ import React from 'react';
 
 import { Box, Grid, Paper, Stack, Typography } from '@mui/material';
 
-import { Accordion, Button, Form, Icon, Input, Select } from '@dolittle/design-system';
+import { Accordion, Button, FileUploadForm, Form, Icon, Input, MaxWidthTextBlock, Select } from '@dolittle/design-system';
+
+import { TextCopyBox } from '../../new/components/TextCopyBox';
+
+export const instructions = [
+    `1. Open Infor ION API. Open the menu from the upper left corner and select 'Infor ION API'.`,
+    `2. Select 'Authorized Apps' from the left hand menu followed by the '+' icon to add a new account.`,
+    `3. Provide a name. Example: "Dolittle_Bridge".`,
+    `4. Under 'Type', select 'Backend Service'.`,
+    `5. Provide a description. Example: "Integration Connector".`,
+    `6. Toggle on 'Use Bridge Authentication. Optional: You can toggle on ‘User Impersonation' if you would like to monitor specific user activity.`,
+    `7. Click the save icon button.`,
+    `8. Scroll down and click 'Download Credentials' button. If you would like to use an account you've previously created, you can access the account via 'Authorized Apps' then selecting the account name.`,
+    `9. When the dialog pops up, toggle on 'Create Service Account' and provide a username from your M3 account you would like to associate with the ION service account.`,
+    `10. Last, click 'Download'. Upload the files below.`,
+];
+
+const InstructionsListItems = () =>
+    <Box sx={{ pl: 3, pt: 3 }}>
+        {instructions.map((item, index) => (
+            <MaxWidthTextBlock key={index} sx={{ mb: 2 }}>{item}</MaxWidthTextBlock>
+        ))}
+    </Box>;
 
 export const ConfigurationView = () => {
     return (
-        <Box sx={{ mt: 8, ml: 2 }}>
+        <Box sx={{ mt: 6, ml: 2 }}>
             <Typography variant='subtitle1'>Configuration Setup</Typography>
 
             <Box sx={{ display: 'flex', mt: 4, gap: 2 }}>
@@ -43,7 +65,25 @@ export const ConfigurationView = () => {
                     progressStatus='connected'
                     sx={{ mt: 8 }}
                 >
+                    <Typography variant='subtitle2' gutterBottom>Download connector bundle</Typography>
+                    <MaxWidthTextBlock>
+                        Download the connector bundle and run it. The bundle consists of a Docker compose file, shell file and README file with instructions.
+                    </MaxWidthTextBlock>
 
+                    <Button
+                        label='Download Connector Bundle'
+                        startWithIcon={<Icon icon='DownloadRounded' />}
+                        onClick={() => { }}
+                        sx={{ my: 3.5 }}
+                    />
+
+                    <Typography variant='subtitle2' gutterBottom>Network firewall configuration</Typography>
+                    <MaxWidthTextBlock>
+                        {`You will most likely need to configure your organization's firewall access to enable connectivity with the bundle. If you are not
+                            responsible for firewall access, please share the information below with the responsible party:`}
+                    </MaxWidthTextBlock>
+
+                    <TextCopyBox />
                 </Accordion>
 
                 <Accordion
@@ -52,7 +92,19 @@ export const ConfigurationView = () => {
                     progressStatus='connected'
                     sx={{ mt: 8 }}
                 >
+                    <MaxWidthTextBlock>
+                        This will allow us to access your service and provide the data, including custom data fields, needed to configure your application logic.
+                    </MaxWidthTextBlock>
 
+                    <Stack spacing={3.5} sx={{ mt: 3 }}>
+                        <Input
+                            id='metadataPublisherUrl'
+                            label='Your Metadata Publisher URL *'
+                            placeholder='Your metadata publisher URL goes here...'
+                            sx={{ width: 1, maxWidth: 500 }}
+                        />
+                        <Input id='metadataPublisherPassword' label='Password *' placeholder='Your password' />
+                    </Stack>
                 </Accordion>
 
                 <Accordion
@@ -61,7 +113,12 @@ export const ConfigurationView = () => {
                     progressStatus='connected'
                     sx={{ mt: 8 }}
                 >
+                    <MaxWidthTextBlock sx={{ mb: 2 }}>
+                        Follow the steps below then upload your credentials. If you already have an ION service account setup, skip to step 8 to access your credentials.
+                    </MaxWidthTextBlock>
 
+                    {/* <InstructionsListItems /> */}
+                    <FileUploadForm onSelected={file => console.log(file)} validFileExtensions={['json']} />
                 </Accordion>
 
             </Form>
