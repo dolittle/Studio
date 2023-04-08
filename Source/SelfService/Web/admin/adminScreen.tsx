df4895a47ca76b3ff3dcb85d0c834540ab17a75d
N00bG1rl <403helen@gmail.com>
2023-04-08 21:45:08 +0300

    Clean code in admin/adminScreen.tsx

--- Source/SelfService/Web/admin/adminScreen.tsx
+++ Source/SelfService/Web/admin/adminScreen.tsx
@@ -2,18 +2,18 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+
 import { Route, Routes, useNavigate, } from 'react-router-dom';
+
+import { Typography } from '@mui/material';
+
+import { Button } from '@dolittle/design-system';
 
 import { Create as CreateCustomer } from './customer/create';
 import { ViewAll as ViewAllCustomers } from './customer/viewAll';
 import { View as ViewCustomer } from './customer/view';
 import { View as ViewApplicationAccess } from './application/view';
-
 import { LayoutWithSidebar } from '../components/layout/layoutWithSidebar';
-
-import { Typography } from '@mui/material';
-
-import { Button } from '@dolittle/design-system';
 
 export const Screen: React.FunctionComponent = () => {
     const navigate = useNavigate();
@@ -22,10 +22,14 @@ export const Screen: React.FunctionComponent = () => {
     const welcome = (
         <>
             <Typography variant='h1' my={2}>Hello Admin</Typography>
-            <Button label='Take me to the Customers' onClick={() => {
-                const href = `/admin/customers`;
-                navigate(href);
-            }}
+            <Button
+                label='Take me to the Customers'
+                variant='filled'
+                endWithIcon='KeyboardDoubleArrowRight'
+                onClick={() => {
+                    const href = `/admin/customers`;
+                    navigate(href);
+                }}
             />
         </>
     );
@@ -34,15 +38,10 @@ export const Screen: React.FunctionComponent = () => {
         <LayoutWithSidebar navigation={nav}>
             <Routes>
                 <Route path="/" element={welcome} />
-
                 <Route path="/customers" element={<ViewAllCustomers />} />
-
                 <Route path="/customer/create" element={<CreateCustomer />} />
-
                 <Route path="/customer/:customerId" element={<ViewCustomer />} />
-
                 <Route path="/customer/:customerId/application/:applicationId/user/access" element={<ViewApplicationAccess />} />
-
             </Routes>
         </LayoutWithSidebar>
     );
