5d86e9fd5e1cfddbee2cb51d6bd45ad4b9538d2c
Pavneet Singh Saund <pav.saund@gmail.com>
2023-04-15 15:15:20 +0200

    Move the form contents to separate component

--- Source/SelfService/Web/integrations/connection/routes.tsx
+++ Source/SelfService/Web/integrations/connection/routes.tsx
@@ -10,7 +10,7 @@ import { ConfigurationView } from './connectionDetails/configuration';
 import { ExposeDataView } from './connectionDetails/exposeData';
 import { MessagesListView } from './connectionDetails/messages/messagesList';
 import { MessagesViewRoot } from './connectionDetails/messages';
-import { ChangeMessageView } from './connectionDetails/messages/changeMessage';
+import { Index as ChangeMessageView } from './connectionDetails/messages/changeMessage';
 
 import { NewConnectionView } from './new';
 
