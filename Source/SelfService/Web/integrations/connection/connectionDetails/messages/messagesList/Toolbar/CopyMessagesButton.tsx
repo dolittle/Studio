1eea78f0df80642e6971a375b4aeb0c4328fa3ea
Pavneet Singh Saund <pav.saund@gmail.com>
2023-04-18 10:05:36 +0100

    Show status indictor while copying messages

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/CopyMessagesButton.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/CopyMessagesButton.tsx
@@ -1,8 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
-import { Button } from '@dolittle/design-system';
+import React, { useState } from 'react';
+import { Button, StatusIndicator } from '@dolittle/design-system';
 import { TableToolbarButton } from './TableToolbarButton';
 
 export type CopyMessagesProps = TableToolbarButton & {
@@ -11,18 +11,34 @@ export type CopyMessagesProps = TableToolbarButton & {
 
 export const CopyMessagesButton = (props: CopyMessagesProps) => {
 
+    const [isLoading, setIsLoading] = useState(false);
+
     const hasSelectedMessages = props.selectedIds.length > 0;
     const hasMany = props.selectedIds.length > 1;
 
     const handleCopyMessages = () => {
-
+        setIsLoading(true);
+        // eslint-disable-next-line no-restricted-globals
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
     };
 
-
-    return <Button
-        label={`Copy message${hasMany ? 's' : ''} to...`}
-        startWithIcon='CopyAllRounded'
-        onClick={() => handleCopyMessages}
-        disabled={!hasSelectedMessages}
-    />;
+    return (
+        <>
+            {!isLoading
+                ? <Button
+                    label={`Copy message${hasMany ? 's' : ''} to...`}
+                    startWithIcon='CopyAllRounded'
+                    onClick={handleCopyMessages}
+                    disabled={!hasSelectedMessages}
+                />
+                :
+                <StatusIndicator
+                    label={`Copying message type${hasMany ? 's' : ''} to...`}
+                    status='waiting'
+                />
+            }
+        </>
+    );
 };
--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/TableToolbarButton.ts
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/TableToolbarButton.ts
@@ -4,4 +4,5 @@
 
 export type TableToolbarButton = {
     selectedIds: string[];
+    isButtonActionExecuting?: boolean
 };
