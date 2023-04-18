1dc9f61f9891547038c12e6d026c1ccceec88dbe
N00bG1rl <403helen@gmail.com>
2023-04-18 18:22:52 +0300

    Fix ExposeData layout

--- Source/SelfService/Web/integrations/connection/connectionDetails/exposeData/index.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/exposeData/index.tsx
@@ -5,9 +5,18 @@ import React, { useState } from 'react';
 
 import { useSnackbar } from 'notistack';
 
-import { Collapse, Divider, Grid, FormGroup, FormControlLabel, FormHelperText, Paper, Switch, TextField, Typography } from '@mui/material';
+import { Box, Collapse, Divider, FormGroup, FormControlLabel, FormHelperText, Grid, Paper, Switch, TextField, Typography } from '@mui/material';
 
 import { Button, IconButton, Link } from '@dolittle/design-system';
+
+const styles = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: { xs: 'column', sm: 'row' },
+    my: 3,
+    gap: 2,
+};
 
 const restApiUrl = 'https://ec456365-3784-4871-a39f-2dfed993e5bb.dolittle.cloud/mym3connector/';
 const openApiDocumentationUrl = 'https://supplierstuff.swagger.io/v2/swagger.json';
@@ -34,126 +43,90 @@ export const ExposeDataView = () => {
     };
 
     return (
-        <Paper sx={{ px: 2, maxWidth: 1200 }}>
-            <Grid container sx={{ my: 3, justifyContent: 'space-between', alignItems: 'center' }}>
-                <Grid item>
-                    <Typography variant='subtitle1'>Exposing your data</Typography>
-                </Grid>
+        <Paper sx={{ px: 2 }}>
+            <Box sx={styles}>
+                <Typography variant='subtitle1'>Exposing your data</Typography>
 
-                <Grid item>
-                    <FormGroup>
-                        <FormControlLabel control={<Switch />} label='Deploy service' sx={{ mx: 0 }} />
-                    </FormGroup>
-                </Grid>
-            </Grid>
+                <FormGroup>
+                    <FormControlLabel control={<Switch />} label='Deploy service' sx={{ mx: 0 }} />
+                </FormGroup>
+            </Box>
 
             <Divider sx={{ borderColor: 'outlineborder' }} />
 
-            <Grid container sx={{ my: 3 }}>
-                <Grid item>
-                    <Typography variant='subtitle2'>Rest API URL</Typography>
-                </Grid>
+            <Box sx={{ my: 3 }}>
+                <Typography variant='subtitle2'>Rest API URL</Typography>
 
-                <Grid container item spacing={1} sx={{ alignItems: 'center', pt: 1 }}>
-                    <Grid item>
-                        <Link target href='#' message={restApiUrl} />
-                    </Grid>
+                <Box sx={{ display: 'flex', alignItems: 'center', pt: 2, gap: 1 }}>
+                    <Link
+                        target
+                        href='#'
+                        message={restApiUrl}
+                    />
+                    <IconButton
+                        tooltipText='Copy rest API URL link to clipboard'
+                        icon='CopyAllRounded'
+                        color='primary'
+                        onClick={handleRestApiLinkCopy}
+                    />
+                </Box>
+            </Box>
 
-                    <Grid item>
-                        <IconButton
-                            tooltipText='Copy rest API URL link to clipboard'
-                            icon='CopyAllRounded'
-                            color='primary'
-                            onClick={handleRestApiLinkCopy}
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
+            <Box sx={{ my: 3 }}>
+                <Typography variant='subtitle2'>Rest API Documentation</Typography>
+                <Typography sx={{ pt: 1.5 }}>Our rest API is documented using OpenAPI.</Typography>
 
-            <Grid container sx={{ my: 3 }}>
-                <Grid container item>
-                    <Typography variant='subtitle2'>Rest API Documentation</Typography>
-                </Grid>
-
-                <Grid container item sx={{ pt: 1.5 }}>
-                    <Typography>Our rest API is documented using OpenAPI.</Typography>
-                </Grid>
-
-                <Grid container item spacing={1} sx={{ alignItems: 'center', pt: 1 }}>
-                    <Grid item>
-                        <Link
-                            target
-                            ariaLabel='OpenAPI documentation'
-                            href={openApiDocumentationUrl}
-                            message={openApiDocumentationUrl}
-                        />
-                    </Grid>
-
-                    <Grid item>
-                        <IconButton
-                            tooltipText='Copy OpenAPI documentation link to clipboard'
-                            icon='CopyAllRounded'
-                            color='primary'
-                            onClick={handleOpenApiDocumentationLinkCopy}
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
+                <Box sx={{ display: 'flex', alignItems: 'center', pt: 2, gap: 1 }}>
+                    <Link
+                        target
+                        ariaLabel='OpenAPI documentation'
+                        href={openApiDocumentationUrl}
+                        message={openApiDocumentationUrl}
+                    />
+                    <IconButton
+                        tooltipText='Copy OpenAPI documentation link to clipboard'
+                        icon='CopyAllRounded'
+                        color='primary'
+                        onClick={handleOpenApiDocumentationLinkCopy}
+                    />
+                </Box>
+            </Box>
 
             <Divider sx={{ borderColor: 'outlineborder' }} />
 
-            <Grid container sx={{ my: 3, justifyContent: 'space-between', alignItems: 'center' }}>
-                <Grid item>
-                    <Typography variant='subtitle2'>Credentials</Typography>
-                </Grid>
-
-                <Grid item>
-                    <Button label='Generate New Credentials' variant='outlined' onClick={() => setOpenCredentials(true)} />
-                </Grid>
-            </Grid>
+            <Box sx={styles}>
+                <Typography variant='subtitle2'>Credentials</Typography>
+                <Button label='Generate New Credentials' variant='outlined' onClick={() => setOpenCredentials(true)} />
+            </Box>
 
             <Collapse in={openCredentials}>
-                <Grid container spacing={3} sx={{ my: 2 }}>
-                    <Grid container item spacing={3}>
-                        <Grid item>
-                            <Typography sx={{ mb: 2 }}>Who or what are these credentials for?</Typography>
-                            <TextField id='credentialsName' label='Name' size='small' />
-                        </Grid>
-
-                        <Grid container item spacing={1} xs={6}>
-                            <Grid item>
-                                <Typography sx={{ mb: 2 }}>Credential Token</Typography>
-                                <TextField
-                                    id='credentialsToken'
-                                    label='Token'
-                                    size='small'
-                                    type='password'
-                                    value={credetialToken}
-                                    disabled
-                                    sx={{ width: 400 }}
-                                />
-                                <FormHelperText>This bearer token should be used in the request header.</FormHelperText>
-                            </Grid>
-
-                            <Grid item sx={{ alignSelf: 'center', mt: 2.5 }}>
-                                <Button
-                                    label='Copy Token'
-                                    startWithIcon='CopyAllRounded'
-                                    onClick={handleTokenCopy}
-                                />
-                            </Grid>
-                        </Grid>
+                <Grid container spacing={3} sx={{ pb: 5 }}>
+                    <Grid item>
+                        <Typography sx={{ mb: 2 }}>Who or what are these credentials for?</Typography>
+                        <TextField id='credentialsName' label='Name' size='small' />
                     </Grid>
 
-                    <Grid container item sx={{ justifyContent: 'flex-end', mt: 2 }}>
+                    <Grid item>
+                        <Typography sx={{ mb: 2 }}>Credential Token</Typography>
+                        <TextField
+                            id='credentialsToken'
+                            label='Token'
+                            size='small'
+                            type='password'
+                            value={credetialToken}
+                            disabled
+                            sx={{ width: 400 }}
+                        />
+                        <FormHelperText>This bearer token should be used in the request header.</FormHelperText>
+                    </Grid>
+
+                    <Grid container item spacing={3} xs={6} sx={{ alignItems: 'center', mt: 0 }}>
                         <Grid item>
-                            <Button
-                                label='Delete credentials'
-                                color='subtle'
-                                variant='outlined'
-                                startWithIcon='DeleteRounded'
-                                onClick={() => setOpenCredentials(false)}
-                            />
+                            <Button label='Copy Token' startWithIcon='CopyAllRounded' onClick={handleTokenCopy} />
+                        </Grid>
+
+                        <Grid item>
+                            <Button label='Delete credentials' startWithIcon='DeleteRounded' onClick={() => setOpenCredentials(false)} />
                         </Grid>
                     </Grid>
                 </Grid>
