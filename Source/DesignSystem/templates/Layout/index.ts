d1d9735f7a904d31fdadcaa940c1088d46f5a37b
N00bG1rl <403helen@gmail.com>
2023-04-19 12:33:03 +0300

    Add components to Design System

--- Source/DesignSystem/templates/Layout/index.ts
+++ Source/DesignSystem/templates/Layout/index.ts
@@ -2,3 +2,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 export { ContentContainer } from './ContentContainer';
+export { ContentDivider } from './ContentDivider';
+export { ContentHeader } from './ContentHeader';
