5549f1ca4781bdc97b0425b5402e2a050f35dcfa
Pavneet Singh Saund <pav.saund@gmail.com>
2023-04-19 11:02:18 +0100

    Use the batch api-method to deploy message types

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/DeployMessagesButton.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/DeployMessagesButton.tsx
@@ -6,7 +6,7 @@ import { enqueueSnackbar } from 'notistack';
 import { Button, StatusIndicator } from '@dolittle/design-system';
 import { UseMutationResult, useQueryClient } from '@tanstack/react-query';
 import { CACHE_KEYS } from '../../../../../../apis/integrations/CacheKeys';
-import { useConnectionsIdMessageMappingsTablesTableMessagesMessageDeployPost } from '../../../../../../apis/integrations/messageMappingApi.hooks';
+import { useConnectionsIdMessageMappingsTablesTableMessagesMessageDeployPost, useConnectionsIdMessageMappingsDeployPost } from '../../../../../../apis/integrations/messageMappingApi.hooks';
 import { ConnectionsIdMessageMappingsTablesTableMessagesMessageDeployPostRequest } from '../../../../../../apis/integrations/generated';
 import { TableToolbarButton } from './TableToolbarButton';
 
@@ -21,57 +21,32 @@ export const DeployMessagesButton = ({
     onSuccess,
     disable,
 }: DeployMessagesButtonProps) => {
-
-    const [numberOfRequests, setNumberOfRequests] = useState(0);
-    const [numberOfErrors, setNumberOfErrors] = useState(0);
-    const [batchSending, setBatchSending] = useState(false);
+    const deployMappingsMutation = useConnectionsIdMessageMappingsDeployPost();
+    const queryClient = useQueryClient();
     const hasSelectedMessages = selectedMessageTypes.length > 0;
     const hasMany = selectedMessageTypes.length > 1;
-    const isLoading = numberOfRequests > 0;
-    const queryClient = useQueryClient();
-
-    const deployMutation = useConnectionsIdMessageMappingsTablesTableMessagesMessageDeployPost({
-        onError: (error, variables, context) => {
-            // console.log('onError', variables);
-            setNumberOfErrors((oldNumber) => oldNumber + 1);
-            enqueueSnackbar(`Failed to deploy message type: ${variables.message}`, { variant: 'error' });
-        },
-
-        onSuccess: (data, variables, context) => {
-            // console.log('onSuccess', data, variables);
-        },
-
-        onSettled: (data, error, variables, context) => {
-            // console.log('onSettled',variables);
-            setNumberOfRequests((oldNumber) => oldNumber - 1);
-        },
-    });
+    const isLoading = deployMappingsMutation.isLoading;
 
 
     const handleDeployMessages = () => {
         onActionExecuting();
-        setBatchSending(true);
-        setNumberOfRequests(selectedMessageTypes.length);
-        selectedMessageTypes.forEach((messageType) => deployMutation.mutate({
+        deployMappingsMutation.mutate({
             id: connectionId,
-            table: messageType.fromTable?.name!,
-            message: messageType.name!,
-        }));
-    };
-
-    useEffect(() => {
-        if (batchSending && numberOfRequests === 0) {
-            if(numberOfErrors === 0) {
+            deployMapping: selectedMessageTypes.map(
+                (messageType) => ({ message: messageType.name, table: messageType.fromTable?.name })
+            )
+        }, {
+            onError(error, variables, context) {
+                enqueueSnackbar(`Failed to deploy message types: ${error}`, { variant: 'error' });
+                queryClient.invalidateQueries({ queryKey: [CACHE_KEYS.ConnectionMessageMappings_GET, connectionId] });
+            },
+            onSuccess(data, variables, context) {
                 enqueueSnackbar(`Message types${hasMany ? 's' : ''} successfully deployed`, { variant: 'success' });
+                onSuccess();
+                queryClient.invalidateQueries({ queryKey: [CACHE_KEYS.ConnectionMessageMappings_GET, connectionId] });
             }
-            setBatchSending(false);
-            setNumberOfErrors(0);
-            onSuccess();
-            queryClient.invalidateQueries({ queryKey: [CACHE_KEYS.ConnectionMessageMappings_GET, connectionId] });
-        };
-    }, [numberOfRequests, batchSending, numberOfErrors]);
-
-
+        });
+    };
 
     return (
         <>
@@ -80,7 +55,7 @@ export const DeployMessagesButton = ({
                     label={`Deploy message types${hasMany ? 's' : ''}...`}
                     startWithIcon='RocketLaunch'
                     onClick={handleDeployMessages}
-                    disabled={!hasSelectedMessages|| disable}
+                    disabled={!hasSelectedMessages || disable}
                 />
                 : <StatusIndicator
                     label={`Deploying message type${hasMany ? 's' : ''}...`}
