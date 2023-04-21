4fbfa6e3b6c27f4e9e815f6f0263babd44722ecc
Pavneet Singh Saund <pav.saund@gmail.com>
2023-04-21 13:11:09 +0100

    Use secondary option by passing in ref as props without forwardRef

--- Source/DesignSystem/atoms/Forms/Form.tsx
+++ Source/DesignSystem/atoms/Forms/Form.tsx
@@ -1,10 +1,13 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useMemo, ReactNode } from 'react';
-import { useForm, FieldValues, FormProvider, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
+
+import React, { useMemo, ReactNode, forwardRef, useImperativeHandle, ForwardedRef } from 'react';
+import { useForm, FieldValues, FormProvider, SubmitHandler, SubmitErrorHandler, UseFormReturn } from 'react-hook-form';
 
 import { Box, SxProps } from '@mui/material';
+
+export type FormRef<T extends FieldValues> = UseFormReturn<T, any>;
 
 /**
  * The props for a {@link Form} component.
@@ -34,6 +37,13 @@ export type FormProps<T extends FieldValues> = {
      * The sx prop lets you add custom styles to the component, overriding the styles defined by Material-UI.
      */
     sx?: SxProps;
+
+    /**
+     * Optional ref that contains the {@link UseFormReturn<T>} object returned from {@link useForm}.
+     * Practical for accessing the form's state from the parent component, and doing things like resetting the form.
+     * More info on approach here: https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forward_and_create_ref/#option-1---wrapper-component
+     */
+    fRef?: ForwardedRef<FormRef<T>>;
 };
 
 /**
@@ -41,12 +51,14 @@ export type FormProps<T extends FieldValues> = {
  * @param {FormProps} props The {@link FormProps} for the form.
  * @returns A {@link Form} component.
  */
-export const Form = <T extends FieldValues>({ initialValues, onSubmit, onSubmitInvalid, children, sx }: FormProps<T>) => {
+export const Form = <T extends FieldValues>({ initialValues, onSubmit, onSubmitInvalid, children, sx, fRef }: FormProps<T>) => {
     const methods = useForm<T>({
         mode: 'onTouched',
         defaultValues: initialValues as any,
     });
     const { handleSubmit } = methods;
+
+    useImperativeHandle(fRef, () => methods, [methods]);
 
     const handleFormSubmit = useMemo(() => handleSubmit(
         (data, event) => onSubmit?.(data, event),
@@ -61,3 +73,4 @@ export const Form = <T extends FieldValues>({ initialValues, onSubmit, onSubmitI
         </Box>
     );
 };
+
