983e896f25b488d8c6fca13ff020c408cfde84a0
Pavneet Singh Saund <pav.saund@gmail.com>
2023-04-25 09:43:34 +0100

    Wire up the Delete Multiple messages Button

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/DeleteMessagesButton.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/messagesList/Toolbar/DeleteMessagesButton.tsx
@@ -1,30 +1,53 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { enqueueSnackbar } from 'notistack';
+import { useQueryClient } from '@tanstack/react-query';
 import { Button, StatusIndicator } from '@dolittle/design-system';
+import { useConnectionsIdMessageMappingsDeleteMultiplePost } from '../../../../../../apis/integrations/messageMappingApi.hooks';
+import { CACHE_KEYS } from '../../../../../../apis/integrations/CacheKeys';
 import { TableToolbarButton } from './TableToolbarButton';
 
 export type DeleteMessagesProps = TableToolbarButton & {
 
 };
 
-export const DeleteMessagesButton = (props: DeleteMessagesProps) => {
-    const [isLoading, setIsLoading] = useState(false);
+export const DeleteMessagesButton = ({
+    connectionId,
+    selectedMessageTypes,
+    onActionExecuting,
+    onActionCompleted,
+    disable
+}: DeleteMessagesProps) => {
+    const deleteMultipleMutation = useConnectionsIdMessageMappingsDeleteMultiplePost();
+    const queryClient = useQueryClient();
+    const isLoading = deleteMultipleMutation.isLoading;
 
-    const hasSelectedMessages = props.selectedMessageTypes.length > 0;
-    const hasMany = props.selectedMessageTypes.length > 1;
+    const hasSelectedMessages = selectedMessageTypes.length > 0;
+    const hasMany = selectedMessageTypes.length > 1;
 
     const handleDeleteMessages = () => {
-        setIsLoading(true);
-        props.onActionExecuting();
-        // eslint-disable-next-line no-restricted-globals
-        setTimeout(() => {
-            setIsLoading(false);
-            enqueueSnackbar(`Message types${hasMany ? 's' : ''} successfully deleted`, { variant: 'success' });
-            props.onActionCompleted();
-        }, 2000);
+        onActionExecuting();
+        deleteMultipleMutation.mutate({
+            id: connectionId,
+            mappingReference: selectedMessageTypes.map(
+                (messageType) => ({ message: messageType.name, table: messageType.fromTable.name })
+            ),
+        }, {
+            onError(error, variables, context) {
+                enqueueSnackbar(`Failed to delete message types: ${error}`, { variant: 'error' });
+                //TODO: Handle error return object to mark which message types failed to delete
+                queryClient.invalidateQueries({ queryKey: [CACHE_KEYS.ConnectionMessageMappings_GET, connectionId] });
+            },
+            onSuccess(data, variables, context) {
+                enqueueSnackbar(`Message types${hasMany ? 's' : ''} successfully deleted`, { variant: 'success' });
+                queryClient.invalidateQueries({ queryKey: [CACHE_KEYS.ConnectionMessageMappings_GET, connectionId] });
+            },
+            onSettled() {
+                onActionCompleted();
+            }
+        });
     };
 
 
@@ -35,7 +58,7 @@ export const DeleteMessagesButton = (props: DeleteMessagesProps) => {
                     label={`Delete message type${hasMany ? 's' : ''}...`}
                     startWithIcon='DeleteRounded'
                     onClick={handleDeleteMessages}
-                    disabled={!hasSelectedMessages || props.disable}
+                    disabled={!hasSelectedMessages || disable}
                 />
                 :
                 <StatusIndicator
