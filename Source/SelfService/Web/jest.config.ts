a6264a920d7553e88e3f334da0482e56a3510a8b
Pavneet Singh Saund <pav.saund@gmail.com>
2023-05-05 11:00:25 +0200

    Add mocks for css or static types

--- Source/SelfService/Web/jest.config.ts
+++ Source/SelfService/Web/jest.config.ts
@@ -3,9 +3,15 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+    // verbose: true,
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleNameMapper:{
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+        '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts'
+    }
 };
 
 export default config;
