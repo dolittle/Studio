a4f465e348d49094a313f7924818d3858cc74d2c
N00bG1rl <403helen@gmail.com>
2023-05-10 20:14:56 +0300

    Add regex comment to 'regex' helper

--- Source/SelfService/Web/utils/helpers/regex.ts
+++ Source/SelfService/Web/utils/helpers/regex.ts
@@ -1,4 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+// Matches one or more consecutive characters that are not hyphens, underscores,
+// periods, letters (either uppercase or lowercase), or digits.
 export const isAlphaNumeric = /[^-._a-zA-Z0-9]+/;
