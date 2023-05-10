88a4b3e0d43720f7b9e9d92c009f740e5be6aa30
N00bG1rl <403helen@gmail.com>
2023-05-10 20:13:53 +0300

    Combine exports from 'strings' helper

--- Source/SelfService/Web/utils/helpers/index.ts
+++ Source/SelfService/Web/utils/helpers/index.ts
@@ -1,15 +1,9 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export {
-    capitalize,
-    removeStringPattern,
-    removeStringPatternAndCapitalize,
-    getRuntimeNumberFromString,
-    trimPrefix,
-    trimSuffix,
-} from './strings';
+export * from './strings';
 export { formatTime, formatStartingDate } from './dates';
+export { getConnectionHealthStatus } from './connectionStatuses';
 export { DownloadLogs } from './downloads';
 export { formatBytes } from './numbers';
 export { isAlphaNumeric } from './regex';
