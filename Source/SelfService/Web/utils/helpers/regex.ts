acdc0ac45517c1448a5e890dbba908a0a184283c
N00bG1rl <403helen@gmail.com>
2023-05-11 02:35:10 +0300

    Add 'alphanumericCharacter' into regex helper

--- Source/SelfService/Web/utils/helpers/regex.ts
+++ Source/SelfService/Web/utils/helpers/regex.ts
@@ -4,3 +4,6 @@
 // Matches one or more consecutive characters that are not hyphens, underscores,
 // periods, letters (either uppercase or lowercase), or digits.
 export const isAlphaNumeric = /[^-._a-zA-Z0-9]+/;
+
+// Matches one or more consecutive characters that are either uppercase letters, lowercase letters, or digits.
+export const alphanumericCharacter = /[a-zA-Z0-9]+/g;
