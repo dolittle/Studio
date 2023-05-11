d48fd174ebc222c95bae0ea96b0e34afac1dc814
Pavneet Singh Saund <pav.saund@gmail.com>
2023-05-11 12:46:47 +0100

    Style CredentialListItems

--- Source/SelfService/Web/integrations/connection/connectionDetails/exposeData/Credentials/CredentialsList.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/exposeData/Credentials/CredentialsList.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Button, ContentDivider } from '@dolittle/design-system';
 
 import { ServiceAccountListDto } from '../../../../../apis/integrations/generated';
@@ -19,13 +19,19 @@ export const CredentialsList = (props: CredentialsListProps) => {
         <>
             {props.credentials.map((credential, index) => (
                 <React.Fragment key={credential.serviceAccountName}>
-                    {index > 0 ? <ContentDivider /> : null}
-                    <Box>
-                        {credential.serviceAccountName}
-                        {credential.description}
-                        {formatDate(credential.createdAt!)}
-                        {credential.createdBy}
-                        <Button label='Delete credentials' startWithIcon='DeleteRounded' onClick={() => props.onDelete(credential.serviceAccountName!)} />
+                    {index > 0 ? <ContentDivider sx={{ my: 2 }} /> : null}
+                    <Box display='flex' flexDirection='column' gap={3} alignItems='flex-start'>
+                        <Box>
+                            Name: {credential.serviceAccountName}
+                            <br />
+                            Description: {credential.description}
+                        </Box>
+                        <Typography variant='body2' color='text.secondary'>These credentials were created on {formatDate(credential.createdAt!)}</Typography>
+                        <Button
+                            label='Delete credentials'
+                            variant='outlined'
+                            onClick={() => props.onDelete(credential.serviceAccountName!)}
+                        />
                     </Box>
                 </React.Fragment>
             ))}
