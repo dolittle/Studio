fffbb6e279b860acddd6a0b8d4b24540c0f2b779
Pavneet Singh Saund <pav.saund@gmail.com>
2023-05-11 16:49:37 +0100

    Remove duplicate Content components

--- Source/DesignSystem/templates/Layout/ContentDivider.tsx
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-import React from 'react';
-
-import { Divider, DividerProps } from '@mui/material';
-
-export const ContentDivider = (props: DividerProps) =>
-    <Divider sx={{ borderColor: 'outlineborder' }} {...props} />;
