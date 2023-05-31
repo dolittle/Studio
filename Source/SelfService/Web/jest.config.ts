fd5ecb76fff0fcd097ee8b559b65d1405da245e9
Pavneet Singh Saund <pav.saund@gmail.com>
2023-05-31 13:59:08 +0100

    try leaning on the default built in babel-jest transformer to compile typescript (and still failing)

--- Source/SelfService/Web/jest.config.ts
+++ Source/SelfService/Web/jest.config.ts
@@ -2,16 +2,39 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import type { Config } from 'jest';
 
+const libs = [
+    // 'DesignSystem',
+    '@react-hook/resize-observer',
+    '@juggle/resize-observer',
+];
+
+const transformIgnorePattern = 'node_modules/(?!' + libs.join('|') + ')';
+const transformPattern = 'node_modules/(' + libs.join('|') + ')';
+
+console.log(transformIgnorePattern);
+
+
 const config: Config = {
     // verbose: true,
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    moduleNameMapper:{
+    moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.ts',
+            '<rootDir>/__mocks__/fileMock.ts',
         '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts'
-    }
+    },
+    transform: {
+        // '\\.[jt]sx?$': 'ts-jest',
+        // transformPattern: 'babel-jest'
+    },
+    transformIgnorePatterns: [
+        transformIgnorePattern,
+    ],
+    testMatch: [
+        '**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/?(*.)+(spec|test).[jt]s?(x)',
+    ],
 };
 
 export default config;
