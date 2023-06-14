63c9e1f4def77866b939867631c08fde314e33c5
N00bG1rl <403helen@gmail.com>
2023-06-14 08:01:50 +0300

    Add minWidth to AigonFixingSvg

--- Source/DesignSystem/atoms/MaintenanceMessage/Building.tsx
+++ Source/DesignSystem/atoms/MaintenanceMessage/Building.tsx
@@ -12,5 +12,5 @@ export const Building = () =>
         <MaxWidthTextBlock variant='h1'>Aigon is currently improving this page.</MaxWidthTextBlock>
         <MaxWidthTextBlock variant='h5' sx={{ mt: 2 }}>We apologize for the inconvenience.</MaxWidthTextBlock>
         <MaxWidthTextBlock sx={{ my: 4 }}>Please give us a few hours to get this page up and running again.</MaxWidthTextBlock>
-        <AigonFixingSvg sx={{ width: 1, maxWidth: 300, height: 'auto' }} />
+        <AigonFixingSvg sx={{ width: 1, minWidth: 200, maxWidth: 300, height: 'auto' }} />
     </Stack>;
