09d88b2ac71600e3c9b2a919aa549d979f2937f6
N00bG1rl <403helen@gmail.com>
2023-06-14 07:52:51 +0300

    Add Design System Card to BackupsScreen

--- Source/SelfService/Web/applications/backupsScreen.tsx
+++ Source/SelfService/Web/applications/backupsScreen.tsx
@@ -6,9 +6,9 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes, useNavigate, generatePath } from 'react-router-dom';
 import { useGlobalContext } from '../context/globalContext';
 
-import { Grid, List, Typography } from '@mui/material';
+import { Box, Grid, List, Typography } from '@mui/material';
 
-import { Button, SimpleCard } from '@dolittle/design-system';
+import { Button, Building, SimpleCard } from '@dolittle/design-system';
 
 import { useRouteApplicationParams } from '../utils/route';
 import { getApplication, HttpResponseApplication } from '../apis/solutions/application';
@@ -20,9 +20,29 @@ import { ListView } from './backup/listView';
 import { BreadCrumbContainer } from '../components/layout/breadcrumbs';
 import { getMenuWithApplication, LayoutWithSidebar } from '../components/layout/layoutWithSidebar';
 
+// const dummy = [
+//     {
+//         name: 'Dev',
+//     },
+//     {
+//         name: 'Prod',
+//     },
+//     {
+//         name: 'Dev',
+//     },
+//     {
+//         name: 'Prod',
+//     },
+// ];
+
+type SimpleCardGridProps = {
+    data: any[];
+    applicationName: string;
+};
+
 export const BackupsScreen = () => {
     const navigate = useNavigate();
-    const { currentEnvironment, hasOneCustomer } = useGlobalContext();
+    const { currentEnvironment, hasOneCustomer, setCurrentEnvironment, setCurrentApplicationId } = useGlobalContext();
 
     const [application, setApplication] = useState({} as HttpResponseApplication);
     const [backupLinksForEnvironment, setBackupLinksForEnvironment] = useState<any[]>([]);
@@ -57,7 +77,7 @@ export const BackupsScreen = () => {
             .then(values => {
                 setBackupLinksForEnvironment(values);
             });
-    }, [application, environments]);
+    }, [environments]);
 
     if (!isLoaded) return null;
 
@@ -92,35 +112,59 @@ export const BackupsScreen = () => {
         },
     ];
 
+    const SimpleCardGrid = ({ data, applicationName }: SimpleCardGridProps) =>
+        <Box sx={{ maxWidth: 920 }}>
+            <Building />
+            <Grid container spacing={4} sx={{ mt: 4 }}>
+                {data.map(environment =>
+                    <Grid key={`${applicationName}-${environment.name}`} item xs={12} md={6}>
+                        <SimpleCard
+                            title={applicationName}
+                            subtitle={`${environment.name} - Environment`}//TODO: Fix this
+                            description={environment.name}
+                            actionButtons={
+                                <>
+                                    <Button
+                                        label='View all backups'
+                                        color='subtle'
+                                        onClick={() => {
+                                            setCurrentApplicationId(application.id);
+                                            setCurrentEnvironment(environment);
+
+                                            const href = `/backups/application/${application.id}/${environment}/list`;
+                                            navigate(href);
+                                        }}
+                                    />
+                                    <Button
+                                        label='Download latest Backup'
+                                        onClick={async (event: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>): Promise<void> => {
+                                            event.stopPropagation();
+
+                                            const url = await getLatestBackupLinkByApplication(application.id, environment);
+                                            window.open(url.url, '_blank');
+                                        }}
+                                    />
+                                </>
+                            }
+                        />
+                    </Grid>
+                )}
+
+                <List>
+                    {environments.map(environment => (
+                        <li key={environment.name}>
+                            <ViewCard application={application} environment={environment.name} />
+                        </li>
+                    ))}
+                </List>
+            </Grid>
+        </Box>;
+
     return (
         <LayoutWithSidebar navigation={nav}>
             <BreadCrumbContainer routes={routes} />
-
             <Routes>
-                <Route path="/overview" element={
-                    <Grid mt={4}>
-                        <List>
-                            {environments.map(environment => (
-                                <li key={environment.name}>
-                                    <ViewCard application={application} environment={environment.name} />
-                                </li>
-                            ))}
-                        </List>
-
-                        {backupLinksForEnvironment.map(environment => {
-                            if (!environment.url) return null;
-
-                            return (<SimpleCard
-                                key={environment.name}
-                                title={application.name}
-                                subtitle={environment.name}
-                                description={environment.url}
-                                actionButtons={<Button label='dkd' />}
-                            />
-                            );
-                        })}
-                    </Grid>
-                } />
+                <Route path="/overview" element={<SimpleCardGrid data={backupLinksForEnvironment} applicationName={application.name} />} />
                 <Route path="/:environment/list" element={<ListView application={application} environment={currentEnvironment} />} />
                 <Route element={<Typography variant='h1' my={2}>Something has gone wrong: backups.</Typography>} />
             </Routes>
