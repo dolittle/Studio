bb10dfa2d1f87e4bbbca2411cfd4d4906a851ae6
Pavneet Singh Saund <pav.saund@gmail.com>
2023-06-16 10:58:07 +0100

    Enable save button when there are changes and valid

--- Source/SelfService/Web/integrations/connection/new/components/ActionButtons.tsx
+++ Source/SelfService/Web/integrations/connection/new/components/ActionButtons.tsx
@@ -4,22 +4,24 @@
 import React from 'react';
 
 import { Box } from '@mui/material';
+import { useFormState } from 'react-hook-form';
 
 import { Button } from '@dolittle/design-system';
 
-export const ActionButtons = () =>
-    <Box sx={{ my: 5 }}>
+export const ActionButtons = () => {
+    const { isValid, isDirty, isSubmitting } = useFormState();
+
+    return <Box sx={{ my: 5 }}>
         <Button
             label='Save connection'
-            //disabled
+            disabled={!isValid || !isDirty || isSubmitting}
             type='submit'
-            sx={{ mr: 3 }}
-        />
+            sx={{ mr: 3 }} />
 
         <Button
             label='Start Mapping Data'
             variant='filled'
             disabled
-            href='#'
-        />
+            href='#' />
     </Box>;
+};
