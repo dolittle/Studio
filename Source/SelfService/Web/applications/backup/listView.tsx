c43b5c20cbc2b3316d8d2f545dfe4d3845174f8a
N00bG1rl <403helen@gmail.com>
2023-06-16 19:03:01 +0300

    Check Backups DataGrid date column

--- Source/SelfService/Web/applications/backup/listView.tsx
+++ Source/SelfService/Web/applications/backup/listView.tsx
@@ -10,21 +10,30 @@ import { BackupsForApplication, getBackupsByApplication } from '../../apis/solut
 
 import { BackupsDataGrid, BackupsDetailsList } from './backupsDataGrid';
 
+import { dateRegex, timeRegex } from '../../utils/helpers/regex';
+
 // Hack to get the date from the file name.
 const getDateFromFileName = (fileName: string) => {
+    if (typeof fileName !== 'string') return 'N/A';
+
     // Remove from string all characters that are not digits, underscores or dashes.
-    // From 'petripoint-dev-2023-06-14_05-29-14' to '----2023-06-14_05-29-14'.
+    // From '/petripoint-dev-backup/mongo/petripoint-dev-2023-06-14_05-29-14.gz.mongodump' to '----2023-06-14_05-29-14'.
     const cleanedFileName = fileName.replace(/[^\d_-]/g, '');
 
     // From '----2023-06-14_05-29-14' to ['----2023-06-14', '05-29-14'].
-    const seperateDateTime = cleanedFileName.split('_');
+    const dateAndTime = cleanedFileName.split('_');
 
     // Remove dashes from the beginning of the string.
     // From '['----2023-06-14', '05-29-14']' to '2023-06-14'.
-    const date = seperateDateTime[0].replace(/^\-+/gm, '');
+    const date = dateAndTime[0].replace(/^\-+/gm, '');
 
     // From '['----2023-06-14', '05-29-14']' to '05:29:14'.
-    const time = seperateDateTime[1].replace(/-/g, ':');
+    const time = dateAndTime[1].replace(/-/g, ':');
+
+    // Check if the date and time are valid.
+    if (!dateRegex.test(date) || !timeRegex.test(time)) {
+        return 'N/A';
+    }
 
     return `${date} at ${time}`;
 };
