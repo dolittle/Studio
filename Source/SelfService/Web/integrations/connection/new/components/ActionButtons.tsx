50a7acaa7a6693a7871963dbb88ffc441a442f3f
Pavneet Singh Saund <pav.saund@gmail.com>
2023-06-19 11:44:20 +0100

    Enable "Start Message Mapping" button when conneciton is Connected

--- Source/SelfService/Web/integrations/connection/new/components/ActionButtons.tsx
+++ Source/SelfService/Web/integrations/connection/new/components/ActionButtons.tsx
@@ -4,24 +4,33 @@
 import React from 'react';
 
 import { Box } from '@mui/material';
+import { useHref } from 'react-router-dom';
 import { useFormState } from 'react-hook-form';
-
 import { Button } from '@dolittle/design-system';
+import { ConnectionModel } from '../../../../apis/integrations/generated';
 
-export const ActionButtons = () => {
+export type ActionButtonsProps = {
+    connection: ConnectionModel;
+
+};
+
+export const ActionButtons = ({ connection }: ActionButtonsProps) => {
     const { isValid, isDirty, isSubmitting } = useFormState();
+    const messageHref = useHref('../messages');
+
+    const isConnected = connection?.status.name === 'Connected';
 
     return <Box sx={{ my: 5 }}>
         <Button
             label='Save connection'
-            disabled={!isValid || !isDirty || isSubmitting}
+            disabled={!isValid || !isDirty || isSubmitting }
             type='submit'
             sx={{ mr: 3 }} />
 
         <Button
             label='Start Mapping Data'
             variant='filled'
-            disabled
-            href='#' />
+            disabled={isConnected}
+            href={messageHref} />
     </Box>;
 };
