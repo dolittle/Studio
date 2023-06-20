5ea1c5b84539ab6445dac0e017fd6c3fec785ab4
N00bG1rl <403helen@gmail.com>
2023-06-20 08:38:58 +0300

    Refactor code

--- Source/DesignSystem/templates/Layout/Content/ContentContainer.stories.tsx
+++ Source/DesignSystem/templates/Layout/Content/ContentContainer.stories.tsx
@@ -1,16 +1,16 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
+// import React from 'react';
 
-import { componentStories, ContentContainer } from '@dolittle/design-system';
+// import { componentStories, ContentContainer } from '@dolittle/design-system';
 
-const { metadata, createStory } = componentStories(ContentContainer);
+// const { metadata, createStory } = componentStories(ContentContainer);
 
-metadata.args = {
-    children: 'ContentContainer',
-};
+// metadata.args = {
+//     children: 'ContentContainer',
+// };
 
-export default metadata;
+// export default metadata;
 
-export const Default = createStory();
+// export const Default = createStory();
