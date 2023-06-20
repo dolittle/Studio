2b6518228face2df9f182fb6492f9302cfe4eda8
Pavneet Singh Saund <pav.saund@gmail.com>
2023-06-20 13:03:57 +0100

    Remove whitesapce

--- Source/SelfService/Web/integrations/connection/new/components/ConnectorBundleConfiguration.tsx
+++ Source/SelfService/Web/integrations/connection/new/components/ConnectorBundleConfiguration.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 import React from 'react';
 
 import { Typography } from '@mui/material';
