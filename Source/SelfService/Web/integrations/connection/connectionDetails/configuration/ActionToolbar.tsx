9e1fc2525234e18ac4091e8364b47fdd7ce7bbc1
Pavneet Singh Saund <pav.saund@gmail.com>
2023-06-22 13:46:10 +0100

    Trigger save on the "next tick" with a useEffect

--- Source/SelfService/Web/integrations/connection/connectionDetails/configuration/ActionToolbar.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/configuration/ActionToolbar.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Box } from '@mui/material';
 import { useFormState } from 'react-hook-form';
@@ -19,6 +19,14 @@ export type ActionToolbarProps = {
 
 export const ActionToolbar = ({ onEdit, onDelete, onCancel, onSave, canEdit = false }: ActionToolbarProps) => {
     const { isValid, isDirty } = useFormState();
+    const [saved, setSaved] = useState(false);
+
+    // Delay triggering onSave until after the default click handler has propagated with the submit button
+    useEffect(() => {
+        if (saved) {
+            onSave();
+        }
+    }, [saved]);
 
     return (
         <Box sx={{ display: 'flex', mt: 4, gap: 2 }}>
@@ -30,7 +38,11 @@ export const ActionToolbar = ({ onEdit, onDelete, onCancel, onSave, canEdit = fa
                 startWithIcon='SaveRounded'
                 disabled={!canEdit || !isDirty || !isValid}
                 type='submit'
-                onClick={onSave}
+                onClick={(event) => {
+                    setSaved(false);
+                    setSaved(true);
+                    return event;
+                }}
             />
             <Button label='Delete Connection' startWithIcon='DeleteRounded' onClick={onDelete} />
         </Box>
