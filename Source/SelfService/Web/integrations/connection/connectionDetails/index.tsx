9442f8a5ef6338985dfb9c445c4ccea97ea910d9
Pavneet Singh Saund <pav.saund@gmail.com>
2023-06-23 09:25:08 +0100

    Redirect to correct tab when loading connection screen

--- Source/SelfService/Web/integrations/connection/connectionDetails/index.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/index.tsx
@@ -1,18 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
-
-import { Outlet, Link, useLocation, Location } from 'react-router-dom';
-
+import React, { useMemo } from 'react';
+import { Link, Location, Outlet, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { useConnectionsIdGet } from '../../../apis/integrations/connectionsApi.hooks';
-
 import { useConnectionId } from '../../routes.hooks';
-
 import { Tabs } from '@dolittle/design-system';
-
 import { getConnectionStatus } from '../../../utils/helpers/connectionStatuses';
-
 import { Page } from '../../../components/layout/page';
 
 //TODO: Make this dynamic based on current url in the tab component
@@ -50,11 +44,24 @@ const tabs = [
     },
 ];
 
+const pendingStatuses = ['registered', 'pending'];
+const childRoutePaths = ['configuration', 'messages', 'expose'];
+
 export const ConnectionDetails = () => {
     const location = useLocation();
     const connectionId = useConnectionId();
     const query = useConnectionsIdGet({ id: connectionId || '' });
     const data = query.data?.value;
+
+    const redirectUrl = useMemo(() => {
+        if (!query.data?.value || childRoutePaths.some((path) => location.pathname.endsWith(path))) {
+            return null;
+        } else {
+            return pendingStatuses.includes(query.data.value.status?.name?.toLowerCase())
+                ? 'configuration'
+                : 'messages';
+        }
+    }, [query.data?.value, location.pathname]);
 
     if (query.isLoading) return <>Loading</>;
     if (!data || !connectionId) return null;
@@ -62,15 +69,21 @@ export const ConnectionDetails = () => {
     const pageTitle = data.name || 'Connection Details';
     const status = data.status?.name?.toLocaleLowerCase();
 
+
     return (
-        <Page
-            title={pageTitle}
-            healthStatus={getConnectionStatus(status).status}
-            healthStatusLabel={getConnectionStatus(status).label}
-            sx={{ mb: 4 }}
-        >
-            <Tabs selectedTab={getCurrentTab(location)} tabs={tabs} />
-            <Outlet />
-        </Page>
+        <>
+            {redirectUrl
+                ? <Navigate to={redirectUrl} replace={true} />
+                : <Page
+                    title={pageTitle}
+                    healthStatus={getConnectionStatus(status).status}
+                    healthStatusLabel={getConnectionStatus(status).label}
+                    sx={{ mb: 4 }}
+                >
+                    <Tabs selectedTab={getCurrentTab(location)} tabs={tabs} />
+                    <Outlet />
+                </Page>
+            }
+        </>
     );
 };
--- Source/SelfService/Web/integrations/connection/index.tsx
+++ Source/SelfService/Web/integrations/connection/index.tsx
@@ -1,33 +1,20 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useEffect } from 'react';
-
-import { useRoutes, useNavigate, useLocation } from 'react-router-dom';
-
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
 import { routes } from './routes';
 import { useConnectionId } from '../routes.hooks';
-
 import { useConnectionsIdGet } from '../../apis/integrations/connectionsApi.hooks';
 import { DebugRouter } from '../../components/debugRouter';
 
-const pendingStatuses = ['registered', 'pending'];
 
 export const Connection = () => {
     const connectionId = useConnectionId();
     const routesElement = useRoutes(routes);
     const query = useConnectionsIdGet({ id: connectionId || '' });
-    // const useNavigateIfStatusIsPending
-    const location = useLocation();
-    const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     if (query.data?.value && !location.pathname.includes('new')) {
-    //         if (pendingStatuses.includes(query.data.value.status?.name?.toLowerCase() || '')) {
-    //             navigate('new', { replace: true });
-    //         }
-    //     }
-    // }, [query.data, location.pathname]);
+
 
     if (query.isError) return <>Something went wrong. Could not load connection details for {connectionId}</>;
 
