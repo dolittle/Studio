524bdc6e87c983583b76e5a85af4c9ee45df09ec
N00bG1rl <403helen@gmail.com>
2023-06-28 13:18:10 +0300

    Check if the resulting page title is correct

--- Source/SelfService/Web/utils/usePageTitle.ts
+++ Source/SelfService/Web/utils/usePageTitle.ts
@@ -4,6 +4,8 @@
 import { useEffect } from 'react';
 
 export function usePageTitle(title: string): void {
+    if (title === '' || title === undefined || title === null) return;
+
     useEffect(() => {
         const prevTitle = document.title;
         document.title = `${title} - ${prevTitle}`;
