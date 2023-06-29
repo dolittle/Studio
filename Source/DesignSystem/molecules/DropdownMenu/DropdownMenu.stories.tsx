1717202a51652688ea9aad8ff33a5b681377e489
N00bG1rl <403helen@gmail.com>
2023-06-29 23:40:31 +0300

    Updated DropdownMenu component

--- Source/DesignSystem/molecules/DropdownMenu/DropdownMenu.stories.tsx
+++ Source/DesignSystem/molecules/DropdownMenu/DropdownMenu.stories.tsx
@@ -1,24 +1,58 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { componentStories, DropdownMenu } from '@dolittle/design-system';
+import React, { useState } from 'react';
+
+import { action } from '@storybook/addon-actions';
+
+import { componentStories, DropdownMenu, DropdownMenuProps } from '../../index';
 
 const { metadata, createStory } = componentStories(DropdownMenu);
 
-import { menuItems } from '../../helpers/DummyContents/DummyNavigationItems';
+const items: DropdownMenuProps['menuItems'] = [
+    {
+        id: '1',
+        label: 'Item 1',
+        onMenuItemSelect: action('Clicked Item 1'),
+    },
+    {
+        id: '2',
+        label: 'Item 2',
+        onMenuItemSelect: action('Clicked Item 2'),
+    },
+    {
+        id: '3',
+        label: 'Item 3',
+        icon: 'AigonixLightCube',
+        onMenuItemSelect: action('Clicked Item 3'),
+    },
+];
 
 metadata.title = 'Dropdown Menu';
 
 metadata.args = {
-    label: 'More options',
-    menuItems,
+    id: 'default-dropdown-menu',
+    menuItems: items,
     iconDropdown: false,
+    selected: '',
 };
 
 export default metadata;
 
 export const Default = createStory();
 
-export const IconDropdown = createStory({
+export const IconDropdownMenu = createStory({
     iconDropdown: true,
 });
+
+export const WithSelection = () => {
+    const [selected, setSelected] = useState('Item 1');
+
+    return (
+        <DropdownMenu
+            id='selected-dropdown-menu'
+            menuItems={items.map(item => ({ ...item, onMenuItemSelect: () => setSelected(item.label) }))}
+            selected={selected}
+        />
+    );
+};
