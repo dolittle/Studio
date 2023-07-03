8481d628f9406b99186299e46750f0445efa498e
N00bG1rl <403helen@gmail.com>
2023-07-03 17:13:13 +0300

    Remove old content

--- Source/DesignSystem/molecules/SideBar/StyledCompenents.tsx
+++ Source/DesignSystem/molecules/SideBar/StyledCompenents.tsx
@@ -1,10 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
-
 import { styled, Theme, CSSObject } from '@mui/material/styles';
-import { Drawer as MuiDrawer, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Drawer as MuiDrawer } from '@mui/material';
 
 const drawerWidth = 240;
 
@@ -23,28 +21,8 @@ const closedMixin = (theme: Theme): CSSObject => ({
         duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
+    width: `calc(${theme.spacing(6.5)} + 1px)`,
 });
-
-type CustomListItemProps = {
-    icon: React.ReactElement;
-    text: string;
-    onClick?: () => void;
-};
-
-export const CustomListItem = ({ icon, text, onClick }: CustomListItemProps) =>
-    <ListItem disablePadding sx={{ minHeight: 64 }}>
-        <ListItemButton onClick={onClick}>
-            <ListItemIcon sx={{ color: 'text.primary' }}>
-                {icon}
-            </ListItemIcon>
-
-            <ListItemText primary={text} primaryTypographyProps={{ variant: 'body2' }} />
-        </ListItemButton>
-    </ListItem>;
 
 export const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== 'open' })(
     ({ theme, open }) => ({
