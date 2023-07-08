5310eabe8b30c6b245fd0b994b6d98a705fda8d7
N00bG1rl <403helen@gmail.com>
2023-07-08 14:36:04 +0300

    Rename HomePage text from Dolittle to Aigonix

--- Source/SelfService/Web/home/homeScreen.tsx
+++ Source/SelfService/Web/home/homeScreen.tsx
@@ -10,10 +10,10 @@ import { HomeScreenCardGridItems } from './homeScreenCardGridItems';
 
 export const HomeScreen = () =>
     <WorkSpaceWithoutSideBarLayout pageTitle='Home'>
-        <Typography variant='h1' sx={{ my: 3 }}>Welcome to Dolittle Studio!</Typography>
+        <Typography variant='h1' sx={{ my: 3 }}>Welcome to Aigonix!</Typography>
 
         <Typography variant='subtitle1' sx={{ maxWidth: 1018, mb: 4 }}>
-            Dolittle Studio is a decentralized, distributed, event-driven, microservice platform built to harness the power of events.
+            Aigonix is a decentralized, distributed, event-driven, microservice platform built to harness the power of events.
             Our goal is to enable your company to build and run microservice based solutions that react in real-time whenever meaningful
             changes occur with a guaranteed uptime.
         </Typography>
