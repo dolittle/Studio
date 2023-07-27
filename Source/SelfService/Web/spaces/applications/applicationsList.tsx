a2a56614145e3f8e4006a204542ca8c93355a7ac
N00bG1rl <403helen@gmail.com>
2023-07-27 15:14:34 +0300

    Refactor code

--- Source/SelfService/Web/spaces/applications/applicationsList.tsx
+++ Source/SelfService/Web/spaces/applications/applicationsList.tsx
@@ -7,22 +7,7 @@ import { ShortInfoWithEnvironment } from '../../apis/solutions/api';
 
 import { List } from '@mui/material';
 
-import { Button } from '@dolittle/design-system';
-
-// Make buttons as wide as the longest one
-const styles = {
-    list: {
-        p: 0,
-        mt: 2.5,
-        display: 'inline-block',
-    },
-    button: {
-        mb: 2,
-        minWidth: 155,
-        minHeight: 36,
-        display: 'block',
-    },
-};
+import { ApplicationsListItem } from './applicationsListItem';
 
 export type ApplicationsListProps = {
     data: ShortInfoWithEnvironment[];
@@ -30,18 +15,12 @@ export type ApplicationsListProps = {
 };
 
 export const ApplicationsList = ({ data, onChoose }: ApplicationsListProps) =>
-    <List sx={styles.list}>
-        {
-            data.map(application => (
-                <Button
-                    variant='filled'
-                    key={`${application.id}-${application.environment}`}
-                    label={`${application.name}-${application.environment}`}
-                    isFullWidth
-                    onClick={() => onChoose(application)}
-                    sx={styles.button}
-                />
-            ))
-        }
-        <Button label='Create new Application' variant='outlined' isFullWidth />
+    <List>
+        {data.map(application =>
+            <ApplicationsListItem
+                key={`${application.id}-${application.environment}`}
+                application={application}
+                onChoose={() => onChoose(application)}
+            />
+        )}
     </List>;
---
+++ Source/SelfService/Web/spaces/applications/applicationsListItem.tsx
@@ -0,0 +1,34 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { ListItem } from '@mui/material';
+
+import { Button } from '@dolittle/design-system';
+
+import { ShortInfoWithEnvironment } from '../../apis/solutions/api';
+
+// Makes ListItem buttons as wide as the longest one.
+const styles = {
+    mb: 2,
+    minWidth: 155,
+    minHeight: 36,
+    display: 'block',
+};
+
+export type ApplicationsListItemProps = {
+    application: ShortInfoWithEnvironment;
+    onChoose: () => void;
+};
+
+export const ApplicationsListItem = ({ application, onChoose }: ApplicationsListItemProps) =>
+    <ListItem sx={{ p: 0 }}>
+        <Button
+            variant='filled'
+            label={`${application.name}-${application.environment}`}
+            isFullWidth
+            onClick={onChoose}
+            sx={styles}
+        />
+    </ListItem>;
