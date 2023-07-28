b69a4090efdeec863b470eb61946c5ddd118f58d
Pavneet Singh Saund <pav.saund@gmail.com>
2023-07-28 08:41:51 +0200

    Wire up the KafkaServiceAccount mutation to Form

--- Source/SelfService/Web/integrations/connection/connectionDetails/consumeDataEventStreams/serviceAccounts/GenerateServiceAccountForm.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/consumeDataEventStreams/serviceAccounts/GenerateServiceAccountForm.tsx
@@ -10,14 +10,16 @@ import { Box, FormHelperText, Typography } from '@mui/material';
 
 import { Button, Form, FormRef, Input, MaxWidthTextBlock } from '@dolittle/design-system';
 
-// import { useConnectionsIdServiceAccountsServiceAccountNamePost } from '../../../../../apis/integrations/serviceAccountApi.hooks';
-import { ResponseError, ServiceAccountCreatedDto } from '../../../../../apis/integrations/generated';
+import { useConnectionsIdKafkaServiceAccountsServiceAccountNamePost } from '../../../../../apis/integrations/kafkaServiceAccountApi.hooks';
+import { AccountAccess, KafkaServiceAccountCreatedDto, ResponseError, ServiceAccountCreatedDto } from '../../../../../apis/integrations/generated';
 import { CACHE_KEYS } from '../../../../../apis/integrations/CacheKeys';
 
 import { GenerateServiceAccountFormSubmitButton } from './GenerateServiceAccountFormSubmitButton';
 
-export type GenerateServiceAccountFormParameters = {
+export type GenerateKafkaServiceAccountFormParameters = {
     name: string;
+    description?: string;
+    access?: AccountAccess;
 };
 
 export type GenerateServiceAccountFormProps = {
@@ -31,12 +33,12 @@ export type GenerateServiceAccountFormProps = {
 export const GenerateServiceAccountForm = (props: GenerateServiceAccountFormProps) => {
     const { enqueueSnackbar } = useSnackbar();
     const queryClient = useQueryClient();
-    const formRef = useRef<FormRef<GenerateServiceAccountFormParameters>>(null);
+    const formRef = useRef<FormRef<GenerateKafkaServiceAccountFormParameters>>(null);
 
     const [token, setToken] = useState<string | undefined>(undefined);
     const [formSubmitError, setFormSubmitError] = useState<string | undefined>(undefined);
 
-    // const generateTokenMutation = useConnectionsIdServiceAccountsServiceAccountNamePost();
+    const generateServiceAccountMutation = useConnectionsIdKafkaServiceAccountsServiceAccountNamePost();
     const hasResult = !!token;
 
     const handleTokenCopy = () => {
@@ -44,31 +46,30 @@ export const GenerateServiceAccountForm = (props: GenerateServiceAccountFormProp
         enqueueSnackbar('Token copied to clipboard.');
     };
 
-    const handleGenerate = (fieldValues: GenerateServiceAccountFormParameters) => {
+    const handleGenerate = (fieldValues: GenerateKafkaServiceAccountFormParameters) => {
         setFormSubmitError(undefined);
-        // generateTokenMutation.mutate({
-        //     id: props.connectionId,
-        //     serviceAccountName: fieldValues.name,
-        // }, {
-        //     onSuccess: (data: ServiceAccountCreatedDto) => {
-        //         setToken(data.token);
-        //         formRef.current?.setValue('token', data.token);
-        //         queryClient.invalidateQueries([CACHE_KEYS.ConnectionServiceAccounts_GET, props.connectionId]);
-        //     },
-        //     onError: async (error) => {
-        //         let message = `Error while generating token.`;
-        //         if (error instanceof ResponseError) {
-        //             if (error.response.body) {
-        //                 const body = await error.response.text();
-        //                 message = `${message} error: ${body}`;
-        //             }
-        //         }
-        //         setFormSubmitError(message);
-        //         enqueueSnackbar(message, { variant: 'error' });
-        //     }
-        // });
-        setToken('token');
-        props.onFormComplete(fieldValues.name);
+        generateServiceAccountMutation.mutate({
+            id: props.connectionId,
+            serviceAccountName: fieldValues.name,
+            description: fieldValues.description,
+            access: fieldValues.access
+        }, {
+            onSuccess: (data: KafkaServiceAccountCreatedDto) => {
+                props.onFormComplete(data.serviceAccountName!);
+                queryClient.invalidateQueries([CACHE_KEYS.ConnectionKafkaServiceAccounts_GET, props.connectionId]);
+            },
+            onError: async (error) => {
+                let message = `Error while generating service account.`;
+                if (error instanceof ResponseError) {
+                    if (error.response.body) {
+                        const body = await error.response.text();
+                        message = `${message} error: ${body}`;
+                    }
+                }
+                setFormSubmitError(message);
+                enqueueSnackbar(message, { variant: 'error' });
+            }
+        });
     };
     const resetForm = () => {
         setFormSubmitError(undefined);
@@ -84,7 +85,7 @@ export const GenerateServiceAccountForm = (props: GenerateServiceAccountFormProp
 
     return (
         <>
-            <Form<GenerateServiceAccountFormParameters>
+            <Form<GenerateKafkaServiceAccountFormParameters>
                 initialValues={{
                     name: '',
                 }}
@@ -101,6 +102,7 @@ export const GenerateServiceAccountForm = (props: GenerateServiceAccountFormProp
                         sx={{ mb: 6 }}
                     >
                         <Input id='name' label='Name' required disabled={hasResult && !formSubmitError} sx={{ mr: 10 }} />
+                        <Input id='description' label='Description' disabled={hasResult && !formSubmitError} />
                     </Box>
                     <Box display='flex' justifyContent='flex-end'>
                         {props.canCancel && <Button
