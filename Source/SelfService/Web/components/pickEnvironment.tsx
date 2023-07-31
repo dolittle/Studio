e5608aa26e658b99a1bee5f66dfeea1e1486cf3a
N00bG1rl <403helen@gmail.com>
2023-07-31 22:57:17 +0300

    Refactor code

--- Source/SelfService/Web/components/pickEnvironment.tsx
+++ Source/SelfService/Web/components/pickEnvironment.tsx
@@ -1,52 +1,23 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useState } from 'react';
+import React from 'react';
 
 import { useGlobalContext } from '../context/globalContext';
 import { useNavigate, generatePath } from 'react-router-dom';
 
-import { Box, Modal, Typography } from '@mui/material';
+import { Paper, Stack, Typography } from '@mui/material';
 
 import { Button } from '@dolittle/design-system';
 
 import { ShortInfoWithEnvironment } from '../apis/solutions/api';
 import { HttpResponseApplication } from '../apis/solutions/application';
 
-import { List } from '@fluentui/react/lib/List';
-import { Link } from '@fluentui/react';
-
-function rand() {
-    return Math.round(Math.random() * 20) - 10;
-};
-
-function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
-
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
-};
-
-const styles = {
-    paper: {
-        position: 'absolute',
-        width: 400,
-        backgroundColor: theme => theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme => theme.shadows[5],
-        padding: theme => theme.spacing(2, 4, 3),
-    },
-};
-
 export type PickEnvironmentProps = {
     applications: ShortInfoWithEnvironment[];
     application: HttpResponseApplication;
     redirectTo: string;
-    openModal: boolean;
+    openModal?: boolean;
 };
 
 export const isEnvironmentValidFromUrl = (applications: ShortInfoWithEnvironment[], currentApplicationId: string, currentEnvironment: string): boolean => {
@@ -59,55 +30,34 @@ export const PickEnvironment = ({ applications, openModal, redirectTo }: PickEnv
     const { setCurrentEnvironment, setCurrentApplicationId } = useGlobalContext();
     const navigate = useNavigate();
 
-    // getModalStyle is not a pure function, we roll the style only on the first render
-    const [modalStyle] = useState(getModalStyle);
-    const [isModalOpen, setIsModalOpen] = useState(openModal);
-
-    const handleModalOpen = () => setIsModalOpen(true);
-    const handleModalClose = () => setIsModalOpen(false);
-
     const environments: ShortInfoWithEnvironment[] = applications;
 
-    const onRenderCell = (item?: ShortInfoWithEnvironment, index?: number | undefined): JSX.Element => {
-        const application = item!;
+    const handleEnvironmentClick = (application: ShortInfoWithEnvironment) => {
+        const href = generatePath(redirectTo, {
+            applicationId: application.id,
+            environment: application.environment,
+        });
 
-        return (
-            <Link onClick={() => {
-                const href = generatePath(redirectTo, {
-                    applicationId: application.id,
-                    environment: application.environment,
-                });
-
-                setCurrentEnvironment(application.environment);
-                setCurrentApplicationId(application.id);
-                handleModalClose();
-                navigate(href);
-            }}
-                underline>
-                {application.name} {application.environment}
-            </Link>
-        );
+        setCurrentEnvironment(application.environment);
+        setCurrentApplicationId(application.id);
+        navigate(href);
     };
 
-    const body = (
-        <Box sx={styles.paper} style={modalStyle}>
-            <Typography variant='h1' my={2}>Pick an environment</Typography>
-            <List items={environments} onRenderCell={onRenderCell} />
-        </Box>
-    );
-
     return (
-        <>
-            {!isModalOpen ? <Button label='Open Modal' onClick={handleModalOpen} /> : null}
+        <Stack sx={{ width: 1, height: 1, minHeight: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+            <Paper sx={{ width: 400, p: 2, textAlign: 'center' }}>
+                <Typography variant='h1' my={2}>Pick an environment</Typography>
 
-            <Modal
-                open={isModalOpen}
-                onClose={handleModalClose}
-                aria-labelledby='simple-modal-title'
-                aria-describedby='simple-modal-description'
-            >
-                {body}
-            </Modal>
-        </>
+                <Stack>
+                    {environments.map(application =>
+                        <Button
+                            key={`${application.name} ${application.environment}`}
+                            label={`${application.name} ${application.environment}`}
+                            onClick={() => handleEnvironmentClick(application)}
+                        />
+                    )}
+                </Stack>
+            </Paper>
+        </Stack>
     );
 };
