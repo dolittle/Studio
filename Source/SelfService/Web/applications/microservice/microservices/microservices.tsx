69eff3f2ebca9f5c624333e069603b9085901dbc
N00bG1rl <403helen@gmail.com>
2023-08-02 14:28:44 +0300

    Remove redundant code

--- Source/SelfService/Web/applications/microservice/microservices/microservices.tsx
+++ Source/SelfService/Web/applications/microservice/microservices/microservices.tsx
@@ -1,10 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
+import { useNavigate } from 'react-router-dom';
 
 import { MicroserviceObject } from '../../../apis/solutions/api';
 import { HttpResponseApplication } from '../../../apis/solutions/application';
@@ -14,7 +14,7 @@ import { canEditMicroservices, microservices } from '../../stores/microservice';
 
 import { Typography } from '@mui/material';
 
-import { Button, LoadingSpinner } from '@dolittle/design-system';
+import { Button } from '@dolittle/design-system';
 
 import { NoMicroservices } from './noMicroservices';
 import { MicroservicesDataGrid } from './microservicesDataGrid';
@@ -29,9 +29,6 @@ export const Microservice = ({ environment, application }: MicroserviceProps) =>
     const { enqueueSnackbar } = useSnackbar();
     const $microservices = useReadable(microservices) as MicroserviceObject[];
 
-    const [hasMicroservices, setHasMicroservices] = useState(false);
-    const [loading, setLoading] = useState(true);
-
     let tempEnvironments = application.environments.map(e => e.name);
     tempEnvironments = [...tempEnvironments, ...$microservices.map(item => item.environment)];
 
@@ -39,13 +36,6 @@ export const Microservice = ({ environment, application }: MicroserviceProps) =>
 
     const newEnvironments = [...new Set(tempEnvironments)];
     const hasEnvironments = newEnvironments.length > 0;
-
-    const filteredMicroservices = $microservices.filter(microservice => microservice.environment === environment);
-
-    useEffect(() => {
-        setHasMicroservices(filteredMicroservices.length > 0);
-        setLoading(false);
-    }, [filteredMicroservices]);
 
     const handleCreateMicroservice = () => {
         if (!canEdit) {
@@ -57,26 +47,16 @@ export const Microservice = ({ environment, application }: MicroserviceProps) =>
         navigate(href);
     };
 
-    const handleCreateEnvironment = () => {
-        // TODO: How to stop this if automation disabled, currently on the environment level.
-        const href = `/environment/application/${application.id}/create`;
-        navigate(href);
-    };
-
-    if (loading) return <LoadingSpinner />;
-
     return (
         <>
-            {!hasEnvironments && <Button label='Create New Environment' onClick={handleCreateEnvironment} />}
-
             <Typography variant='h1' sx={{ my: 2 }}>Microservices</Typography>
 
-            {hasMicroservices ?
-                <MicroservicesDataGrid application={application} environment={environment} microservices={filteredMicroservices} /> :
+            {$microservices.length > 0 ?
+                <MicroservicesDataGrid application={application} environment={environment} microservices={$microservices} /> :
                 <NoMicroservices onCreate={() => handleCreateMicroservice()} />
             }
 
-            {hasEnvironments && hasMicroservices &&
+            {hasEnvironments && $microservices.length > 0 &&
                 <Button
                     label='Deploy New Microservice'
                     variant='fullwidth'
