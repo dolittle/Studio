4a0eaf98a1aaf3770980918305441897caeffca8
Pavneet Singh Saund <pav.saund@gmail.com>
2023-08-09 17:11:42 +0200

    Disable deploy button when service status is deploying

--- Source/SelfService/Web/integrations/connection/connectionDetails/consumeDataRestAPI/EnableRestApiSection.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/consumeDataRestAPI/EnableRestApiSection.tsx
@@ -2,10 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import React from 'react';
 import { Typography } from '@mui/material';
-import { Button, ContentSection, ContentParagraph } from '@dolittle/design-system';
+import { Button, ContentSection, ContentParagraph, StatusIndicator } from '@dolittle/design-system';
+import { RestApiServiceStatus } from '../../../../apis/integrations/generated';
 
 export type EnableRestApiSectionProps = {
     onEnableRestApi?: () => void;
+    status: RestApiServiceStatus;
 };
 
 export const EnableRestApiSection = (props: EnableRestApiSectionProps) => {
@@ -14,7 +16,7 @@ export const EnableRestApiSection = (props: EnableRestApiSectionProps) => {
             <ContentParagraph>
                 To enable the Rest API service, press the Deploy service button. The first time you enable the Rest API may take a few minutes to set up and deploy your dedicated service.
             </ContentParagraph>
-            <Button label='Enable Rest Api' variant='fullwidth' startWithIcon='RocketLaunch' onClick={() => props.onEnableRestApi?.()} />
+            <Button label='Enable Rest Api' variant='fullwidth' startWithIcon='RocketLaunch' onClick={() => props.onEnableRestApi?.()} disabled={props.status !== 'Off'} />
         </ContentSection>
     );
 };
