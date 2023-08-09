91fca57e445e2020a9ba5d60225ef5869ffd02f5
Pavneet Singh Saund <pav.saund@gmail.com>
2023-08-09 16:53:51 +0200

    Add temporary dropdown to simulate service status

--- Source/SelfService/Web/integrations/connection/connectionDetails/consumeDataRestAPI/index.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/consumeDataRestAPI/index.tsx
@@ -5,9 +5,9 @@ import React from 'react';
 
 import { useSnackbar } from 'notistack';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Select, MenuItem } from '@mui/material';
 
-import { Button, ContentContainer, ContentHeader, ContentParagraph, ContentSection, IconButton, Link, Switch } from '@dolittle/design-system';
+import { Button, ContentContainer, ContentHeader, ContentParagraph, ContentSection, IconButton, Link, StatusIndicator, StatusIndicatorProps, Switch } from '@dolittle/design-system';
 import { useConnectionsIdRestApiStatusGet } from '../../../../apis/integrations/connectionRestApiApi.hooks';
 import { useConnectionIdFromRoute } from '../../../routes.hooks';
 import { CredentialsContainer } from './Credentials/CredentialsContainer';
@@ -19,6 +19,7 @@ import { EnableRestApiSection } from './EnableRestApiSection';
 
 export const ConsumeDataRestAPIView = () => {
     const [forceShowEnable, setForceShowEnable] = React.useState(true);
+    const [forcedServiceStatus, setForcedServiceStatus] = React.useState<string>('');
     const { enqueueSnackbar } = useSnackbar();
     const connectionId = useConnectionIdFromRoute();
     const { data: apiStatus } = useConnectionsIdRestApiStatusGet({ id: connectionId });
@@ -35,9 +36,26 @@ export const ConsumeDataRestAPIView = () => {
 
     const showEnableSection = (apiStatus?.target === 'Disabled') || forceShowEnable;
     const showInfoSection = (apiStatus?.target === 'Enabled') && !forceShowEnable;
+    const serviceStatus = forcedServiceStatus || apiStatus?.service;
 
     const restApiUrl = `${apiStatus?.basePath}swagger/index.html`;
     const openApiDocumentationUrl = `${apiStatus?.basePath}swagger/v1/swagger.json`;
+
+    const statusIndicatorFromServiceStatus = (status: string | undefined): StatusIndicatorProps['status'] => {
+        switch (status?.toLocaleLowerCase()) {
+            case 'off':
+                return 'error';
+            case 'deploying':
+                return 'waiting';
+            case 'active':
+                return 'success';
+            case 'unhealthy':
+                return 'warning';
+            case 'terminating':
+                return 'waiting';
+        }
+        return 'unknown';
+    };
 
 
     return (
@@ -46,13 +64,27 @@ export const ConsumeDataRestAPIView = () => {
                 <ContentHeader
                     title='Consume data over a REST API'
                     buttonsSlot={
-                        <Switch.UI
-                            id='deploy-switch'
-                            label='Force deployed'
-                            checked={!forceShowEnable}
-                            sx={{ mx: 0 }}
-                            onChange={() => setForceShowEnable(!forceShowEnable)}
-                        />}
+                        <>
+                            <Switch.UI
+                                id='deploy-switch'
+                                label='Force deployed'
+                                checked={!forceShowEnable}
+                                sx={{ mx: 0 }}
+                                onChange={() => setForceShowEnable(!forceShowEnable)}
+                            />
+                            <Select
+                                id='service-status'
+                                label='Simulated Service Status'
+                                onChange={(event) => setForcedServiceStatus(event.target.value as string)}>
+                                    <MenuItem value=''>None</MenuItem>
+                                    <MenuItem value='Off'>Off</MenuItem>
+                                    <MenuItem value='Deploying'>Deploying</MenuItem>
+                                    <MenuItem value='Active'>Active</MenuItem>
+                                    <MenuItem value='Unhealthy'>Unhealthy</MenuItem>
+                                    <MenuItem value='Terminating'>Terminating</MenuItem>
+                                </Select>
+                            <StatusIndicator status={statusIndicatorFromServiceStatus(serviceStatus)} label={serviceStatus} />
+                        </>}
                 />
                 <ContentParagraph>
                     You can consume data through a Rest API service.
