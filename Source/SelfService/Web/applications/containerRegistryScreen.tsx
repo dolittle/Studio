4cfa71978b0c5f89dc3344581067db2dc46f63d4
N00bG1rl <403helen@gmail.com>
2023-08-17 18:53:13 +0300

    Cleanup code

--- Source/SelfService/Web/applications/containerRegistryScreen.tsx
+++ Source/SelfService/Web/applications/containerRegistryScreen.tsx
@@ -8,13 +8,11 @@ import { useGlobalContext } from '../context/globalContext';
 
 import { Typography } from '@mui/material';
 
-import { ShortInfo } from '../apis/solutions/api';
-import { getApplication, getApplicationsListing, HttpResponseApplication, HttpResponseApplications } from '../apis/solutions/application';
+import { getApplication, getApplicationsListing, HttpResponseApplication } from '../apis/solutions/application';
 
-import { RouteNotFound } from '../components/notfound';
-import { TopNavBar } from '../components/layout/topNavBar';
 import { getMenuWithApplication, LayoutWithSidebar } from '../components/layout/layoutWithSidebar';
 import { RegistryContainer } from './containerregistry/registryContainer';
+import { RouteNotFound } from '../components/notfound';
 
 import { withRouteApplicationState } from '../spaces/applications/withRouteApplicationState';
 
@@ -22,7 +20,6 @@ export const ContainerRegistryScreen = withRouteApplicationState(({ routeApplica
     const navigate = useNavigate();
     const { hasOneCustomer } = useGlobalContext();
 
-    //const [applications, setApplications] = useState([] as ShortInfo[]);
     const [application, setApplication] = useState({} as HttpResponseApplication);
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -35,7 +32,6 @@ export const ContainerRegistryScreen = withRouteApplicationState(({ routeApplica
             getApplicationsListing(),
             getApplication(currentApplicationId),
         ]).then(values => {
-            //const applicationsData = values[0] as HttpResponseApplications;
             const applicationData = values[1];
 
             if (!applicationData?.id) {
@@ -44,8 +40,6 @@ export const ContainerRegistryScreen = withRouteApplicationState(({ routeApplica
                 return;
             }
 
-            // TODO this should be unique
-            //setApplications(applicationsData.applications);
             setApplication(applicationData);
             setIsLoaded(true);
         }).catch(error => console.log(error));
@@ -63,8 +57,6 @@ export const ContainerRegistryScreen = withRouteApplicationState(({ routeApplica
 
     return (
         <LayoutWithSidebar navigation={nav}>
-            {/* <TopNavBar routes={routes} applications={applications} applicationId={currentApplicationId} /> */}
-
             <Routes>
                 <Route path='/overview/*' element={<RegistryContainer application={application} />} />
                 <Route path='*' element={<RouteNotFound redirectUrl={'overview'} auto={true} />} />
