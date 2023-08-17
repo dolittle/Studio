cb84bd0957ee9e5e8c82482f8801404f7de110bf
N00bG1rl <403helen@gmail.com>
2023-08-17 19:39:29 +0300

    Replace the AdminScreen layout with a new one

--- Source/SelfService/Web/admin/adminScreen.tsx
+++ Source/SelfService/Web/admin/adminScreen.tsx
@@ -9,39 +9,35 @@ import { Typography } from '@mui/material';
 
 import { Button } from '@dolittle/design-system';
 
+import { WorkSpaceLayoutWithSidePanel } from '../components/layout/workSpaceLayout';
 import { Create as CreateCustomer } from './customer/create';
 import { ViewAll as ViewAllCustomers } from './customer/viewAll';
 import { View as ViewCustomer } from './customer/view';
 import { View as ViewApplicationAccess } from './application/view';
-import { LayoutWithSidebar } from '../components/layout/layoutWithSidebar';
 
-export const Screen: React.FunctionComponent = () => {
-    const nav = [];
+const welcome = (
+    <>
+        <Typography variant='h1' my={2}>Hello Admin</Typography>
+        <Button
+            label='Take me to the Customers'
+            variant='filled'
+            endWithIcon='KeyboardDoubleArrowRight'
+            overrides={{
+                component: Link,
+                to: '/admin/customers'
+            }}
+        />
+    </>
+);
 
-    const welcome = (
-        <>
-            <Typography variant='h1' my={2}>Hello Admin</Typography>
-            <Button
-                label='Take me to the Customers'
-                variant='filled'
-                endWithIcon='KeyboardDoubleArrowRight'
-                overrides={{
-                    component: Link,
-                    to: '/admin/customers'
-                }}
-            />
-        </>
-    );
+export const Screen = () =>
+    <WorkSpaceLayoutWithSidePanel pageTitle='Administrator' sidePanelMode='applications'>
+        <Routes>
+            <Route path='/' element={welcome} />
+            <Route path='/customers' element={<ViewAllCustomers />} />
+            <Route path='/customer/create' element={<CreateCustomer />} />
+            <Route path='/customer/:customerId' element={<ViewCustomer />} />
+            <Route path='/customer/:customerId/application/:applicationId/user/access' element={<ViewApplicationAccess />} />
+        </Routes>
+    </WorkSpaceLayoutWithSidePanel>;
 
-    return (
-        <LayoutWithSidebar navigation={nav}>
-            <Routes>
-                <Route path="/" element={welcome} />
-                <Route path="/customers" element={<ViewAllCustomers />} />
-                <Route path="/customer/create" element={<CreateCustomer />} />
-                <Route path="/customer/:customerId" element={<ViewCustomer />} />
-                <Route path="/customer/:customerId/application/:applicationId/user/access" element={<ViewApplicationAccess />} />
-            </Routes>
-        </LayoutWithSidebar>
-    );
-};
