26d8b82a97da1d95eb8394a65633d1fa8fe9e0e0
N00bG1rl <403helen@gmail.com>
2023-08-17 19:20:09 +0300

    Cleanup code

--- Source/SelfService/Web/applications/documentationScreen.tsx
+++ Source/SelfService/Web/applications/documentationScreen.tsx
@@ -3,14 +3,9 @@
 
 import React, { useEffect, useState } from 'react';
 
-import { Route, useNavigate, Routes, generatePath } from 'react-router-dom';
+import { generatePath, Route, useNavigate, Routes } from 'react-router-dom';
 import { useGlobalContext } from '../context/globalContext';
 
-// I wonder if scss is scoped like svelte. I hope so!
-// Not scoped like svelte
-import '../spaces/applications/applicationScreen.scss';
-
-import { ShortInfo } from '../apis/solutions/api';
 import { HttpResponseApplication, getApplicationsListing, getApplication } from '../apis/solutions/application';
 
 import { TopNavBar } from '../components/layout/topNavBar';
@@ -25,22 +20,18 @@ export const DocumentationScreen = withRouteApplicationState(({ routeApplication
 
     const currentApplicationId = routeApplicationParams.applicationId;
 
-    //const [applications, setApplications] = useState([] as ShortInfo[]);
     const [application, setApplication] = useState({} as HttpResponseApplication);
     const [isLoaded, setIsLoaded] = useState(false);
 
     const href = `/problem`;
 
     useEffect(() => {
-        if (!currentApplicationId) {
-            return;
-        }
+        if (!currentApplicationId) return;
 
         Promise.all([
             getApplicationsListing(),
             getApplication(currentApplicationId),
         ]).then(values => {
-            //const applicationsData = values[0] as HttpResponseApplications;
             const applicationData = values[1];
 
             if (!applicationData?.id) {
@@ -48,8 +39,6 @@ export const DocumentationScreen = withRouteApplicationState(({ routeApplication
                 return;
             }
 
-            // TODO this should be unique
-            //setApplications(applicationsData.applications);
             setApplication(applicationData);
             setIsLoaded(true);
         }).catch(() => {
@@ -97,10 +86,6 @@ export const DocumentationScreen = withRouteApplicationState(({ routeApplication
     //         name: 'Verify access to kubernetes',
     //     },
     // ];
-
-    // const redirectUrl = generatePath('/documentation/application/:applicationId/overview', {
-    //     applicationId: currentApplicationId,
-    // });
 
     return (
         <LayoutWithSidebar navigation={nav}>
