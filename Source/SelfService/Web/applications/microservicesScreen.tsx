f3ea2c621b948af353a1ba349b9521aee543e6f3
N00bG1rl <403helen@gmail.com>
2023-08-17 13:01:30 +0300

    Cleanup code

--- Source/SelfService/Web/applications/microservicesScreen.tsx
+++ Source/SelfService/Web/applications/microservicesScreen.tsx
@@ -6,10 +6,6 @@ import React, { useEffect, useState } from 'react';
 import { Route, useNavigate, Routes, generatePath } from 'react-router-dom';
 import { useGlobalContext } from '../context/globalContext';
 
-// I wonder if scss is scoped like svelte. I hope so!
-// Not scoped like svelte
-import '../spaces/applications/applicationScreen.scss';
-
 import { mergeMicroservicesFromGit, mergeMicroservicesFromK8s } from './stores/microservice';
 
 import { getMicroservices } from '../apis/solutions/api';
@@ -20,7 +16,6 @@ import { MicroserviceNewScreen } from './microservice/microserviceNewScreen';
 import { MicroserviceViewScreen } from './microservice/microserviceViewScreen';
 import { LayoutWithSidebar, getMenuWithApplication } from '../components/layout/layoutWithSidebar';
 import { RouteNotFound } from '../components/notfound';
-//import { TopNavBar } from '../components/layout/topNavBar';
 
 import { withRouteApplicationState } from '../spaces/applications/withRouteApplicationState';
 
@@ -46,7 +41,7 @@ export const MicroservicesScreen = withRouteApplicationState(({ routeApplication
             //const applicationsData = values[0] as HttpResponseApplications;
             const applicationData = values[1];
 
-            if (!applicationData?.id) {
+            if (!applicationData.id) {
                 navigate(href);
                 return;
             }
@@ -113,15 +108,14 @@ export const MicroservicesScreen = withRouteApplicationState(({ routeApplication
     //         name: 'View',
     //     },
     // ];
+    {/* <TopNavBar routes={routes} applications={applications} applicationId={currentApplicationId} /> */ }
 
     return (
         <LayoutWithSidebar navigation={nav}>
-            {/* <TopNavBar routes={routes} applications={applications} applicationId={currentApplicationId} /> */}
-
             <Routes>
-                <Route path="/overview" element={<Microservice application={application} />} />
-                <Route path="/create" element={<MicroserviceNewScreen application={application} />} />
-                <Route path="/view/:microserviceId" element={<MicroserviceViewScreen application={application} />} />
+                <Route path='/overview' element={<Microservice application={application} />} />
+                <Route path='/create' element={<MicroserviceNewScreen application={application} />} />
+                <Route path='view/:microserviceId' element={<MicroserviceViewScreen application={application} />} />
                 <Route path='*' element={<RouteNotFound redirectUrl={'overview'} auto={true} />} />
             </Routes>
         </LayoutWithSidebar>
