df897d67807005cb218bd5cc703f1939b3e626de
N00bG1rl <403helen@gmail.com>
2023-08-23 19:08:50 +0300

    Add Chip to Stories

--- Source/DesignSystem/atoms/Chip/Chip.stories.tsx
+++ Source/DesignSystem/atoms/Chip/Chip.stories.tsx
@@ -1,2 +1,32 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { action } from '@storybook/addon-actions';
+
+import { componentStories } from '../../componentStories';
+
+import { Chip } from './Chip';
+
+const { metadata, createStory } = componentStories(Chip);
+
+metadata.parameters = {
+    docs: {
+        description: {
+            component: ``
+        },
+    },
+};
+
+metadata.argTypes = {
+    onDelete: { control: false },
+    sx: { control: false },
+};
+
+metadata.args = {
+    label: 'Default',
+    onDelete: action('deleted'),
+};
+
+export default metadata;
+
+export const Default = createStory();
