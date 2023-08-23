b9f2a8a100a640645da4e8ddb3e9d6651157e29e
Pavneet Singh Saund <pav.saund@gmail.com>
2023-08-23 16:46:20 +0200

    Add some meaningful content to the Filters section

--- Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/MessageFilterSection.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/messages/changeMessage/components/MessageFilterSection.tsx
@@ -5,9 +5,11 @@ import React from 'react';
 
 import { useFormState } from 'react-hook-form';
 
-import { Button, ContentParagraph, ContentSection, Input } from '@dolittle/design-system';
+import { Box, Typography } from '@mui/material';
+import { Button, Link, ContentParagraph, ContentSection, Input } from '@dolittle/design-system';
 
 import { ViewModeProps } from '../ViewMode';
+import { TextCopyBox } from '../../../../../../components/TextCopyBox';
 
 export type MessageFilterSectionProps = ViewModeProps & {
 
@@ -16,11 +18,28 @@ export type MessageFilterSectionProps = ViewModeProps & {
 export const MessageFilterSection = (props: MessageFilterSectionProps) => {
 
     return (
-        <ContentSection hideHeader>
+        <ContentSection title='Filters'>
             <ContentParagraph>
-                Something something filters filters
+                The filtering mechanism helps you focus on the data you are interested in instead of having to deal with all the data and filtering when you are processing it.
             </ContentParagraph>
-            <Input id='jqFilter' label='jq predicate' multiline rows={3} isFullWidth/>
+            <ContentParagraph>
+                To filter the data to your needs, you can use a jq predicate. The jq predicate is a JSON query language that allows you to filter the data based on the JSON structure.
+                You can learn more about jq here <Link
+                    target
+                    ariaLabel='jq user manual'
+                    href={'https://stedolan.github.io/jq/manual/'}
+                    message={'https://stedolan.github.io/jq/manual/'}
+                />
+            </ContentParagraph>
+            <ContentParagraph>
+                Here is an example of a jq filter that matches all messages that have a Facility property with a value of 220. This way you can filter out all messages that are not relevant to you:
+                <TextCopyBox instructions={'(.Facility | tonumber == 220)'}>
+                    <Typography component={'span'} variant='monospace'>(.Facility | tonumber == 220)</Typography>
+                    <br />
+                    <br />
+                </TextCopyBox>
+            </ContentParagraph>
+            <Input sx={{ mt: 2 }} id='jqFilter' label='jq predicate' multiline rows={3} isFullWidth />
         </ContentSection>
     );
 };
