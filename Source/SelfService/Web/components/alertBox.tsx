12e61c55ed0d2cc2907f6be06e459c8a72689fd6
N00bG1rl <403helen@gmail.com>
2023-08-23 17:30:21 +0300

    Add explanatory comment to AlertBox

--- Source/SelfService/Web/components/alertBox.tsx
+++ Source/SelfService/Web/components/alertBox.tsx
@@ -2,21 +2,28 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+
 import { Alert, AlertColor } from '@mui/material';
 
 import { useGlobalContext } from '../context/globalContext';
 
-export const AlertBox: React.FunctionComponent = () => {
+/**
+ * Represents an alert box.
+ * @returns custom alert box that takes error messages from the global context.
+ *
+ * Not in use currently, but kept for reference.
+ */
+export const AlertBox = () => {
     const { lastMessage, clearNotification } = useGlobalContext();
+
     const severity = lastMessage.level as AlertColor;
+
     const dismiss = () => {
         window.clearTimeout(timer);
         clearNotification();
     };
 
-    if (lastMessage.message === '') {
-        return null;
-    }
+    if (lastMessage.message === '') return null;
 
     const timer = window.setTimeout(() => dismiss(), 3000);
 
