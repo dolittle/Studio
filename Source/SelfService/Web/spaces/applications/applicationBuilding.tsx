65b8375c7f3ecdcf5c8d994e25730feb11a75638
N00bG1rl <403helen@gmail.com>
2023-08-29 22:07:58 +0300

    Add ApplicationBuilding component

---
+++ Source/SelfService/Web/spaces/applications/applicationBuilding.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+
+import { ProblemPage } from '@dolittle/design-system';
+
+export const ApplicationBuilding = () => <ProblemPage variant='building' />;
