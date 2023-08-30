0ba18ba2f058522a4f30b6875fb6f014fad04c4a
Pavneet Singh Saund <pav.saund@gmail.com>
2023-08-30 15:28:51 +0200

    Wire up configuration of cronExpression

---
+++ Source/SelfService/Web/integrations/connection/connectionDetails/configuration/configuration/ConnectorConfigurationForm.tsx
@@ -0,0 +1,160 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React, { useImperativeHandle, useState, useCallback, useEffect, useMemo } from 'react';
+
+import { SubmitHandler } from 'react-hook-form';
+
+import { Form, FormRef } from '@dolittle/design-system';
+
+import { ConnectionModel } from '../../../../../apis/integrations/generated';
+import { useConnectionsIdConfigurationExporterCronPost } from '../../../../../apis/integrations/connectionConfigurationApi.hooks';
+
+
+//TODO: Can this be replaced with using `watch` from react-hook-form?
+const useForceSubscribeToCronExpressionStateChanges = (currentForm: FormRef<ConnectorConfigurationFormParameters> | undefined) => {
+    useEffect(() => {
+        if (currentForm) {
+            const { isDirty } = currentForm.getFieldState('cronExpression', currentForm.formState);
+        }
+    }, [currentForm]);
+};
+
+export type ConfigurationSaveActionName =
+    | 'Cron';
+
+export type ConfigurationSaveActionState =
+    | { status: 'success' }
+    | { status: 'error', errorMessage?: string };
+
+export type ConfigurationFormSaveAction = { name: ConfigurationSaveActionName } & ConfigurationSaveActionState;
+
+export type ConnectorConfigurationFormSaveState = ConfigurationFormSaveAction[];
+
+export type ConnectorConfigurationFormRef = {
+    reset: (keepDefault: boolean) => void;
+};
+
+export type ConnectorConfigurationFormParameters = {
+    cronExpression: string;
+};
+
+export type ConnectorConfigurationFormProps = {
+    connectionId: string;
+    connection: ConnectionModel
+    onSaved?: (saveState: ConnectorConfigurationFormSaveState) => void;
+    children?: React.ReactNode;
+};
+
+export const ConnectorConfigurationForm = React.forwardRef<ConnectorConfigurationFormRef, ConnectorConfigurationFormProps>(({
+    connectionId,
+    connection,
+    onSaved,
+    children
+}: ConnectorConfigurationFormProps,
+    ref: React.Ref<ConnectorConfigurationFormRef>
+) => {
+    const [currentForm, setCurrentForm] = useState<FormRef<ConnectorConfigurationFormParameters>>();
+    const [lastSaveState, setLastSaveState] = useState<ConnectorConfigurationFormSaveState>();
+    const formRef = useCallback((ref) => {
+        if (ref) {
+            setCurrentForm(ref);
+        }
+    }, []);
+    if (currentForm) {
+        const cronExpressionWatch = currentForm.watch('cronExpression');
+    }
+
+    useImperativeHandle(ref, () => ({
+        reset(keepDefault: boolean) {
+            if (currentForm) {
+                currentForm.reset(keepDefault ? defaultValues : undefined);
+            }
+        }
+    }), [currentForm]);
+
+
+    useEffect(() => {
+        if (currentForm?.formState.isSubmitSuccessful && lastSaveState?.length) {
+            currentForm.reset(currentForm.getValues());
+            onSaved?.(lastSaveState);
+        }
+    }, [currentForm?.reset, currentForm?.formState.isSubmitSuccessful, currentForm?.formState.defaultValues, onSaved, lastSaveState]);
+
+    const cronMutation = useConnectionsIdConfigurationExporterCronPost();
+
+    const defaultValues: ConnectorConfigurationFormParameters = useMemo(() => (
+        {
+            cronExpression: 'johnny',
+        }
+    ), [connection]);
+
+    useForceSubscribeToCronExpressionStateChanges(currentForm);
+
+    /**
+     * Reset the form defaults when the default values change, and keep any dirty values
+     * The idea here is to set the *Initial* state. If the form needs to be reset, then use the reset function available on the imperativeRef from parent component.
+     */
+    useEffect(() => {
+        if (currentForm) {
+            currentForm.reset(defaultValues, { keepDirtyValues: true });
+        }
+
+    }, [defaultValues, currentForm]);
+
+    const handleSave: SubmitHandler<ConnectorConfigurationFormParameters> = useCallback((data) => {
+        if (!connectionId || !currentForm) {
+            return;
+        }
+        setLastSaveState([]);
+        const saveActions: (
+            | { name: Extract<ConfigurationSaveActionName, 'Cron'>, action: Promise<void> }
+        )[] = [];
+        const getFieldState = (field) => currentForm.getFieldState(field, currentForm.formState);
+
+        const cronExpressionFieldState = getFieldState('cronExpression');
+        if (cronExpressionFieldState.isDirty) {
+            saveActions.push({
+                name: 'Cron',
+                action: cronMutation.mutateAsync(
+                    {
+                        id: connectionId,
+                        cronExpression: data.cronExpression,
+                    },
+                )
+            });
+        }
+
+        Promise.allSettled(saveActions.map((action) => action.action))
+            .then((results) => {
+                // use the index of the result to get the corresponding save action name and map this to a success or error state.
+                const saveState = results.map((result, index) => {
+                    const saveAction = saveActions[index];
+                    if (result.status === 'fulfilled') {
+                        const successSate: ConfigurationFormSaveAction = { name: saveAction.name, status: 'success' };
+                        return successSate;
+                    }
+                    const errorState: ConfigurationFormSaveAction = { name: saveAction.name, status: 'error', errorMessage: result.reason };
+                    return errorState;
+                });
+                setLastSaveState(saveState);
+            });
+    }, [
+        currentForm?.getFieldState,
+        connectionId,
+        cronMutation,
+    ]);
+
+    return (
+        <Form<ConnectorConfigurationFormParameters>
+            initialValues={defaultValues}
+            onSubmit={(data, event) => handleSave(data, event)}
+            sx={{ ml: 3 }}
+            fRef={formRef}
+        >
+            {children}
+        </Form>
+    );
+});
+
+ConnectorConfigurationForm.displayName = 'M3ConfigurationForm';
---
+++ Source/SelfService/Web/integrations/connection/connectionDetails/configuration/configuration/ExporterConfigurationSection.tsx
@@ -0,0 +1,34 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React from 'react';
+import { ContentSection, Form, Input, Link, Tooltip } from '@dolittle/design-system';
+import { ConnectionModel } from '../../../../../apis/integrations/generated';
+
+export type CronFormParameters = {
+    cronExpression: string;
+};
+
+export type ExporterConfigurationSectionProps = {
+    connectionModel?: ConnectionModel
+    canEdit: boolean;
+};
+
+export const ExporterConfigurationSection = ({ connectionModel, canEdit }: ExporterConfigurationSectionProps) => {
+
+    const handlCronFormSubmit = (data: CronFormParameters) => {
+        throw new Error('Function not implemented.');
+    };
+
+    return (
+        <ContentSection title='Exporter Settings'>
+            <Tooltip
+                tooltipTitle='Cron Expression'
+                tooltipText={<>The Exporter is usually configured to force update all data every ... as a way to ensure the system is in sync. You can override this setting by supplying your own <Link message='cron expression' href='https://crontab.cronhub.io/' target /></>}
+                sx={{ top: 16 }}
+            >
+                <Input id='cronExpression' label='Cron Expression' disabled={!canEdit} />
+            </Tooltip>
+        </ContentSection>
+    );
+};
