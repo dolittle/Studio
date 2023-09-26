a504987fea5f741e2872bdf6fd89ed8ae6276c2d
N00bG1rl <403helen@gmail.com>
2023-09-26 14:23:32 +0300

    Make LoadingSpinner to fill full screen

--- Source/DesignSystem/atoms/LoadingSpinner/LoadingSpinner.tsx
+++ Source/DesignSystem/atoms/LoadingSpinner/LoadingSpinner.tsx
@@ -2,16 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
+import { createPortal } from 'react-dom';
 
 import { Box, CircularProgress, CircularProgressProps } from '@mui/material';
 
 export const LoadingSpinner = (props: CircularProgressProps) =>
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 1, p: 2 }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', p: 2 }}>
         <CircularProgress {...props} />
     </Box>;
 
 // TODO: Move this to a separate component.
 export const FullPageLoadingSpinner = (props: CircularProgressProps) =>
-    <Box sx={{ width: 1, height: 1, position: 'absolute', top: 0, left: 0, zIndex: '9999', backgroundColor: '#191A2150' }}>
-        <LoadingSpinner {...props} />
-    </Box>;
+    createPortal(
+        <Box sx={{ width: 1, height: 1, position: 'absolute', top: 0, left: 0, zIndex: '9999', backgroundColor: '#191A2150' }}>
+            <LoadingSpinner {...props} />
+        </Box>,
+        document.body,
+    );
