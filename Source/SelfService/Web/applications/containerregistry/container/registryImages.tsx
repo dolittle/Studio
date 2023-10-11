6e4bd4210a13fa5d84f5c91b135de9d5d8f48573
N00bG1rl <403helen@gmail.com>
2023-10-11 17:42:04 +0300

    Add DataGridPro to RegistryImages

--- Source/SelfService/Web/applications/containerregistry/container/registryImages.tsx
+++ Source/SelfService/Web/applications/containerregistry/container/registryImages.tsx
@@ -1,13 +1,25 @@
 // Copyright (c) Aigonix. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { useNavigate, generatePath } from 'react-router-dom';
 
 import { Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { DataGridPro, GridRowId, GridRowParams, GridColDef } from '@mui/x-data-grid-pro';
+
+import { dataGridDefaultProps, DataGridCustomToolbar, DataGridWrapper, DetailPanelExpandIcon, DetailPanelCollapseIcon } from '@dolittle/design-system';
 
 import { ContainerRegistryImages } from '../../../apis/solutions/containerregistry';
+
+export const registryImagesDataGridColumns: GridColDef[] = [
+    {
+        field: 'name',
+        headerName: 'Container Image',
+        minWidth: 300,
+        flex: 1,
+    },
+];
 
 export type RegistryImagesProps = {
     applicationId: string;
@@ -17,8 +29,38 @@ export type RegistryImagesProps = {
 export const RegistryImages = ({ applicationId, data }: RegistryImagesProps) => {
     const navigate = useNavigate();
 
+    // TODO: Repeated code from HealthStatusDataGrid.tsx. Move to Design System.
+    const [detailPanelExpandedRowIds, setDetailPanelExpandedRowIds] = useState<GridRowId[]>([]);
+    const handleDetailPanelExpandedRowIdsChange = (newIds: GridRowId[]) => {
+        if (detailPanelExpandedRowIds) {
+            // Remove previously expanded row id so only one panel can be expanded at the same time.
+            newIds = newIds.slice(-1);
+            setDetailPanelExpandedRowIds(newIds);
+        } else {
+            setDetailPanelExpandedRowIds(newIds);
+        }
+    };
+    const getDetailPanelHeight = useCallback(() => 'auto', []);
+
     return (
         <>
+            <DataGridWrapper>{/* sx={{ mb: 2 }} */}
+                <DataGridPro
+                    {...dataGridDefaultProps}
+                    rows={data.images}
+                    columns={registryImagesDataGridColumns}
+                    //getDetailPanelContent={({ row }: GridRowParams<HealthStatusDataGridRow>) => <DetailPanelContent row={row} />}
+                    getDetailPanelHeight={getDetailPanelHeight}
+                    detailPanelExpandedRowIds={detailPanelExpandedRowIds}
+                    onDetailPanelExpandedRowIdsChange={handleDetailPanelExpandedRowIdsChange}
+                    components={{
+                        DetailPanelExpandIcon,
+                        DetailPanelCollapseIcon,
+                        Toolbar: () => <DataGridCustomToolbar title={data.url} />,
+                    }}
+                />
+            </DataGridWrapper>
+
             <p>{data.url}</p>
 
             <TableContainer component={Paper}>
