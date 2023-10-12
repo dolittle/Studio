705a0cbb6ee3621ed2f1124b3766fd7d44800240
N00bG1rl <403helen@gmail.com>
2023-10-12 18:10:01 +0300

    Add DataGrid to Container Registry page (hard coded)

--- Source/SelfService/Web/applications/containerregistry/container/registryImages.tsx
+++ Source/SelfService/Web/applications/containerregistry/container/registryImages.tsx
@@ -1,18 +1,98 @@
 // Copyright (c) Aigonix. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useNavigate, generatePath } from 'react-router-dom';
 
 import { Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 import { DataGridPro, GridRowId, GridRowParams, GridColDef } from '@mui/x-data-grid-pro';
 
-import { dataGridDefaultProps, DataGridCustomToolbar, DataGridWrapper, DetailPanelExpandIcon, DetailPanelCollapseIcon } from '@dolittle/design-system';
+import {
+    dataGridDefaultProps,
+    DataGridCustomToolbar,
+    DataGridWrapper,
+    DetailPanelExpandIcon,
+    DetailPanelCollapseIcon,
+    Link,
+    LoadingSpinner,
+} from '@dolittle/design-system';
 
-import { ContainerRegistryImages } from '../../../apis/solutions/containerregistry';
+import { ContainerRegistryImages, ContainerRegistryTags, getTagsInContainerRegistry } from '../../../apis/solutions/containerregistry';
 
-export const registryImagesDataGridColumns: GridColDef[] = [
+const registryTagDataGridColumns: GridColDef[] = [
+    {
+        field: 'name',
+        headerName: 'Tag',
+        minWidth: 300,
+        flex: 1,
+        renderCell: (params) =>
+            <Link
+                message={params.row.name}
+                href={`/microservices/application/${params.row.application}/create?kind=dolittle-microservice#head-image=${encodeURIComponent(params.row.name)}:${params.row.name}`}
+            />
+    },
+    {
+        field: 'createdTime',
+        headerName: 'Created At',
+        minWidth: 300,
+        flex: 1,
+        valueFormatter: (params) => params.value.toLocaleString(),
+    },
+    {
+        field: 'lastUpdateTime',
+        headerName: 'Updated At',
+        minWidth: 300,
+        flex: 1,
+        valueFormatter: (params) => params.value.toLocaleString(),
+    },
+    {
+        field: 'digest',
+        headerName: 'Digest',
+        minWidth: 300,
+        flex: 1,
+        renderCell: (params) =>
+            <Link
+                message={params.row.name}
+                href={`/microservices/application/${params.row.application}/create?kind=dolittle-microservice#head-image=${encodeURIComponent(params.row.name)}:${params.row.name}@${params.row.digest}`}
+            />
+    },
+    {
+        field: 'signed',
+        headerName: 'Signed',
+        minWidth: 300,
+        flex: 1,
+        valueGetter: (params) => params.row.signed ? 'true' : 'false',
+    },
+];
+
+const DetailPanelContent = ({ row }: GridRowParams) => {
+    const [containerRegistryTags, setContainerRegistryTags] = useState({
+        name: '',
+        tags: [],
+    } as ContainerRegistryTags);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        Promise.all([getTagsInContainerRegistry(row.application, row.name)])
+            .then(values => setContainerRegistryTags(values[0]))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    if (isLoading) return <LoadingSpinner />;
+
+    return (
+        <DataGridWrapper>
+            <DataGridPro
+                {...dataGridDefaultProps}
+                rows={containerRegistryTags.tags}
+                columns={registryTagDataGridColumns}
+            />
+        </DataGridWrapper>
+    );
+};
+
+const registryImagesDataGridColumns: GridColDef[] = [
     {
         field: 'name',
         headerName: 'Container Image',
@@ -29,7 +109,7 @@ export type RegistryImagesProps = {
 export const RegistryImages = ({ applicationId, data }: RegistryImagesProps) => {
     const navigate = useNavigate();
 
-    // TODO: Repeated code from HealthStatusDataGrid.tsx. Move to Design System.
+    //TODO: Repeated code from HealthStatusDataGrid.tsx. Move to Design System.
     const [detailPanelExpandedRowIds, setDetailPanelExpandedRowIds] = useState<GridRowId[]>([]);
     const handleDetailPanelExpandedRowIdsChange = (newIds: GridRowId[]) => {
         if (detailPanelExpandedRowIds) {
@@ -44,23 +124,6 @@ export const RegistryImages = ({ applicationId, data }: RegistryImagesProps) => 
 
     return (
         <>
-            <DataGridWrapper>{/* sx={{ mb: 2 }} */}
-                <DataGridPro
-                    {...dataGridDefaultProps}
-                    rows={data.images}
-                    columns={registryImagesDataGridColumns}
-                    //getDetailPanelContent={({ row }: GridRowParams<HealthStatusDataGridRow>) => <DetailPanelContent row={row} />}
-                    getDetailPanelHeight={getDetailPanelHeight}
-                    detailPanelExpandedRowIds={detailPanelExpandedRowIds}
-                    onDetailPanelExpandedRowIdsChange={handleDetailPanelExpandedRowIdsChange}
-                    components={{
-                        DetailPanelExpandIcon,
-                        DetailPanelCollapseIcon,
-                        Toolbar: () => <DataGridCustomToolbar title={data.url} />,
-                    }}
-                />
-            </DataGridWrapper>
-
             <p>{data.url}</p>
 
             <TableContainer component={Paper}>
@@ -100,6 +163,23 @@ export const RegistryImages = ({ applicationId, data }: RegistryImagesProps) => 
                     </TableBody>
                 </Table>
             </TableContainer>
+
+            <DataGridWrapper>
+                <DataGridPro
+                    {...dataGridDefaultProps}
+                    rows={data.images}
+                    columns={registryImagesDataGridColumns}
+                    getDetailPanelContent={DetailPanelContent}
+                    getDetailPanelHeight={getDetailPanelHeight}
+                    detailPanelExpandedRowIds={detailPanelExpandedRowIds}
+                    onDetailPanelExpandedRowIdsChange={handleDetailPanelExpandedRowIdsChange}
+                    components={{
+                        DetailPanelExpandIcon,
+                        DetailPanelCollapseIcon,
+                        Toolbar: () => <DataGridCustomToolbar title={data.url} />,
+                    }}
+                />
+            </DataGridWrapper>
         </>
     );
 };
