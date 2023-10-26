0b1e6d8b22e165dcc7b83cea5e05889ae121df4c
N00bG1rl <403helen@gmail.com>
2023-10-26 10:04:59 +0300

    Change DialogForm LoadingSpinner size

--- Source/DesignSystem/atoms/LoadingSpinner/LoadingSpinner.tsx
+++ Source/DesignSystem/atoms/LoadingSpinner/LoadingSpinner.tsx
@@ -6,11 +6,21 @@ import React from 'react';
 import { Box, CircularProgress, CircularProgressProps } from '@mui/material';
 
 /**
+ * The props for a {@link LoadingSpinner} component.
+ */
+export type LoadingSpinnerProps = CircularProgressProps & {
+    /**
+     * Whether or not the loading spinner should take up the full height of the screen.
+     */
+    fullHeight?: boolean;
+};
+
+/**
  * The {@link LoadingSpinner} component is used to display a loading spinner.
- * @param {CircularProgress} props - The {@link CircularProgressProps}.
+ * @param {LoadingSpinnerProps} props - The {@link LoadingSpinnerProps}.
  * @returns A {@link LoadingSpinner} component.
  */
-export const LoadingSpinner = (props: CircularProgressProps) =>
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', p: 2 }}>
+export const LoadingSpinner = (props: LoadingSpinnerProps) =>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: props.fullHeight ? '100vh' : 'auto', p: 2 }}>
         <CircularProgress {...props} />
     </Box>;
