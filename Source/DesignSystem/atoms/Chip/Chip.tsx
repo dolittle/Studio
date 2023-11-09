19ef805c007ebc7ac4e8292a93408155428a4f8e
N00bG1rl <403helen@gmail.com>
2023-11-09 11:21:22 +0200

    Add Chip documentation

--- Source/DesignSystem/atoms/Chip/Chip.tsx
+++ Source/DesignSystem/atoms/Chip/Chip.tsx
@@ -9,13 +9,26 @@ import { Chip as MuiChip, SxProps } from '@mui/material';
  * The props for a {@link Chip} component.
  */
 export type ChipProps = {
+    /**
+     * The label of the chip.
+     */
     label: string;
+
+    /**
+     * The onDelete prop lets you add a delete functionality to the chip.
+     */
     onDelete?: () => void;
+
+    /**
+     * The sx prop lets you add custom styles to the component, overriding the styles defined by Material-UI.
+     */
     sx?: SxProps;
 };
 
 /**
- * The {@link Chip} component is used to display a chip.
+ * The chip component is used to show a small amount of information with a delete functionality.
+ * @param {ChipProps} props - The {@link ChipProps}.
+ * @returns A {@link Chip} component.
  */
 export const Chip = ({ label, onDelete, sx }: ChipProps) =>
     <MuiChip
