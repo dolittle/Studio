2e5a42b62588d26355be5fe293a269756ee18bf8
N00bG1rl <403helen@gmail.com>
2023-11-14 12:20:48 +0200

    Improve 'commands' tab wording

--- Source/SelfService/Web/integrations/connection/connectionDetails/commands/index.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/commands/index.tsx
@@ -1,28 +1,33 @@
 // Copyright (c) Aigonix. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Typography } from '@mui/material';
 
 import { ContentContainer, ContentWithSubtitle, NoContentSection, LoadingSpinner } from '@dolittle/design-system';
 
 export const CommandsView = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    if (isLoading) return <LoadingSpinner />;
+
     return (
         <ContentContainer sx={{ py: 2 }}>
             <Typography variant='subtitle1' sx={{ pb: 4 }}>
-                Commands
+                Your Commands
             </Typography>
 
             <Typography>
-                This is where you can send commands to the connection.
+                This is where you can manage your commands.
             </Typography>
 
             <ContentWithSubtitle title='Send a command' infoTooltipLabel='Send a command to the connection'>
                 <NoContentSection
                     title='No commands yet...'
-                    description={`To send a command to the connection, click the 'Send a command' button.`}
+                    description={`To send a command to the connection, click the 'Send A Command' button.`}
                     label='Send a command'
+                    icon='RocketLaunch'
                     onCreate={() => { }}
                 />
             </ContentWithSubtitle>
