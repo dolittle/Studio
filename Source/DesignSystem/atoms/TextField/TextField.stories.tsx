ab880cae00683defef2857211b6cb6a87b94fca4
N00bG1rl <403helen@gmail.com>
2023-12-20 12:12:04 +0200

    Update TextField component

--- Source/DesignSystem/atoms/TextField/TextField.stories.tsx
+++ Source/DesignSystem/atoms/TextField/TextField.stories.tsx
@@ -1,7 +1,11 @@
 // Copyright (c) Aigonix. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { componentStories, TextField } from '../../index';
+import React from 'react';
+
+import { action } from '@storybook/addon-actions';
+
+import { componentStories, IconButton, TextField } from '../../index';
 
 const { metadata, createStory } = componentStories(TextField);
 
@@ -12,25 +16,29 @@ metadata.title = 'Text Field';
 metadata.parameters = {
     docs: {
         description: {
-            component: `Text fields allow users to enter text into a UI.`
+            component: 'Text fields allow users to enter text into a UI.',
         },
-    },
-    controls: {
-        exclude: ['onValueChange', 'value', 'sx'],
     },
 };
 
+metadata.argTypes = {
+    value: { control: false },
+    onValueChange: { control: false },
+    sx: { control: false },
+    overrides: { control: false },
+};
+
 metadata.args = {
+    id: 'text-field',
     label: '',
     size: 'small',
     placeholder: '',
     helperText: '',
     isDisabled: false,
     isFullWidth: false,
-    startIcon: undefined,
-    endIcon: undefined,
-    iconColor: undefined,
-    onValueChange: () => { },
+    isRequired: false,
+    iconColor: 'inherit',
+    onValueChange: action('Value changed!'),
 };
 
 export const Default = createStory();
