e7923af6b10f04390a21d6ada6116989c9d3892e
N00bG1rl <403helen@gmail.com>
2024-01-11 17:23:45 +0200

    Refactor RestAPIView

--- Source/SelfService/Web/integrations/connection/connectionDetails/consumeData/RestAPI/resourcesSection/ActiveRestApiView.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/consumeData/RestAPI/resourcesSection/ActiveRestApiView.tsx
@@ -3,33 +3,18 @@
 
 import React from 'react';
 
-import { useSnackbar } from 'notistack';
-
 import { Typography } from '@mui/material';
 
-import { IconButton, InlineWrapper, Link } from '@dolittle/design-system';
-
+import { CopyIconButton, InlineWrapper, Link } from '@dolittle/design-system';
 
 export type ActiveRestApiViewProps = {
     restApiBaseUrl: string;
 };
 
 export const ActiveRestApiView = ({ restApiBaseUrl }: ActiveRestApiViewProps) => {
-    const { enqueueSnackbar } = useSnackbar();
-
     const baseApiUrlWithTrailingSlash = restApiBaseUrl.endsWith('/') ? restApiBaseUrl : `${restApiBaseUrl}/`;
     const restApiUrl = `${baseApiUrlWithTrailingSlash}swagger/index.html`;
     const openApiDocumentationUrl = `${baseApiUrlWithTrailingSlash}swagger/v1/swagger.json`;
-
-    const handleRestApiLinkCopy = () => {
-        navigator.clipboard.writeText(restApiUrl);
-        enqueueSnackbar('Rest API URL copied to clipboard.');
-    };
-
-    const handleOpenApiDocumentationLinkCopy = () => {
-        navigator.clipboard.writeText(openApiDocumentationUrl);
-        enqueueSnackbar('OpenAPI documentation copied to clipboard.');
-    };
 
     return (
         <>
@@ -38,7 +23,7 @@ export const ActiveRestApiView = ({ restApiBaseUrl }: ActiveRestApiViewProps) =>
 
                 <InlineWrapper>
                     <Link label={restApiUrl} href={restApiUrl} target ariaLabel='Rest API URL' />
-                    <IconButton tooltipText='Copy rest API URL link to clipboard' icon='CopyAllRounded' color='primary' onClick={handleRestApiLinkCopy} />
+                    <CopyIconButton text={restApiUrl} message='Rest API URL copied to clipboard.' color='primary' tooltipText='Copy the Rest API URL link to the clipboard.' />
                 </InlineWrapper>
             </section>
 
@@ -47,7 +32,7 @@ export const ActiveRestApiView = ({ restApiBaseUrl }: ActiveRestApiViewProps) =>
 
                 <InlineWrapper>
                     <Link label={openApiDocumentationUrl} href={openApiDocumentationUrl} target ariaLabel='OpenAPI documentation' />
-                    <IconButton tooltipText='Copy OpenAPI documentation link to clipboard' icon='CopyAllRounded' color='primary' onClick={handleOpenApiDocumentationLinkCopy} />
+                    <CopyIconButton text={openApiDocumentationUrl} message='OpenAPI documentation copied to clipboard.' color='primary' tooltipText='Copy the OpenAPI documentation link to the clipboard.' />
                 </InlineWrapper>
             </section>
         </>
