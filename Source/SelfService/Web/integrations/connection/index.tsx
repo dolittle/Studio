13a5f8c3f5244be49c7c7e60ccd5768847622c2d
N00bG1rl <403helen@gmail.com>
2024-01-25 13:22:16 +0200

    Change Connection error display to AlertBox

--- Source/SelfService/Web/integrations/connection/index.tsx
+++ Source/SelfService/Web/integrations/connection/index.tsx
@@ -5,6 +5,8 @@ import React from 'react';
 
 import { useRoutes } from 'react-router-dom';
 
+import { AlertBox } from '@dolittle/design-system';
+
 import { useConnectionsIdGet } from '../../apis/integrations/connectionsApi.hooks';
 
 import { routes } from './routes';
@@ -15,9 +17,10 @@ import { DebugRouter } from '../../components/debugRouter';
 export const Connection = () => {
     const connectionId = useConnectionIdFromRoute();
     const routesElement = useRoutes(routes);
+
     const query = useConnectionsIdGet({ id: connectionId });
 
-    if (query.isError) return <>Something went wrong. Could not load connection details for {connectionId}</>;
+    if (query.isError) return <AlertBox message={`Could not load connection details for '${connectionId}'.`} />;
 
     return (
         <>
