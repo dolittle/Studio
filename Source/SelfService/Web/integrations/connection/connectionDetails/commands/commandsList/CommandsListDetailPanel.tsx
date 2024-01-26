e67acc4438640c960c0a84c7f6e4bc9ff35fc499
N00bG1rl <403helen@gmail.com>
2024-01-26 15:23:26 +0200

    Add CommandsListDetailPanel

--- Source/SelfService/Web/integrations/connection/connectionDetails/commands/commandsList/CommandsListDetailPanel.tsx
+++ Source/SelfService/Web/integrations/connection/connectionDetails/commands/commandsList/CommandsListDetailPanel.tsx
@@ -3,93 +3,114 @@
 
 import React from 'react';
 
-import { Box, Stack, Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
+import { DataGridPro, DataGridProProps, GridRowId } from '@mui/x-data-grid-pro';
 
-import { Button, Checkbox, Form, InlineWrapper, Input, Select } from '@dolittle/design-system';
+import { Button, CreateButton, DataGridCustomToolbar, dataGridDefaultProps, DataGridWrapper, Form, InlineWrapper, Input, Select } from '@dolittle/design-system';
+
+const commandsListDetailPanelRows = [
+    {
+        m3Argument: 'CFAB',
+        description: 'Description',
+        parameterName: 'Parameter Name',
+        mode: 'Optional',
+        defaultValue: 'Default Value',
+    },
+    {
+        m3Argument: 'FACI',
+        description: 'Description',
+        parameterName: 'Parameter Name',
+        mode: 'Required',
+        defaultValue: 'Default Value',
+    },
+    {
+        m3Argument: 'CHID',
+        description: 'Description',
+        parameterName: 'Parameter Name',
+        mode: 'Hardcoded value',
+        defaultValue: 'Default Value',
+    },
+];
+
+const commandsListDetailPanelColumns = [
+    {
+        field: 'm3Argument',
+        headerName: 'M3 Argument',
+        flex: 1,
+    },
+    {
+        field: 'description',
+        headerName: 'Description',
+        flex: 1,
+    },
+    {
+        field: 'parameterName',
+        headerName: 'Parameter Name',
+        flex: 1,
+    },
+    {
+        field: 'mode',
+        headerName: 'Mode',
+        //<Select
+        //id='mode'
+        //             label='Mode'
+        //             options={
+        //     [
+        //     { value: 'Create', displayValue: 'Create' },
+        //     { value: 'Update', displayValue: 'Update' },
+        //     { value: 'Delete', displayValue: 'Delete' },
+        //             ]}
+        // />
+        flex: 1,
+    },
+    {
+        field: 'defaultValue',
+        headerName: 'Default Value',
+        flex: 1,
+    },
+];
+
+type CommandEditParametersProps = {
+    namespace: string;
+    description: string;
+};
 
 export const CommandsListDetailPanel = ({ row }: any) => {
     return (
-        <Box sx={{ m: 3, ml: 8 }}>
-            <Typography variant='subtitle1' sx={{ pb: 4 }}>
-                {row.name}
-            </Typography>
+        <Paper sx={{ p: 2, pl: 7.5 }}>
+            <Typography variant='h4' gutterBottom>{row.name}</Typography>
+            <Typography variant='body1' color='text.secondary'>{`Transaction: ${'MSN051MI > GetPaymentReq'}`}</Typography>
 
-            <Form
+            <Form<CommandEditParametersProps>
                 initialValues={{
-                    nameSpace: row.nameSpace,
+                    namespace: row.nameSpace,
                     description: row.description,
                 }}
                 onSubmit={() => { }}
             >
-                <Stack spacing={2} sx={{ width: 220 }}>
-                    <Input id='nameSpace' label='Namespace' />
-                    <Input id='endpoint' label='Endpoint' />
-                </Stack>
-
-                <Typography variant='subtitle1' sx={{ pt: 4 }}>
-                    Parameters
-                </Typography>
-
-                <InlineWrapper sx={{ alignItems: 'flex-start' }}>
-                    <Stack sx={{ mt: 2, width: 150 }}>
-                        <Typography variant='body2'>
-                            M3 Arguments
-                        </Typography>
-
-                        <Checkbox id='m3-arg-1' label='CFAB' />
-                        <Checkbox id='m3-arg-2' label='FACI' />
-                        <Checkbox id='m3-arg-3' label='CHID' />
-                    </Stack>
-
-                    <Stack spacing={1} sx={{ mt: 2, width: 400 }}>
-                        <Typography variant='body2'>
-                            Parameter Name
-                        </Typography>
-
-                        <InlineWrapper>
-                            <Typography sx={{ width: 150 }}>Facility Name</Typography>
-                            <Input id='facilityName' label='Facility Name' />
-                        </InlineWrapper>
-
-                        <InlineWrapper>
-                            <Typography sx={{ width: 150 }}>Facility</Typography>
-                            <Input id='facility' label='Facility' />
-                        </InlineWrapper>
-
-                        <InlineWrapper>
-                            <Typography sx={{ width: 150 }}>Changed By</Typography>
-                            <Input id='changedBy' label='Changed By' />
-                        </InlineWrapper>
-                    </Stack>
-
-                    <Stack spacing={1} sx={{ mt: 2, mx: 4, width: 150 }}>
-                        <Typography variant='body2'>
-                            Mode
-                        </Typography>
-
-                        <Select
-                            id='mode'
-                            label='Mode'
-                            options={[
-                                { value: 'Create', displayValue: 'Create' },
-                                { value: 'Update', displayValue: 'Update' },
-                                { value: 'Delete', displayValue: 'Delete' },
-                            ]}
-                        />
-                    </Stack>
-
-                    <Stack spacing={1} sx={{ mt: 2 }}>
-                        <Typography variant='body2'>
-                            Default Value
-                        </Typography>
-
-                        <Input id='value1' label='' />
-                        <Input id='value2' label='' />
-                    </Stack>
+                <InlineWrapper sx={{ my: 4 }}>
+                    <Typography>Namespace:</Typography>
+                    <Input id='namespace' label='Namespace' sx={{ '& .MuiOutlinedInput-root': { width: 220 } }} />
                 </InlineWrapper>
 
-                <Button label='Add Parameter' type='submit' variant='filled' sx={{ mt: 6 }} />
+                <DataGridWrapper background='dark' sx={{ height: 300 }}>
+                    <DataGridPro
+                        {...dataGridDefaultProps}
+                        rows={commandsListDetailPanelRows}
+                        columns={commandsListDetailPanelColumns}
+                        getRowId={row => row.m3Argument}
+                        autoHeight={false}
+                        components={{
+                            Toolbar: () => <DataGridCustomToolbar title='Parameters'>
+                                <Button label='Add Parameters' startWithIcon='AddCircle' />
+                                <Button label='Save Changes' startWithIcon='AddCircle' />
+                            </DataGridCustomToolbar>
+                        }}
+                    />
+                </DataGridWrapper>
+
+                {/* <CreateButton label='Save changes and close' onCreate={() => { }} /> */}
             </Form>
-        </Box>
+        </Paper>
     );
 };
