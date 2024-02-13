5f48bfdaa1ab0757467697046a2eb0111be6ca67
N00bG1rl <403helen@gmail.com>
2024-02-13 16:56:04 +0200

    Start with 'command view' page

---
+++ Source/SelfService/Web/integrations/connection/connectionDetails/commands/command/index.tsx
@@ -0,0 +1,16 @@
+// Copyright (c) Aigonix. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React, { useState } from 'react';
+
+//import { useLocation, useParams } from 'react-router-dom';
+
+import { AlertBox, ContentContainer } from '@dolittle/design-system';
+
+export const CommandView = () => {
+    return (
+        <ContentContainer>
+            <></>
+        </ContentContainer>
+    );
+};
